e5dccb13ef4e996652b40646757eb45c4ea7e06d
luiscornejocornejo <kayser19992005@gmail.com>
2023-02-02 11:41:49 -0300

    init

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,35 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    server: { 
+        https: false, 
+        host: 'localhost', 
+    }, 
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });
+
+
