7b4101f90eda9a27c043ed54bf16baa9d2dcb8a7
luiscornejocornejo <kayser19992005@gmail.com>
2023-02-02 21:52:23 -0300

    dise

--- resources/js/components/LuisComponent.vue
+++ resources/js/components/LuisComponent.vue
@@ -57,7 +57,7 @@
                     <!-- users -->
                     <div class="row">
                         <div class="col">
-                            <div class="card-body py-0 mb-3" data-simplebar >
+                            <div class="card-body py-0 mb-3" data-simplebar style="max-height: 346px">
 
 
                                 <section v-if="errored">
