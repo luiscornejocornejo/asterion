976a3bcd8ea04e778b884fc1d37f110dcdf30736
luiscornejocornejo <kayser19992005@gmail.com>
2023-02-05 14:39:36 -0300

    chat

--- resources/js/components/LuisComponent.vue
+++ resources/js/components/LuisComponent.vue
@@ -235,7 +235,7 @@
                                         aria-label="scrollable content" style="height: auto; overflow: hidden scroll;">
                                         <div class="simplebar-content" style="padding: 0px 24px;">
                                             <div class="timeline-alt py-0">
-                                                <div v-for="extrah in historial"  class="timeline-item extrahistorial2">
+                                                <div v-for="extrah in historialllll"  class="timeline-item extrahistorial2">
                                                     <i
                                                         class="mdi mdi-upload bg-info-lighten text-info timeline-icon"></i>
                                                     <div class="timeline-item-info">
@@ -501,6 +501,10 @@ export default {
             this.nombreusuario = b;
             this.ticket_id = d;
             this.source = e;
+            this.historialllll = this.extrahistorial(d);
+            console.log('extrahistorial.')
+
+            console.log(this.historialllll)
 
             if (e == "Email") {
                 this.datoooo = this.extrasmail(d);
@@ -529,10 +533,7 @@ export default {
 
 
             }
-            this.historialllll = this.extrahistorial(d);
-            console.log('extrahistorial.')
-
-            console.log(this.historialllll)
+            
 
         }
 
