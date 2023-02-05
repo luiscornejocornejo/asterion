03ce7479049ee6fc9ac3ceefdd6b09462a852c17
luiscornejocornejo <kayser19992005@gmail.com>
2023-02-05 14:34:43 -0300

    chat

--- resources/js/components/LuisComponent.vue
+++ resources/js/components/LuisComponent.vue
@@ -235,7 +235,7 @@
                                         aria-label="scrollable content" style="height: auto; overflow: hidden scroll;">
                                         <div class="simplebar-content" style="padding: 0px 24px;">
                                             <div class="timeline-alt py-0">
-                                                <div v-for="extrah in historialllll"  class="timeline-item extrahistorial2">
+                                                <div v-for="extrah in historial"  class="timeline-item extrahistorial2">
                                                     <i
                                                         class="mdi mdi-upload bg-info-lighten text-info timeline-icon"></i>
                                                     <div class="timeline-item-info">
@@ -530,6 +530,8 @@ export default {
 
             }
             this.historialllll = this.extrahistorial(d);
+            console.log('extrahistorial.')
+
             console.log(this.historialllll)
 
         }
