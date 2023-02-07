0accc0c968b7486bf0d936513b7ebe8a2c32e93c
luiscornejocornejo <kayser19992005@gmail.com>
2023-02-07 14:24:32 -0300

    chat

--- resources/js/components/LuisComponent.vue
+++ resources/js/components/LuisComponent.vue
@@ -293,7 +293,7 @@
     <div class="col-xxl-3 col-xl-6 order-xl-1 order-xxl-2">
 
         <div class="accordion accordion-flush" id="accordionFlushExample">
-            <div class="accordion-item">
+            <div class="accordion-item bg-primary">
                 <h2 class="accordion-header" id="flush-headingOne">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                         data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
@@ -375,7 +375,7 @@
                 </div>
 
             </div>
-            <div class="accordion-item">
+            <div class="accordion-item bg-primary">
                 <h2 class="accordion-header" id="flush-headingTwo">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                         data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
