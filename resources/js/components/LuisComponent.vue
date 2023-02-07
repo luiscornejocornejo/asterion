ef7e450d47ec393d2ba6036f5808f2f53b56d7bc
luiscornejocornejo <kayser19992005@gmail.com>
2023-02-07 01:30:24 -0300

    chat

--- resources/js/components/LuisComponent.vue
+++ resources/js/components/LuisComponent.vue
@@ -68,7 +68,7 @@
                                 <section v-else>
                                     <div v-if="loading">Cargando...</div>
 
-                                    <div v-else v-for="currency in info  hora(currency.creacion)"  
+                                    <div v-else v-for="currency in info , hora(currency.creacion)"  
                                         class="currency shadow-sm p-3 mb-3 bg-white rounded"
                                         v-on:click="pasar(currency.depto, currency.nombreusuario, currency.lastupdate, currency.ticket_id, currency.source,currency.creacion)">
 
