3a7366775c6113cebcd806581a2ce39dc3987035
luiscornejocornejo <kayser19992005@gmail.com>
2023-02-08 02:27:48 -0300

    chat

--- resources/views/sienna/chat.blade.php
+++ resources/views/sienna/chat.blade.php
@@ -18,7 +18,7 @@
         </div>
         @endif
 
-        <div class="content-page" style="height: auto; ">
+        <div class="content-page p-2" style="height: auto; ">
 
 
                 <!-- Start Content-->
