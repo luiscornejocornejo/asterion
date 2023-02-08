49b7f0645f9a3910f221612e0f6b145655d6c5a8
luiscornejocornejo <kayser19992005@gmail.com>
2023-02-08 00:46:16 -0300

    chat

--- resources/js/components/LuisComponent.vue
+++ resources/js/components/LuisComponent.vue
@@ -77,8 +77,8 @@
                                             <a href="javascript:void(0);" class="text-body">
                                                 {{ currency.chat_status }}
 
-                                                <div :style="{ 'background-color': currency.priority_color }" style="float: left;width: 10px;height: 100px;"
-                                                    ></div> 
+                                                <div :style="{ 'background-color': currency.priority_color }" style="float: left;width: 10px;height: 100%;"
+                                                  class="p-2"  ></div> 
                                                    
                                                 <div class="container">
                                                     <div class="row">
