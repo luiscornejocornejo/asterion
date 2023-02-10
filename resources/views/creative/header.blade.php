06373db66bcc29d9a5410a7abb48b10f5f6d0b10
luiscornejocornejo <kayser19992005@gmail.com>
2023-02-10 12:59:01 -0300

    inicio

--- resources/views/creative/header.blade.php
+++ resources/views/creative/header.blade.php
@@ -97,8 +97,8 @@ foreach ($resultados2 as $valuee2) {
             document.getElementById("idregistro").value = x;
         }
     </script>
-    <script data-id='xenioo' data-node='meerkat' src="https://static.xenioo.com/webchat/xenioowebchat.js"></script>
+   <script data-id='xenioo' data-node='meerkat' src="https://static.xenioo.com/webchat/xenioowebchat.js"></script>
 <script>
-    xenioowebchat.Start("e64014d6-2770-4523-b2b2-1c9a56a3122f");
+    xenioowebchat.Start("057a5242-4957-445d-9a5c-d4415cb07ea3");
 </script>
 </head>
