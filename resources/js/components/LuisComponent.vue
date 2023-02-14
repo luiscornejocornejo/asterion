9886999be42fa4ab4fd2107a3f5bdff128154c59
luiscornejocornejo <kayser19992005@gmail.com>
2023-02-14 15:10:22 -0300

    inicio

--- resources/js/components/LuisComponent.vue
+++ resources/js/components/LuisComponent.vue
@@ -31,7 +31,7 @@
                                     <section v-else>
                                         <div v-if="loading">Cargando...</div>
 
-                                        <div v-else v-for="currency in info " v-bind:style="bgc"
+                                        <div v-else v-for="currency in info " v-bind:style="bgc"  :style="{ 'background-color': currency.chat_status }"
                                             v-on:click="pasar(currency.depto, currency.nombreusuario, currency.lastupdate, currency.ticket_id, currency.source, currency.creacion, currency.topic, currency.status_id, currency.priority_desc, currency.priority_color, currency.asignado, currency.user_id)"
                                             class="currency  p-2 mb-3 bg-white rounded " style=" box-shadow: 4px 4px rgba(0,0,0,0.25);  border-left-color: rgba(0, 255, 0, 0.5);  ">
 
@@ -42,7 +42,7 @@
                                               
 
                                                 <div class="container"
-                                                    :style="{ 'background-color': currency.chat_status }">
+                                                    >
                                                     <div class="row">
                                                         <div class="col-6 align-self-start"
                                                             style="display:inline-block;">
