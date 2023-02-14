0202bfd5155af07946763c3ba6cbf3d8cc53aa3a
luiscornejocornejo <kayser19992005@gmail.com>
2023-02-14 13:25:25 -0300

    inicio

--- resources/js/components/LuisComponent.vue
+++ resources/js/components/LuisComponent.vue
@@ -33,7 +33,7 @@
 
                                         <div v-else v-for="currency in info " v-bind:style="bgc"
                                             v-on:click="pasar(currency.depto, currency.nombreusuario, currency.lastupdate, currency.ticket_id, currency.source, currency.creacion, currency.topic, currency.status_id, currency.priority_desc, currency.priority_color, currency.asignado, currency.user_id)"
-                                            class="currency  p-2 mb-3 bg-white rounded " style="   box-shadow: 30px 0px rgba(0,0,0,0.5);">
+                                            class="currency  p-2 mb-3 bg-white rounded " style="   box-shadow: 10px 10px rgba(0,0,0,0.5);">
 
 
                                             <a href="javascript:void(0);" class="text-body">
