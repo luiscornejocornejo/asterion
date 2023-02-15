7c51e1f358261a7c2c323bb69fbadaef3a252c4d
luiscornejocornejo <kayser19992005@gmail.com>
2023-02-15 09:32:26 -0300

    ini

--- resources/js/components/LuisComponent.vue
+++ resources/js/components/LuisComponent.vue
@@ -32,7 +32,7 @@
                                         <div v-if="loading">Cargando...</div>
 
                                         <div v-else v-for="currency in info "  style="box-shadow: 4px 4px rgba(0,0,0,0.25);border-left:2px solid {{ currency.priority_color }}"  
-                                          :style="{ 'background-color': currency.chat_status},{ 'border-left': currency.priority_color} "
+                                          :style="{ 'background-color': currency.chat_status, 'border-left': currency.priority_color} "
                                             v-on:click="pasar(currency.depto, currency.nombreusuario, currency.lastupdate, currency.ticket_id, currency.source, currency.creacion, currency.topic, currency.status_id, currency.priority_desc, currency.priority_color, currency.asignado, currency.user_id)"
                                             class="currency  p-2 mb-3  rounded " >
 {{  currency.priority_color }}
