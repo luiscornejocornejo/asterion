2bc916181e4e458e314754a54bb331013daa476b
luiscornejocornejo <kayser19992005@gmail.com>
2023-02-22 15:26:36 -0300

    ini

--- resources/js/components/LuisComponent.vue
+++ resources/js/components/LuisComponent.vue
@@ -64,7 +64,7 @@
                                                             </h5>
                                                         </div>
                                                         <div class="col-6 align-self-end" style="display:inline-block;">
-                                                            <span class="float-end text-muted small">Asignado</span>
+                                                            <span class="float-end text-muted small"></span>
                                                         </div>
                                                     </div>
                                                     <div class="row">
@@ -72,7 +72,7 @@
                                                             style="display:inline-block;">
 
 
-                                                            <h5 v-if="currency.source == 'Whatsapp'"> <svg style=""
+                                                            <h5 v-if="currency.source == 'Whatsapp'" style="color: #98a6ad;"> <svg style=""
                                                                     xmlns="http://www.w3.org/2000/svg" width="16"
                                                                     height="16" fill="currentColor"
                                                                     class="bi bi-whatsapp" color="#25D366"
@@ -80,7 +80,7 @@
                                                                     <path
                                                                         d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
                                                                 </svg> {{ currency.number }}</h5>
-                                                            <h5 v-if="currency.source == 'API'"><svg fill="#000000"
+                                                            <h5 v-if="currency.source == 'API'" style="color: #98a6ad;"><svg fill="#000000"
                                                                     height="16px" width="16px" version="1.1"
                                                                     id="Layer_1" xmlns="http://www.w3.org/2000/svg"
                                                                     xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -114,7 +114,7 @@
                                                                     </g>
                                                                 </svg>{{ currency.number }}</h5>
 
-                                                            <h5 v-if="currency.source == 'SMS'">
+                                                            <h5 v-if="currency.source == 'SMS'" style="color: #98a6ad;">
                                                                 <svg xmlns="http://www.w3.org/2000/svg"
                                                                     viewBox="0 0 100 100" width="16px" height="16px">
                                                                     <path
@@ -122,7 +122,7 @@
                                                                 </svg>
                                                                 {{ currency.number }}
                                                             </h5>
-                                                            <h5 v-if="currency.source == 'Telegram'">
+                                                            <h5 v-if="currency.source == 'Telegram'" style="color: #98a6ad;">
                                                                 <svg version="1.1" id="Layer_1"
                                                                     xmlns="http://www.w3.org/2000/svg"
                                                                     xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
@@ -135,7 +135,7 @@
                                                                 </svg>
                                                                 {{ currency.number }}
                                                             </h5>
-                                                            <h5 v-if="currency.source == 'Facebook'">
+                                                            <h5 v-if="currency.source == 'Facebook'" style="color: #98a6ad;">
                                                                 <svg xmlns="http://www.w3.org/2000/svg"
                                                                     viewBox="0 0 48 48" width="16px" height="16px">
                                                                     <path fill="#3F51B5"
@@ -145,7 +145,7 @@
                                                                 </svg>
                                                                 {{ currency.number }}
                                                             </h5>
-                                                            <h5 v-if="currency.source == 'Discord'">
+                                                            <h5 v-if="currency.source == 'Discord'" style="color: #98a6ad;">
                                                                 <svg xmlns="http://www.w3.org/2000/svg"
                                                                     xmlns:xlink="http://www.w3.org/1999/xlink"
                                                                     viewBox="0 0 50 50" width="16px" height="16px">
@@ -154,7 +154,7 @@
                                                                 </svg>
                                                                 {{ currency.number }}
                                                             </h5>
-                                                            <h5 v-if="currency.source == 'Email'">
+                                                            <h5 v-if="currency.source == 'Email'" style="color: #98a6ad;">
                                                                 <svg xmlns="http://www.w3.org/2000/svg"
                                                                     viewBox="0 0 50 50" width="16px" height="16px">
                                                                     <path
@@ -162,7 +162,7 @@
                                                                 </svg>
                                                                 {{ currency.number }}
                                                             </h5>
-                                                            <h5 v-if="currency.source == 'Slack'">
+                                                            <h5 v-if="currency.source == 'Slack'" style="color: #98a6ad;">
                                                                 <svg xmlns="http://www.w3.org/2000/svg"
                                                                     viewBox="0 0 40 40" width="16px" height="16px">
                                                                     <path fill="#8cd3df"
@@ -212,11 +212,11 @@
                                                                 </svg>
                                                                 {{ currency.number }}
                                                             </h5>
-                                                            <h5 v-if="currency.source == ''">
+                                                            <h5 v-if="currency.source == ''" style="color: #98a6ad;">
 
                                                                 {{ currency.number }}
                                                             </h5>
-                                                            <h5 v-if="currency.source == 'Instagram'">
+                                                            <h5 v-if="currency.source == 'Instagram'" style="color: #98a6ad;">
                                                                 <svg xmlns="http://www.w3.org/2000/svg"
                                                                     viewBox="0 0 48 48" width="16px" height="16px">
                                                                     <radialGradient id="yOrnnhliCrdS2gy~4tD8ma"
@@ -250,7 +250,7 @@
                                                                 </svg>
                                                                 {{ currency.number }}
                                                             </h5>
-                                                            <h5 v-if="currency.source == 'Web'">
+                                                            <h5 v-if="currency.source == 'Web'" style="color: #98a6ad;">
                                                                 <svg fill="#000000" height="16px" width="16px"
                                                                     version="1.1" id="Layer_1"
                                                                     xmlns="http://www.w3.org/2000/svg"
