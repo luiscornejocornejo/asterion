f3224bbf5c477cfcf9ba2a29aa2daff861d69aa9
luiscornejocornejo <kayser19992005@gmail.com>
2023-02-23 08:10:21 -0300

    ini

--- resources/views/sienna/chat.blade.php
+++ resources/views/sienna/chat.blade.php
@@ -18,7 +18,7 @@
         </div>
         @endif
 
-        <div class="content-page p-8 ml-2 mr-2" style="height: auto; ">
+        <div class="content-page p-8 ml-2 mr-2 m-4" style="height: auto; ">
 
 
                 <!-- Start Content-->
