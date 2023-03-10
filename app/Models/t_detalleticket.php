f2c09a8ccaebd244ae5b98963982aadbcdde6ea2
luiscornejocornejo <kayser19992005@gmail.com>
2023-03-10 15:33:26 +0000

    bita

---
+++ app/Models/t_detalleticket.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class t_detalleticket extends Model
+{
+    use HasFactory;
+}
