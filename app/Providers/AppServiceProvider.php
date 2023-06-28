9f5eb52dd41fc0184ab7d0e212a6b476bc418a9e
luiscornejocornejo <kayser19992005@gmail.com>
2023-06-28 00:09:24 -0300

    dd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Pagination\Paginator;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,8 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        URL::forceScheme('https');
-       // Paginator::useBootstrap();
+        \URL::forceScheme('https');
 
-    }
+Paginator::useBootstrap();
+
+    }  
 }
