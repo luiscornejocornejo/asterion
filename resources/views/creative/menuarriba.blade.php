6048aef204201bde49af5b4f4be89df4a403ecc9
luiscornejocornejo <kayser19992005@gmail.com>
2023-07-12 20:45:22 -0300

    ee

--- resources/views/creative/menuarriba.blade.php
+++ resources/views/creative/menuarriba.blade.php
@@ -21,10 +21,10 @@ foreach($resultados as $value){
                         <!-- Logo light -->
                         <a href="#" class="logo-light">
                             <span class="logo-lg m2">
-                                <img src="/img/suri.jpg" alt="logo" height="22">
+                                <img src="/img/suri.jpg" alt="logo" height="100">
                             </span>
                             <span class="logo-sm m2">
-                                <img src="/img/suri.jpg" alt="small logo" height="22">
+                                <img src="/img/suri.jpg" alt="small logo" height="100">
                             </span>
                         </a>
 
