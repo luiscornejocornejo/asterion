7fc329d4142f040b90e49b8008b9c1cb4231f4cf
luiscornejocornejo <kayser19992005@gmail.com>
2023-07-25 13:45:36 -0300

    rrr

--- resources/views/creative/menuarriba.blade.php
+++ resources/views/creative/menuarriba.blade.php
@@ -60,7 +60,7 @@ foreach ($resultados as $value) {
                     
                 }
 
-                if($subdomain_tmp =="redlam"){?>
+                if($subdomain_tmp =="redlam2"){?>
  <button class="btn btn-primary btn-sm" id="btnAbrirAgregarBien" data-toggle="modal" data-target="#modalExample">
     <i class="mdi mdi-plus-circle-outline"></i>Iniciar Conversacion
 </button>
