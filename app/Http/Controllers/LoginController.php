805720e65af9e94b5f3d3bbc164e9120016d413c
luiscornejocornejo <kayser19992005@gmail.com>
2023-07-31 11:09:50 -0300

    rr

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -202,8 +202,10 @@ class LoginController extends Controller
 
                                             //  dd($res);
                                                  $urlfinal=$res['Data']['Home'];
-                                                 $urlfinal=explode("auth",$urlfinal);
-                                                 $url=$urlfinal[0]."bots/6d3b5545-0b8a-4215-9baa-c7cd8d518913/conversation";
+                                                 $urlfinal.="conversation";
+                                                 $url=$urlfinal;
+                                                // $urlfinal=explode("auth",$urlfinal);
+                                                 //$url=$urlfinal[0]."bots/6d3b5545-0b8a-4215-9baa-c7cd8d518913/conversation";
                         //  dd($url);https://designer.xenioo.com/en/bots/6d3b5545-0b8a-4215-9baa-c7cd8d518913/conversation
                                             
                                              //   dd($url);
