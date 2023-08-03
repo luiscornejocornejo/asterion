6816e1468d6afa38057477986f23eadfbc41bdf4
luiscornejocornejo <kayser19992005@gmail.com>
2023-08-03 18:15:14 -0300

    cc

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -159,7 +159,7 @@ class LoginController extends Controller
                     echo  $AccountAPIKey="9474C39E-5E40-4A99-96B4-9709EAFA677A";
                      $BotAPIKey="Ob7mdpIh3YaiU9F3MOQUuOiW";
                      $BotAPISecret="uavtbNMlky1FUijOjnE0PdsLbxsxCoA0sas2uO6gCoyV7zZd";
-                     $saliente="https://meerkat.xenioo.com/bc/eeghPqCSLVA78h7FlGLAY0Zf/nSfNwBMaY92Oc8ERWTSWLsvf";
+                     $saliente="";
                      session(['saliente' => $saliente]);
                      $url='https://meerkat.xenioo.com/authorization/sso';
  
