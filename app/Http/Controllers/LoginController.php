476fc7dcdcad6a6bfc3158bae10537acccbc6842
luiscornejocornejo <kayser19992005@gmail.com>
2023-08-09 10:31:01 -0300

    dd

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -147,15 +147,15 @@ class LoginController extends Controller
                     if( $url=='https://publicapi.xenioo.com/sso/authorize'){
 
 
-                        dd($res);
+                        //dd($res);
                                                  $urlfinal=$res['Data']['Home'];
-                                                // $urlfinal.="conversation";
+                                                 $urlfinal.="conversation";
                                                  $url=$urlfinal;
                                                 //
                                                 
                                              //   $urlfinal=explode("auth",$urlfinal);
                                                //
-                                                $url=$urlfinal[0]."bots/6d3b5545-0b8a-4215-9baa-c7cd8d518913/conversation";
+                                               // $url=$urlfinal[0]."bots/6d3b5545-0b8a-4215-9baa-c7cd8d518913/conversation";
                         //  dd($url);https://designer.xenioo.com/en/bots/6d3b5545-0b8a-4215-9baa-c7cd8d518913/conversation
                                             
                                              //   dd($url);
