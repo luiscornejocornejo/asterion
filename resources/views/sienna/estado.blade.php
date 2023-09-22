9f0cc853a3fd24654d129ff32012efb6ab082a15
luiscornejocornejo <kayser19992005@gmail.com>
2023-09-21 23:43:02 -0300

    ff

--- resources/views/sienna/estado.blade.php
+++ resources/views/sienna/estado.blade.php
@@ -1,4 +1,4 @@
-@include('pp.header')
+@include('creative2.header')
 
 <div id="principal">
     <div class="mx-auto" style="width: 1000px;margin-top: 70px;">
@@ -91,4 +91,4 @@ header('Content-type: image/png');
     </div>
 </div>
 <br><br><br>
-    @include('pp.footer')
+    @include('creative2.footer')
