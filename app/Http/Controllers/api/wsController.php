245535edd3a5695a68443c649e3d18a9d061dbff
luiscornejocornejo <kayser19992005@gmail.com>
2023-10-17 22:39:04 -0300

    asa

--- app/Http/Controllers/api/wsController.php
+++ app/Http/Controllers/api/wsController.php
@@ -330,7 +330,7 @@ class wsController extends Controller
 
         $prueba = $this->conectar(14);
         //si es distinta a 1 aa otra base
-       echo  $query3="select * from ".$inte.".ws_cliente where nombre='".$merchant."'";
+         $query3="select * from ".$inte.".ws_cliente where nombre='".$merchant."'";
         $datos = DB::connection('mysql2')->select($query3);
 
         foreach($datos as $val){
