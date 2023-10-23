b90976f5f29139560a4b06f9fdf6f42ef78ecdb4
luiscornejocornejo <kayser19992005@gmail.com>
2023-10-23 14:28:01 -0300

    swsss

--- resources/views/facu/menu.blade.php
+++ resources/views/facu/menu.blade.php
@@ -1,3 +1,17 @@
+
+
+<?php
+
+use Illuminate\Support\Facades\DB;
+
+$categoria = session('categoria');
+
+$query = "SELECT m.nombre as nombremenu,i.nombre as icono,m.id FROM `categoria_menu` cm
+join menu m on m.id=cm.menu join icono i on i.id=m.icono WHERE categoria=" . $categoria . ";
+";
+$resultados = DB::select($query);
+
+?>
 <div class="leftside-menu menuitem-active">
 
 <!-- Brand Logo Light -->
@@ -54,15 +68,112 @@
             <span> Conversaciones </span>
         </a>
     </li>
-   
-    
+
+
     <li class="side-nav-item position-absolute fixed-bottom background-buttons">
         <a data-bs-toggle="collapse" href="#" aria-expanded="false" aria-controls="sidebarDashboards" class="side-nav-link background-buttons">
             <i class="uil-exit"></i>
             <span> Cerrar sesión </span>
         </a>
     </li>
+    <?php
+if ($categoria == 1) {?>
 
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#sidebarDashboards" aria-expanded="false" aria-controls="sidebarDashboards" class="side-nav-link">
+                    <i class="uil-home-alt"></i>
+                    <span class="badge bg-success float-end"></span>
+                    <span> Sienna </span>
+                </a>
+                <div class="collapse" id="sidebarDashboards">
+                    <ul class="side-nav-second-level">
+                        <li>
+                        <a target="_self" href="/sienna">
+                                    <span id="sienna" onmouseover="changeColor(this.id, '#38e991')"  onmouseout="retro(this.id, '#a6e8ff')" class="sin" data-key="t-Base">Sienna</span>
+                                </a>
+                        </li>
+                        <li>
+                        <a target="_self" href="/siennaabm?id=13">
+                                    <span id="Usuarios" onmouseover="changeColor(this.id, '#38e991')"  onmouseout="retro(this.id, '#a6e8ff')" class="sin" data-key="t-Base">Usuarios</span>
+                                </a>
+                        </li>
+                        <li>
+                        <a target="_self" href="/siennamenu?id=1">
+                                    <span id="report" onmouseover="changeColor(this.id, '#38e991')"  onmouseout="retro(this.id, '#a6e8ff')" class="sin" data-key="t-Base">Report</span>
+                                </a>
+                        </li>
+
+
+                    </ul>
+                </div>
+            </li>
+            <?php 
+}?>
+       <?php
+            if (sizeof($resultados) > 0) {
+
+                foreach ($resultados as $value) {
+
+            ?>
+
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#<?php echo $value->nombremenu; ?>" aria-expanded="false" aria-controls="<?php echo $value->nombremenu; ?>" class="side-nav-link">
+                    <i class="<?php echo $value->icono; ?>"></i>
+                    <span class="badge bg-success float-end"></span>
+                    <span> <?php echo $value->nombremenu; ?> </span>
+                </a>
+                <div class="collapse" id="<?php echo $value->nombremenu; ?>">
+                <?php
+
+                        $query2 = "SELECT m.servicio,m.id,m.nombre FROM `menu_reporte` mr 
+                        join masterreport m on m.id=mr.masterreport where menu=" . $value->id . ";
+                        ";
+                        $resultados2 = DB::select($query2);
+                        if (sizeof($resultados2) > 0) {
+                        ?>
+                    <ul class="side-nav-second-level">
+                        <li>
+                        <?php
+                                foreach ($resultados2 as $value2) {
+                                    $nom=$value2->nombre;
+                                    $a ="";
+                                    if ($value2->servicio == 2) {
+                                        $a = "siennagraficos?id=" . $value2->id ;
+                                    } elseif ($value2->servicio == 1) {
+                                        $a = "siennareport?id=" . $value2->id ;
+                                    } elseif ($value2->servicio == 3) {
+                                        $a = "siennaendpoint?id=" . $value2->id ;
+                                    } elseif ($value2->servicio == 4) {
+                                        $a = "siennaemail?id=" . $value2->id ;
+                                    } elseif ($value2->servicio == 5) {
+
+                                        $a = "siennaabm?id=" . $value2->id ;
+                                    } elseif ($value2->servicio == 6) {
+
+                                        $a = "siennaform?id=" . $value2->id ;
+                                    }
+                                    elseif ($value2->servicio == 9) {
+
+                                        $a = "chat?id=" . $value2->id ;
+                                    }
+
+                                    
+                                ?>
+                                    <li><a target='_self' href="<?php echo $a; ?>"><span id="<?php echo $nom;?> " onmouseover="changeColor(this.id, '#38e991')"  onmouseout="retro(this.id, '#a6e8ff')" class="sin" data-key="t-Base"><?php echo $nom;?></span>
+                             </a></li>
+                            <?php
+                                }?>
+                        </li>
+                        
+                      
+                    
+                       
+                    </ul>
+
+                    <?php }?>
+                </div>
+            </li>
+            <?php }}?>
 
     </ul>
     <!--- End Sidemenu -->
