eb257e203c4d781c2d0d51e6d026af3daf94839b
luiscornejocornejo <kayser19992005@gmail.com>
2023-10-23 14:21:50 -0300

    sss

--- resources/views/sienna/facusinheader.blade.php
+++ resources/views/sienna/facusinheader.blade.php
@@ -4,88 +4,8 @@
   <div class="wrapper">
 
       <!-- ========== Left Sidebar Start ========== -->
-      <div class="leftside-menu menuitem-active">
+      @include('facu.menu')
 
-          <!-- Brand Logo Light -->
-          <a href="index.html" class="logo logo-light">
-              <span class="logo-lg">
-                  <img src="assetsfacu/images/logo.svg" alt="logo">
-              </span>
-              <span class="logo-sm mt-2 ms-1">
-                  <img src="assetsfacu/images/logo.png" alt="small logo">
-              </span>
-          </a>
-
-          <!-- Brand Logo Dark -->
-          <a href="index.html" class="logo logo-dark">
-              <span class="logo-lg">
-                  <img src="assetsfacu/images/logo-dark.png" alt="dark logo">
-              </span>
-              <span class="logo-sm">
-                  <img src="assetsfacu/images/logo-dark-sm.png" alt="small logo">
-              </span>
-          </a>
-
-          <!-- Sidebar Hover Menu Toggle Button -->
-          <div class="button-sm-hover" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Show Full Sidebar" data-bs-original-title="Show Full Sidebar">
-              <i class="ri-checkbox-blank-circle-line align-middle"></i>
-          </div>
-
-          <!-- Full Sidebar Menu Close Button -->
-          <div class="button-close-fullsidebar">
-              <i class="ri-close-fill align-middle"></i>
-          </div>
-
-          <!-- Sidebar -left -->
-          <div class="h-100 show" id="leftside-menu-container" data-simplebar="init"><div class="simplebar-wrapper" style="margin: 0px;"><div class="simplebar-height-auto-observer-wrapper"><div class="simplebar-height-auto-observer"></div></div><div class="simplebar-mask"><div class="simplebar-offset" style="right: 0px; bottom: 0px;"><div class="simplebar-content-wrapper" tabindex="0" role="region" aria-label="scrollable content" style="height: 100%; overflow: scroll hidden;"><div class="simplebar-content" style="padding: 0px;">
-              <!-- Leftbar User -->
-              <div class="leftbar-user">
-                  <a href="pages-profile.html">
-                      <img src="assetsfacu/images/users/avatar-1.jpg" alt="user-image" height="42" class="rounded-circle shadow-sm">
-                      <span class="leftbar-user-name mt-2">Dominic Keller</span>
-                  </a>
-              </div>
-
-              <!--- Sidemenu -->
-              <ul class="side-nav">
-                <li class="side-nav-item mt-2 background-buttons">
-                  <a data-bs-toggle="collapse" href="#" aria-expanded="false" aria-controls="sidebarDashboards" class="side-nav-link hovering-pan ">
-                      <i class="uil-dashboard"></i>
-                      <span> Dashboards </span>
-                  </a>
-              </li>
-              <li class="side-nav-item background-buttons">
-                  <a data-bs-toggle="collapse" href="#" aria-expanded="false" aria-controls="sidebarDashboards" class="side-nav-link hovering-pan ">
-                      <i class="uil uil-comment-message"></i>
-                      <span> Conversaciones </span>
-                  </a>
-              </li>
-              <li class="side-nav-item background-buttons">
-                <a data-bs-toggle="collapse" href="#" aria-expanded="false" aria-controls="sidebarDashboards" class="side-nav-link hovering-pan">
-                    <i class="mdi mdi-whatsapp"></i>
-                    <span> Saliente masivo </span>
-                </a>
-            </li>
-              <li class="side-nav-item background-buttons">
-                  <a data-bs-toggle="collapse" href="#" aria-expanded="false" aria-controls="sidebarDashboards" class="side-nav-link hovering-pan">
-                      <i class="uil-ticket"></i>
-                      <span> Tickets </span>
-                  </a>
-              </li>
-              <li class="side-nav-item position-absolute fixed-bottom background-buttons">
-                  <a data-bs-toggle="collapse" href="#" aria-expanded="false" aria-controls="sidebarDashboards" class="side-nav-link background-buttons">
-                      <i class="uil-exit"></i>
-                      <span> Cerrar sesión </span>
-                  </a>
-              </li>
-
-
-              </ul>
-              <!--- End Sidemenu -->
-
-              <div class="clearfix"></div>
-          </div></div></div></div><div class="simplebar-placeholder" style="width: auto; height: 1195px;"></div></div><div class="simplebar-track simplebar-horizontal" style="visibility: visible;"><div class="simplebar-scrollbar" style="width: 32px; display: block; transform: translate3d(0px, 0px, 0px);"></div></div><div class="simplebar-track simplebar-vertical" style="visibility: hidden;"><div class="simplebar-scrollbar" style="height: 0px; transform: translate3d(0px, 0px, 0px); display: none;"></div></div></div>
-      </div>
       <!-- ========== Left Sidebar End ========== -->
 
       <!-- ============================================================== -->
