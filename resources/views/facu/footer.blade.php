55b31f96d0a75beed92068edb7b3265a8d6deef5
luiscornejocornejo <kayser19992005@gmail.com>
2023-10-23 14:19:23 -0300

    asas

---
+++ resources/views/facu/footer.blade.php
@@ -0,0 +1,35 @@
+
+
+<a href="https://api.whatsapp.com/send?phone=TUNUMERO&text=Hola,%20quiero%20información" target="_blank" class="whatsapp">
+    <i class="mdi mdi-send" style="font-size: 25px;"></i>
+  </a>
+  <!-- Theme Settings -->
+
+
+  <!-- Vendor js -->
+  <script src="assetsfacu/js/vendor.min.js"></script>
+
+  <!-- Daterangepicker js -->
+  <script src="assetsfacu/vendor/daterangepicker/moment.min.js"></script>
+  <script src="assetsfacu/vendor/daterangepicker/daterangepicker.js"></script>
+
+  <!-- Apex Charts js -->
+  <script src="assetsfacu/vendor/apexcharts/apexcharts.min.js"></script>
+
+  <!-- Vector Map js -->
+  <script src="assetsfacu/vendor/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js"></script>
+  <script src="assetsfacu/vendor/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js"></script>
+
+  <!-- Dashboard App js -->
+  <script src="assetsfacu/js/pages/demo.dashboard.js"></script>
+
+  <!-- App js -->
+  <script src="assetsfacu/js/app.min.js"></script>
+
+
+<svg id="SvgjsSvg1001" width="2" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev" style="overflow: hidden; top: -100%; left: -100%; position: absolute; opacity: 0;"><defs id="SvgjsDefs1002"></defs><polyline id="SvgjsPolyline1003" points="0,0"></polyline><path id="SvgjsPath1004" d="M0 0 "></path></svg><div class="daterangepicker ltr single opensright"><div class="ranges"></div><div class="drp-calendar left single" style="display: block;"><div class="calendar-table"></div><div class="calendar-time" style="display: none;"></div></div><div class="drp-calendar right" style="display: none;"><div class="calendar-table"></div><div class="calendar-time" style="display: none;"></div></div><div class="drp-buttons"><span class="drp-selected"></span><button class="cancelBtn btn btn-sm btn-default" type="button">Cancel</button><button class="applyBtn btn btn-sm btn-primary" disabled="disabled" type="button">Apply</button> </div></div><div class="jvectormap-label"></div>
+
+
+
+</body>
+</html>
