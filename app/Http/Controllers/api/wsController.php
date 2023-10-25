7c59391029378b4f23710a9b15a62f395ed0b8c0
luiscornejocornejo <kayser19992005@gmail.com>
2023-10-25 15:01:47 -0300

    123

--- app/Http/Controllers/api/wsController.php
+++ app/Http/Controllers/api/wsController.php
@@ -485,11 +485,58 @@ class wsController extends Controller
 
       echo   $idticketestado=$request->idticketestado;
       echo   $statos=$request->statos;
-
+      echo   $idconv=$request->idconv;
+      echo   $idbot=$request->idbot;
+      
 
       $si2 = siennatickets::find($idticketestado);
       $si2->siennaestado=$statos;
       $si2->save();
+
+      if($statos==4){
+
+        $url="https://suricata4..com.ar/api/closechat";
+        $curl = curl_init();
+
+        // Prepare data array with account key, bot key, and account secret
+        $data = array(
+            "token" => "EDElDqlQf3RDP5EDK1pHhugV9M6aCXtwAm57SD0G5JYZjw7RxwZbbfdKMhWYdUUM",
+            "idbot" => $BOT_KEY,
+            "idconv" => $idconv
+        );
+  
+        // Set headers for the cURL request
+        $headers = array(
+            'Accept: application/json',
+            'Content-Type: application/json',
+            'Cookie: xenioo-id=Bearer+ZlHPzQ0ZfubwcHXAjjXMG0hDlJI22S1S0dqgKs0H7O06PghfV3BRy6Wxmn7PLb6RUmfIXRXiijo5X8E7%2flsAUV24IzaB28PYO%2bw90fEOTrp8Hx0WQCQ%2btq69lwpWUZpCg0ga2p%2bQD%2bI9KFMrCB6Ht%2bJM4ZOuekNf%2bYWtUBQ%2bm1prYPb8nDXWuRnU6qgtzr7zInbdRjyNhsdg41gTr7AstZ3sLt2wAXQS%2ba8zSGYe1UZY7gvoYm%2fGKj6TbvAdWnO0WXTVkwnB1jhMbWDX38PYGt2jkNoaUXRWxncuQSxJRzUIBWuTJGju%2b7EZOaoK07cXNk%2bUPBMSV1Q9gV6Gzc8CkA%3d%3d',
+        );
+           $url='https://suricata4..com.ar/api/closechat';
+     
+        // Set options for the cURL request
+        $options = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => json_encode($data),
+            CURLOPT_HTTPHEADER => $headers,
+        );
+  
+        // Set the options for cURL resource
+        curl_setopt_array($curl, $options);
+  
+        // Execute the cURL request
+        $response = curl_exec($curl);
+  
+        // Close the cURL resource
+        curl_close($curl);
+
+      }
       return redirect()
       ->back()
       ->with('success', 'Se modifico el estado  correctamente!');
