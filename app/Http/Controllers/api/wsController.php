3f8f79d8bf3ed465c72483bf0e04fe8e0fff4097
luiscornejocornejo <kayser19992005@gmail.com>
2023-10-25 15:27:41 -0300

    asa

--- app/Http/Controllers/api/wsController.php
+++ app/Http/Controllers/api/wsController.php
@@ -533,7 +533,7 @@ class wsController extends Controller
         // Execute the cURL request
         $response = curl_exec($curl);
   
-        dd($response);
+        dd($response);   
         // Close the cURL resource
         curl_close($curl);
 
