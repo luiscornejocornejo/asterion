056d846d4999a16b2b025744dffe88db69f36dbf
luiscornejocornejo <kayser19992005@gmail.com>
2023-10-27 19:56:17 -0300

    ss

--- resources/views/sienna/ticketssienna.blade.php
+++ resources/views/sienna/ticketssienna.blade.php
@@ -411,16 +411,15 @@ foreach ($datos as $item) {
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
 <div id="modalExample33" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="info-header-modalLabel" aria-hidden="true">
-    <div class="modal">
+    <div class="modal-dialog"  role="document">
         <div class="modal-content">
             <div class="modal-header bg-info">
                 <h4 class="modal-title text-white" id="info-header-modalLabel">cONVERSACION</h4>
                 <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-hidden="true"></button>
             </div>
             <div class="modal-body">
-            <div>
+           
                  <iframe id="vista" src="" sandbox="allow-forms allow-scripts allow-popups allow-same-origin allow-top-navigation" ></iframe>
-                 </div>
            
             
             </div>
