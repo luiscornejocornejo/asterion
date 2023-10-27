516b1b0996379b41f8a68558e1ff8baf773af1e1
luiscornejocornejo <kayser19992005@gmail.com>
2023-10-27 20:01:57 -0300

    asa

--- resources/views/sienna/ticketssienna.blade.php
+++ resources/views/sienna/ticketssienna.blade.php
@@ -29,7 +29,7 @@ img.style.transform = 'scale(1.9)';
 
     }
     function vista(dd) {
-        alert(dd);
+        
         document.getElementById("vista").contentWindow.document.location.href=dd;
         document.getElementById('vista').src = dd;
 
