a65d6abe59bd83009db977d35579dd68f04a93e4
luiscornejocornejo <kayser19992005@gmail.com>
2023-10-28 11:48:06 -0300

    sss

--- resources/views/facu/footer.blade.php
+++ resources/views/facu/footer.blade.php
@@ -39,6 +39,7 @@
 <script src="assetsfacu/vendor/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
 <script src="assetsfacu/vendor/datatables.net-select/js/dataTables.select.min.js"></script>
 
+<script src="assetsfacu/js/pages/demo.datatable-init.js"></script>
 
 <svg id="SvgjsSvg1001" width="2" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev" style="overflow: hidden; top: -100%; left: -100%; position: absolute; opacity: 0;"><defs id="SvgjsDefs1002"></defs><polyline id="SvgjsPolyline1003" points="0,0"></polyline><path id="SvgjsPath1004" d="M0 0 "></path></svg><div class="daterangepicker ltr single opensright"><div class="ranges"></div><div class="drp-calendar left single" style="display: block;"><div class="calendar-table"></div><div class="calendar-time" style="display: none;"></div></div><div class="drp-calendar right" style="display: none;"><div class="calendar-table"></div><div class="calendar-time" style="display: none;"></div></div><div class="drp-buttons"><span class="drp-selected"></span><button class="cancelBtn btn btn-sm btn-default" type="button">Cancel</button><button class="applyBtn btn btn-sm btn-primary" disabled="disabled" type="button">Apply</button> </div></div><div class="jvectormap-label"></div>
 
