f8f02b2379ba8493cdf965bc6bfa5c345e7277e9
luiscornejocornejo <kayser19992005@gmail.com>
2023-11-02 09:44:07 -0300

    ddd

--- resources/views/sienna/logeado.blade.php
+++ resources/views/sienna/logeado.blade.php
@@ -5,8 +5,12 @@ $urlfinalunico = session('urlxennio');
 
 
 $urlfinalunico=str_replace("conversation","logout",$urlfinalunico);
-$url = file_get_contents($urlfinalunico); 
+//$url = file_get_contents($urlfinalunico); 
 
     var_dump($urlfinalunico);
     echo $url;
+
+    use Illuminate\Support\Facades\Redirect;
+
+ Redirect::to($urlfinalunico);
     ?>
