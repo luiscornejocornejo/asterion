6a1a7a34a99c86aef5ca918aec26b720309fd7f6
luiscornejocornejo <kayser19992005@gmail.com>
2023-11-03 00:12:35 -0300

    ddd

--- app/Http/Controllers/ChauController.php
+++ app/Http/Controllers/ChauController.php
@@ -5,10 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Redirect;
+use App\Models\siennaloginxenioo;
+
 class ChauController extends Controller
 {
   public function logout()
   {
+
+    $idusuario=session('idusuario');
+    $xen = siennaloginxenioo::where('idusuario', '=', $idusuario)->get();       
+    $xen->login=2;
+    $xen->save();
     session()->forget('idusuario');
 
     
