ced85b6464bf8ae1943312a9c2d602a199ab949a
luiscornejocornejo <kayser19992005@gmail.com>
2023-11-03 00:32:20 -0300

    ggg

--- app/Observers/siennaticketsObserver.php
+++ app/Observers/siennaticketsObserver.php
@@ -1,8 +1,32 @@
 <?php
 
 namespace App\Observers;
+use App\Models\siennaloginxenioo;
 
 class siennaticketsObserver
 {
     //
+
+    public function created(siennatickets $siennatickets)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "updated" event.
+     *
+     * @param  \App\Models\siennatickets  $siennatickets
+     * @return void
+     */
+    public function updated(siennatickets $siennatickets)
+    {
+        //
+        $dato=$siennatickets->id;
+        $xen = new siennaloginxenioo();
+            $xen->idusuario=session('idusuario');
+            $xen->categoria=session('categoria');
+            $xen->login=$siennatickets->id;
+            $xen->save();
+
+    }
 }
