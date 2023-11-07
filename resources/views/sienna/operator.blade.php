7e45ecb10d3202958c436f48da278c16b7ec19ef
luiscornejocornejo <kayser19992005@gmail.com>
2023-11-07 14:10:30 -0300

    ddd

--- resources/views/sienna/operator.blade.php
+++ resources/views/sienna/operator.blade.php
@@ -63,7 +63,7 @@ function vista(dd) {
                                         }*/
 
                                         $bgcolor="success";
-                                    
+                                     
                                  
                                         
                                         ?>
