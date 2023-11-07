6169f634e868023792106ab95e720ed5c92a9963
luiscornejocornejo <kayser19992005@gmail.com>
2023-11-07 14:36:51 -0300

    ddd

--- resources/views/sienna/operator.blade.php
+++ resources/views/sienna/operator.blade.php
@@ -16,10 +16,18 @@
 
         
         url="https://opticom.suricata.cloud/api/statussiennaxdepto?depto=3";
-        const response = await fetch(url);
-    var data = await response.json();
-        alert($data);
-
+        axios.get(url)
+                .then(function (response) {
+                    // función que se ejecutará al recibir una respuesta
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    // función para capturar el error
+                    console.log(error);
+                })
+                .then(function () {
+                    // función que siempre se ejecuta
+                });
     
 
     }
