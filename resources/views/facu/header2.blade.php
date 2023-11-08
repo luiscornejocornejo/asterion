277c094f7b6ce532783d18ca5da365a4aab624da
luiscornejocornejo <kayser19992005@gmail.com>
2023-11-08 16:58:39 -0300

    ggg

--- resources/views/facu/header2.blade.php
+++ resources/views/facu/header2.blade.php
@@ -1,6 +1,7 @@
 <html  lang="en" data-bs-theme="light" data-layout-mode="fluid" data-menu-color="dark" data-topbar-color="light" data-layout-position="fixed" data-sidenav-size="condensed" class="menuitem-active"><head>
   <meta charset="utf-8">
   <title>Suricata Cx</title>
+  <meta http-equiv="refresh" content="60">
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description">
