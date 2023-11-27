5a50e246a444f5969fd1af107e981708d7a7011d
luiscornejocornejo <kayser19992005@gmail.com>
2023-11-27 00:35:13 -0300

    hhh

---
+++ resources/views/sienna/ticketsmodals/departamentos.blade.php
@@ -0,0 +1,34 @@
+<div id="bs-example-modal-sm2" class="modal fade bs-example-modal-center " tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header bg-dark text-white">
+                    <h5 class="modal-title">Cambiar Depto</h5>
+                    <button type="button" class="btn-close text-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="/api/cambiardeptosienna" method="post">
+                        <input type="hidden" name="_token" v-bind:value="csrf">
+                        <input value="" type="hidden" name="idconv" id="idconv">
+                        <input value="" type="hidden" name="user_id" id="user_id">
+                        <input value="<?php echo $subdomain_tmp;?>" type="hidden" name="idbot" id="idbot">
+                        <input value=""  type="hidden" name="idticketdepto" id="idticketdepto">
+                        <input value="" type="hidden" name="bot_channel" id="bot_channel">
+  
+                        <input  type="hidden" name="merchant" id="merchant" value=" <?php echo $subdomain_tmp;?> ">
+
+                        <div v-for="department in departments ">
+                        <?php foreach($deptos as $dep){?>
+                            <input value="<?php echo $dep->id;?>" class="form-radio" type="radio" name="statos">&nbsp;
+                        <span class=" fw-bold" style="color: #98a6ad;font-size: 12px;"><?php echo$dep->nombre;?></span>
+                            <br><br>
+
+                            <?php }?>   
+                                             </div>
+                        <button type="submit" class="btn btn-success
+                                    waves-effect waves-light">Cambiar</button>
+
+                    </form>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div>
