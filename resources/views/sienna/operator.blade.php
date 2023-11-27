6f9c5902efa79343b6f002ebbab5322387f1ba3f
luiscornejocornejo <kayser19992005@gmail.com>
2023-11-27 00:43:09 -0300

    hgggg

--- resources/views/sienna/operator.blade.php
+++ resources/views/sienna/operator.blade.php
@@ -230,18 +230,9 @@ function vista(dd) {
           <!-- content -->
       </div>
       <!-- Modal of conversation-->
-      <div>
-        <div class="modal fade" id="bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg">
-                <div class="modal-content">
-                   
-                    <div class="modal-body">
-                        <iframe id="vista" src="" width="100%" height="800px" class="border rounded-3" style="height:400px !important"></iframe>
-                    </div>
-                </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
-        </div>
-      </div>
+      @include('sienna.ticketsmodals.coversacion')
+
+      
       <!-- End Modal -->
       
       <!-- Small modal Status-->
---
+++ resources/views/sienna/ticketsmodals/conversacion.blade.php
@@ -0,0 +1,12 @@
+<div>
+        <div class="modal fade" id="bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                   
+                    <div class="modal-body">
+                        <iframe id="vista" src="" width="100%" height="800px" class="border rounded-3" style="height:400px !important"></iframe>
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div>
+      </div>
