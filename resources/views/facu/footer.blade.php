55bcb51a7c50841f008da0336e5628fa2de3a4e0
luiscornejocornejo <kayser19992005@gmail.com>
2023-12-04 19:41:21 -0300

    ddd

--- resources/views/facu/footer.blade.php
+++ resources/views/facu/footer.blade.php
@@ -13,9 +13,12 @@
   <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
-
+    <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap5.min.js"></script>
     <script type="text/javascript">
    new DataTable('#example', {
+    "responsive": true,
+
   "language" : {
     "url": "//cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json"
   }
