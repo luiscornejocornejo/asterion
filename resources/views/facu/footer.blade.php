7a2f1790859190700173b948f40bfb340e412318
luiscornejocornejo <kayser19992005@gmail.com>
2023-12-04 12:24:02 -0300

    ddd

--- resources/views/facu/footer.blade.php
+++ resources/views/facu/footer.blade.php
@@ -25,6 +25,15 @@
 <script src="assetsfacu/vendor/datatables.net-bs5/js/dataTables.bootstrap5.min.js"></script>
 <script src="assetsfacu/vendor/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
 <script src="assetsfacu/vendor/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js"></script>
+<script src="assetsfacu/vendor/datatables.net-fixedcolumns-bs5/js/fixedColumns.bootstrap5.min.js"></script>
+<script src="assetsfacu/vendor/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
+<script src="assetsfacu/vendor/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+<script src="assetsfacu/vendor/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js"></script>
+<script src="assetsfacu/vendor/datatables.net-buttons/js/buttons.html5.min.js"></script>
+<script src="assetsfacu/vendor/datatables.net-buttons/js/buttons.flash.min.js"></script>
+<script src="assetsfacu/vendor/datatables.net-buttons/js/buttons.print.min.js"></script>
+<script src="assetsfacu/vendor/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
+<script src="assetsfacu/vendor/datatables.net-select/js/dataTables.select.min.js"></script>
 
 <script src="assetsfacu/js/pages/demo.datatable-init.js"></script>
 
