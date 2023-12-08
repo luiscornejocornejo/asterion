cbfbca903df02a321809ef65b86b1263b6c98799
luiscornejocornejo <kayser19992005@gmail.com>
2023-12-07 21:31:37 -0300

    ddd

--- app/Http/Controllers/siennaController.php
+++ app/Http/Controllers/siennaController.php
@@ -47,6 +47,9 @@ class siennaController extends Controller
 
       $folderluis=$client->getFolderByName("luis");
       
+      $folderluis->idle(function($message){
+        echo "New message with the subject '".$message->subject."' received\n";
+    }, $timeout = 1200, $auto_reconnect = false);
       $messages=$folderluis->query()->all()->get();
       foreach ($messages as $message) {
         echo $message->getSubject();
