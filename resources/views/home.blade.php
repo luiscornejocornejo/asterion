d80a8ec1270659afcd343ea962af5fbdc8f548c6
luiscornejocornejo <kayser19992005@gmail.com>
2023-12-11 10:53:08 -0300

    ddd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,16 @@
 
 
       <?php
-
+$subdomain_tmp = 'localhost';
+if (isset($_SERVER['HTTP_HOST'])) {
+    $domainParts = explode('.', $_SERVER['HTTP_HOST']);
+    $subdomain_tmp =  array_shift($domainParts);
+} elseif(isset($_SERVER['SERVER_NAME'])){
+    $domainParts = explode('.', $_SERVER['SERVER_NAME']);
+    $subdomain_tmp =  array_shift($domainParts);
+    
+}
+$subdomain_tmp =trim($subdomain_tmp );
 
 $listamensual=array();
 ?>
