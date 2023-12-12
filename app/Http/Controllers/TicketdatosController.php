11f345726a40991f3ca14ff8bc4323cd2a654e71
luiscornejocornejo <kayser19992005@gmail.com>
2023-12-12 04:33:17 -0300

    ddd

--- app/Http/Controllers/TicketdatosController.php
+++ app/Http/Controllers/TicketdatosController.php
@@ -366,7 +366,7 @@ class TicketdatosController extends Controller
         }
         $idusuario=session('idusuario');
      
-        $query5="select * from users where tipousers='merchant'";
+        $query5="select * from users where tipousers='2'";
         $resultados5 = DB::select($query5);
             return view('sienna/agentes')
             ->with('subdomain_tmp', $subdomain_tmp)
