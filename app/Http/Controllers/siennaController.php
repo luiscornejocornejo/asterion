80345ea449f174179a24cac90845ea4ef224aef7
luiscornejocornejo <kayser19992005@gmail.com>
2023-12-12 00:36:14 -0300

    ddd

--- app/Http/Controllers/siennaController.php
+++ app/Http/Controllers/siennaController.php
@@ -88,11 +88,23 @@ class siennaController extends Controller
     echo   $query1="
 
 
-    CREATE TABLE IF NOT EXISTS  ".$val->Database.".siennasource (
+    CREATE TABLE IF NOT EXISTS  ".$val->Database.".siennatickets (
       id int auto_increment NOT NULL,
-      nombre varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
-      svg text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
-      colore varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+      siennadepto smallint NULL,
+      cliente varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+      siennatopic smallint NULL,
+      siennaestado smallint NULL,
+      siennasource varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+      created_at datetime NULL,
+      updated_at datetime NULL,
+      t_cerrado datetime NULL,
+      cel varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+      nya varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+      conversation_url varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+      conversation_id varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+      cedula varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+      user_id varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+      asignado int NULL,
       CONSTRAINT `PRIMARY` PRIMARY KEY (id)
     )
     ENGINE=InnoDB
