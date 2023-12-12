79461273ca0377f38ad2ece9edccdd7e362c4b17
luiscornejocornejo <kayser19992005@gmail.com>
2023-12-12 03:55:08 -0300

    sss

--- app/Http/Controllers/ChauController.php
+++ app/Http/Controllers/ChauController.php
@@ -12,38 +12,34 @@ class ChauController extends Controller
   public function logout()
   {
 
-    $idusuario=session('idusuario');
+    $idusuario = session('idusuario');
 
-          $subdomain_tmp = 'localhost';
-            if (isset($_SERVER['HTTP_HOST'])) {
-                $domainParts = explode('.', $_SERVER['HTTP_HOST']);
-                $subdomain_tmp =  array_shift($domainParts);
-            } elseif(isset($_SERVER['SERVER_NAME'])){
-                $domainParts = explode('.', $_SERVER['SERVER_NAME']);
-                $subdomain_tmp =  array_shift($domainParts);
-                
-            }
+    $subdomain_tmp = 'localhost';
+    if (isset($_SERVER['HTTP_HOST'])) {
+      $domainParts = explode('.', $_SERVER['HTTP_HOST']);
+      $subdomain_tmp =  array_shift($domainParts);
+    } elseif (isset($_SERVER['SERVER_NAME'])) {
+      $domainParts = explode('.', $_SERVER['SERVER_NAME']);
+      $subdomain_tmp =  array_shift($domainParts);
+    }
 
-            if($subdomain_tmp=="opticom"){
+    $idxen = 0;
+    $xen = siennaloginxenioo::where('idusuario', '=', $idusuario)->get();
+    foreach ($xen as $val) {
 
-                  $idxen=0;
-                  $xen = siennaloginxenioo::where('idusuario', '=', $idusuario)->get();  
-                  foreach($xen as $val){
+      $idxen = $val->id;
+    }
+    if ($idxen <> 0) {
+      $xen2 = siennaloginxenioo::find($idxen);
+      $xen2->login = 2;
+      $xen2->save();
+    }
 
-                    $idxen=$val->id;
-                  }     
-                  if($idxen<>0){
-                    $xen2=siennaloginxenioo::find($idxen); 
-                    $xen2->login=2;
-                    $xen2->save();
-                  }
-                  
 
-            }
+
     session()->forget('idusuario');
 
-    
+
     return Redirect::to(env('APP_URL'));
-    
   }
 }
