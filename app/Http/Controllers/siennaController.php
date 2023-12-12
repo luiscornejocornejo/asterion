ccd5cbc374544e4a511f03d28a4a9b7f3103359e
luiscornejocornejo <kayser19992005@gmail.com>
2023-12-12 00:27:18 -0300

    hhh

--- app/Http/Controllers/siennaController.php
+++ app/Http/Controllers/siennaController.php
@@ -99,9 +99,10 @@ class siennaController extends Controller
       DEFAULT CHARSET=utf8mb4
       COLLATE=utf8mb4_0900_ai_ci
       COMMENT='';";
+      $resultados1 = DB::select($query1);
+
     }
 
-    $resultados1 = DB::select($query1);
 
 
   }
