2d71c722a4e478d5f34041990ad3900ba40c7399
luiscornejocornejo <kayser19992005@gmail.com>
2023-12-14 17:31:00 -0300

    eee

--- app/Http/Controllers/TicketdatosController.php
+++ app/Http/Controllers/TicketdatosController.php
@@ -698,7 +698,7 @@ class TicketdatosController extends Controller
         echo   $estado=$request->estado;
 
         $idbot=$request->idbot;
-        $idconv=$request->conversation_id2;
+        $idconv=$request->idconv;
         $bot_channel="WhatsAppChannel";
 
         if($estado==4){
@@ -741,7 +741,7 @@ class TicketdatosController extends Controller
             // Execute the cURL request
             $response = curl_exec($curl);
       
-            dd($response);   
+           // dd($response);   
             // Close the cURL resource
             curl_close($curl);
     
