ecde26a0f721561a535df8de17618197c6856db1
luiscornejocornejo <kayser19992005@gmail.com>
2023-12-14 17:25:43 -0300

    rrr

--- app/Http/Controllers/TicketdatosController.php
+++ app/Http/Controllers/TicketdatosController.php
@@ -741,7 +741,7 @@ class TicketdatosController extends Controller
             // Execute the cURL request
             $response = curl_exec($curl);
       
-            //dd($response);   
+            dd($response);   
             // Close the cURL resource
             curl_close($curl);
     
