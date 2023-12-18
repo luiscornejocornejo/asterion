3831d9fc2684af40f4849445c7596a0d2c59dc09
luiscornejocornejo <kayser19992005@gmail.com>
2023-12-18 00:54:03 -0300

    ddd

--- app/Http/Controllers/siennaController.php
+++ app/Http/Controllers/siennaController.php
@@ -86,10 +86,10 @@ class siennaController extends Controller
           continue;
       }
     echo   $query1="
-    DROP TABLE ".$val->Database.".t_cliente; ";
+    DROP TABLE ".$val->Database.".t_detalleticket; ";
    /* DROP TABLE ".$val->Database.".;
     DROP TABLE ".$val->Database.".;
-    DROP TABLE ".$val->Database.".t_detalleticket;
+    DROP TABLE ".$val->Database.".;
     DROP TABLE ".$val->Database.".t_extraticket;
     DROP TABLE ".$val->Database.".t_prioridad;
     DROP TABLE ".$val->Database.".t_staff;
