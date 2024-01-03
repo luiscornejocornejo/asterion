2fd4f7a21fc9ee398bd7f977cfa8def6b874bba2
luiscornejocornejo <kayser19992005@gmail.com>
2024-01-03 11:13:57 -0300

    eee

--- resources/views/sienna/ticketsmodals/conversacion.blade.php
+++ resources/views/sienna/ticketsmodals/conversacion.blade.php
@@ -10,3 +10,56 @@
             </div><!-- /.modal-dialog -->
         </div>
       </div>
+      <div class="card widget-flat">
+  <div class="card-body">
+    <div class="d-flex justify-content-between">
+      <div>
+        <h4 class="fw-normal text-dark" title="Number of Customers">
+          Información de usuario
+        </h4>
+      </div>
+      <div>
+        <i
+          class="mdi mdi-card-account-details widget-icon bg-secondary-lighten text-secondary"
+        ></i>
+      </div>
+    </div>
+    <hr style="margin-top: 10px" />
+    <div class="d-flex mt-2">
+      <i class="mdi mdi-card-account-details"></i>&nbsp;Numero cliente:&nbsp;
+      <span class="badge badge-secondary-lighten line-h"> {{cedula}} </span>
+    </div>
+    <div class="d-flex mt-2">
+      <i class="mdi mdi-account"></i>&nbsp;Nombre:&nbsp;
+      <span class="badge badge-secondary-lighten hover-overlay line-h">
+        Pepito
+      </span>
+    </div>
+    <div class="d-flex mt-2">
+      <i class="mdi mdi-home"></i>&nbsp;Domicilio:&nbsp;
+      <span class="badge badge-secondary-lighten line-h">
+        Calle Falsa 123
+      </span>
+    </div>
+    <div class="d-flex mt-2">
+      <i class="mdi mdi-whatsapp text"></i>&nbsp;Whatsapp:&nbsp;
+      <span class="badge badge-secondary-lighten line-h"> 5491122334455 </span>
+    </div>
+    <div class="d-flex mt-2">
+      <i class="mdi mdi-email"></i>&nbsp;Email:&nbsp;
+      <span class="badge badge-secondary-lighten line-h">
+        pepito@gmail.com
+      </span>
+    </div>
+    <div class="d-flex mt-2">
+      <i class="mdi mdi-account-cash"></i>&nbsp;Estado de cuenta:&nbsp;
+      <span class="badge badge-success-lighten line-h"> Normal </span>
+      <span class="badge badge-warning-lighten line-h ms-1"> Suspendido </span>
+    </div>
+    <div class="d-flex mt-2">
+      <i class="mdi mdi-antenna"></i>&nbsp;Estado de servicio:&nbsp;
+      <span class="badge badge-success-lighten line-h"> Normal </span>
+      <span class="badge badge-danger-lighten line-h ms-1"> Masivo </span>
+    </div>
+  </div>
+</div>
