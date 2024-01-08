78a141782fd98f00cc6535dc47b32003a419f425
luiscornejocornejo <kayser19992005@gmail.com>
2024-01-08 15:36:19 -0300

    hhh

--- resources/views/sienna/abm.blade.php
+++ resources/views/sienna/abm.blade.php
@@ -179,15 +179,26 @@
         <i class="mdi mdi-plus" style="font-size: 25px;"></i>
       </div>
 
-      <div id="create-user-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="dark-header-modalLabel" aria-hidden="true">
+<!-- Modal for Create Ticket -->
+<div id="create-user-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="dark-header-modalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header bg-dark">
                 <h4 class="modal-title text-light" id="dark-header-modalLabel">Añadir usuario</h4>
                 <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-hidden="true"></button>
-            </div
-</div><!-- /.modal-content -->
+            </div>
+            <div class="modal-body">
+                <form>                                                                                 
+                   <iframe src="https://infitelecom.suricata.cloud/siennacreate?report=135" class="w-100 h-100"></iframe>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light border" data-bs-dismiss="modal">Cancelar</button>
+                <button type="submit" class="btn btn-success">Crear</button>
+            </div>
+        </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div>
+<!-- End of modal Create Ticket -->
 <br><br><br>
 @include('facu.footer')
