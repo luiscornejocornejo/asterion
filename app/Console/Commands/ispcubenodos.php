265d1c92433a826977dff6f29b7b4407b888ac53
luiscornejocornejo <kayser19992005@gmail.com>
2024-01-22 14:52:32 -0300

    ddd

--- app/Console/Commands/ispcubenodos.php
+++ app/Console/Commands/ispcubenodos.php
@@ -44,7 +44,7 @@ class ispcubenodos extends Command
           if (in_array($val->Database, $listadono)) {
               continue;
           }
-        echo $query1="select count(*) as cuantos from ".$val->Database.".siennaintegracion where nombre='ispcube' ";
+         $query1="select count(*) as cuantos from ".$val->Database.".siennaintegracion where nombre='ispcube' ";
        
             $resultados1 = DB::select($query1);
 
