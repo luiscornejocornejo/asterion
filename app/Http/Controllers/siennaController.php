bcb8ac3d588124b3893813e184e20b487cbc910c
luiscornejocornejo <kayser19992005@gmail.com>
2024-01-26 19:05:54 -0300

    ddd

--- app/Http/Controllers/siennaController.php
+++ app/Http/Controllers/siennaController.php
@@ -88,7 +88,7 @@ echo $response;
       
      
       $messages=$folderluis->query()->all()->get();
-      //dd($messages);
+      dd($messages);
       $vueltas=0;
       foreach ($messages as $message) {
         echo $message->getSubject();
