0a154e8af38145e39097d12b174f29e695bfd013
luiscornejocornejo <kayser19992005@gmail.com>
2024-01-27 02:59:12 -0300

    eee

--- app/Console/Commands/mailtickets.php
+++ app/Console/Commands/mailtickets.php
@@ -7,11 +7,9 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Mail;
 use App\Models\empresa;
-use App\Models\siennatickets;
+use App\Models\siennaticketssoporte;
 use Webklex\PHPIMAP\ClientManager;
 use Webklex\PHPIMAP\Client;
-use App\Models\siennaseguimientos;
-
 class mailtickets extends Command
 {
     /**
@@ -88,28 +86,31 @@ class mailtickets extends Command
            //crear ticket en sienna
 
            
-            $si = new siennatickets();
-            $si->siennadepto = "1";
-            $si->cliente = "";
-            $si->siennatopic = "2";
-            $si->cedula = "";
-            $si->siennaestado = "1";
-            $si->siennasource = "7";
-            $si->cel ="" ;
-            $si->nya = $asunto;
-            $si->asignado = 0;
-            $si->user_id = "";
-            $si->conversation_url = "";
-            $si->conversation_id = "";
-            $si->save();
+        $si = new siennaticketssoporte();
+        $si->siennadepto = "1";
+        $si->cliente = "";
+        $si->siennatopic = "2";
+        $si->cedula = "";
+        $si->siennaestado = "1";
+        $si->siennasource = "7";
+        $si->cel = "";
+        $si->nya = $asunto;
+        $si->asignado = 0;
+        $si->user_id = "";
+        $si->conversation_url = "";
+        $si->conversation_id = "";
+        $si->save();
+/*
+        $se = new siennaseguimientos();
+        $se->ticket = $si->id;
+        $se->tipo = "6";
+        $se->descripcion = $nya;
+        $se->autor = "sistema";
+        $se->save();
 
-            $se = new siennaseguimientos();
-            $se->ticket = $si->id;
-            $se->tipo = "6";
-            $se->descripcion = $nya;
-            $se->autor = "sistema";
-            $se->save();
-
+           $querye="INSERT INTO soporte.siennatickets ( siennadepto, cliente, siennatopic, siennaestado, siennasource, created_at, updated_at, t_cerrado, cel, nya, conversation_url, conversation_id, cedula, user_id, asignado)
+            VALUES( 1, '', 1, 1, '3', now(), now(), now(), '0', '".$nya."', '', '', '', '', 0);";
+            $resultados1 = DB::select($querye);*/
             //$message = $message->move($folder_path = "luisleidos");
            $vueltas++;
            if($vueltas==5){
