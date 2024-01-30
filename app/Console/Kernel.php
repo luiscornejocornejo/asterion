16526d9a6484c1905d1c56852119ead42a58e177
luiscornejocornejo <kayser19992005@gmail.com>
2024-01-30 12:16:28 -0300

    fff

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
 
     protected function schedule(Schedule $schedule)
-    {
+    { 
         // $schedule->command('inspire')->hourly();
         $schedule->command('semanal:asignacion')->cron('*/1 * * * *')->withoutOverlapping()->emailOutputOnFailure('kayser1712@gmail.com');
         //$schedule->command('semanal:nodos')->daily()->emailOutputOnFailure('kayser1712@gmail.com');
