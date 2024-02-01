a092eb990dd0677d7f6894f6496828551c52a177
luiscornejocornejo <kayser19992005@gmail.com>
2024-02-01 18:39:36 -0300

    rrr

--- app/Http/Controllers/TicketdatosController.php
+++ app/Http/Controllers/TicketdatosController.php
@@ -1326,7 +1326,7 @@ class TicketdatosController extends Controller
     }
     public function nodes(Request $request)
     {
-        $query = "SELECT *,b.nombre as nombredelestadonodo,a.nombre as nombredelnodo  FROM `nodos` a join estadonodo b
+        $query = "SELECT *,b.nombre as nombredelestadonodo,a.nombre as nombredelnodo,a.id idnodo  FROM `nodos` a join estadonodo b
         on  b.id=a.estadonodo";
         $nodes = DB::select($query);
 
