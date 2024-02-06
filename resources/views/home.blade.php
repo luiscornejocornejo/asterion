9f55db4e639fab3e0d4392d7d609c3c3e5671380
luiscornejocornejo <kayser19992005@gmail.com>
2024-02-06 05:47:16 -0300

    eee

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -97,7 +97,7 @@ if(isset($_GET['fecha'])){
                     var URLactual = window.location.href;
                     var porciones = URLactual.split('.');
                     let result = porciones[0].replace("https://", "");
-                    alert(start);
+                    let ticketcerrados=0;
                     url = "https://"+result+".suricata.cloud/api/cerradoscant?ini=" + start + "&fin=" + endDate + "";
                     axios.get(url)
                     .then(function (response) {
