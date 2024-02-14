b2e2f8fe92c0a22dff303fe0115dc00c22bb3124
luiscornejocornejo <kayser19992005@gmail.com>
2024-02-14 00:46:34 -0300

    dddd

--- resources/views/facu/footer.blade.php
+++ resources/views/facu/footer.blade.php
@@ -15,13 +15,27 @@
     <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap5.min.js"></script>
+
+
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
+
+
     <script type="text/javascript">
    new DataTable('#example', {
     "responsive": true,
 
   "language" : {
     "url": "//cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json"
-  }
+  },
+  dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
 }
 )
     </script>
