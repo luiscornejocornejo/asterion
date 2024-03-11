e10ed12dc8cae7e6e4cb101ed93701d5965877c4
luiscornejocornejo <kayser19992005@gmail.com>
2024-03-11 20:27:08 -0300

    eee

--- app/Models/linknetclientes.php
+++ app/Models/linknetclientes.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class base extends Model
+class linknetclientes extends Model
 {
     protected $table = 'linknetclientes';
 }
