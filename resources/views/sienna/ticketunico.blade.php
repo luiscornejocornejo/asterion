96d5e47a53e6f2893df7e0579bb689f7efa9d21b
luiscornejocornejo <kayser19992005@gmail.com>
2024-03-13 20:43:52 -0300

    rrr

--- resources/views/sienna/ticketunico.blade.php
+++ resources/views/sienna/ticketunico.blade.php
@@ -174,6 +174,10 @@
                         <form action="/api/siennacrearseguimiento2" method="POST"  enctype="multipart/form-data">
 
                                 <div class="mt-2 ">
+                                <div>
+                                        <label class="form-label">Subir archivo</label>
+                                        <input name="logo" class="form-control" type="file" id="inputGroupFile04">
+                                    </div>
                                     <div class="mb-2">
                                         <label class="form-label">Nota interna</label>
                                         <div class="input-group">
@@ -181,13 +185,10 @@
 
                                         <input value="<?php echo $resultados[0]->ticketid;?>" type="hidden" name="idticketseguimiento" id="idticketseguimiento">
                                             <input name="comentario" type="text" class="form-control" aria-label="Recipient's username">
-                                            <button class="btn btn-primary" type="button"><i class="mdi mdi-send"></i></button>
+                                            <button class="btn btn-primary" type="submit"><i class="mdi mdi-send"></i></button>
                                         </div>
                                     </div>
-                                    <div>
-                                        <label class="form-label">Subir archivo</label>
-                                        <input name="logo" class="form-control" type="file" id="inputGroupFile04">
-                                    </div>
+                                    
                                 </div>
                         </form>
                             <div class="mt-2">
