e98b21f2c7892da9568e4d88c72255fd660a5ccd
luiscornejocornejo <kayser19992005@gmail.com>
2024-03-13 14:33:10 -0300

    kkk

--- resources/views/sienna/ticketunico.blade.php
+++ resources/views/sienna/ticketunico.blade.php
@@ -10,7 +10,7 @@
      
 
    
- <iframe allow="camera;microphone"  src="<?php  echo $resultados[0]->conversation_url;?> " width="100%" height="800px" class="border rounded-3" style="height:400px !important"></iframe>
+ <iframe allow="camera;microphone"  src="<?php  echo $resultados[0]->conversation_url;?> " width="100%" height="800px" class="border rounded-3" style="height:600px !important"></iframe>
   
    
 </div> 
