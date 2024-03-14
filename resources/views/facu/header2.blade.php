cbf55c7d90d1af0374ee88e644bc6f78b12d334a
luiscornejocornejo <kayser19992005@gmail.com>
2024-03-14 11:28:47 -0300

    sss

--- resources/views/facu/header2.blade.php
+++ resources/views/facu/header2.blade.php
@@ -74,12 +74,6 @@ z-index: 999;}
     </style>
 
 <script src="sienna/js/5tickets.js"></script>
-<script data-id='xenioo' data-node='cm91dGluZy54ZW5pb28uY29t' src="https://res.xenioo.com/plugin/xenioo.js"></script>
-<script>
-xenioowebchat.EmbedChatLanding({
-container: 'xenioowebchat',
-landingUrl: 'https://res.xenioo.com/landing//display.html?route=cm91dGluZy54ZW5pb28uY29t&bot_key=sUccEJjrHUiJ...'
-});
-</script>
+
 </head>
 <body class="show" onload="maxid()">
