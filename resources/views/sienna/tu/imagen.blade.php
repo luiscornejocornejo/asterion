963f41a5c81dc308d875359fa67834b109f27b0a
luiscornejocornejo <kayser19992005@gmail.com>
2024-03-15 16:50:32 -0300

    ddd

--- resources/views/sienna/tu/imagen.blade.php
+++ resources/views/sienna/tu/imagen.blade.php
@@ -1,7 +1,7 @@
 
 
 
-<div id="standard-modal-img" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="dark-header-modalLabel" aria-hidden="true">
+<div id="bs-example-modal-img" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="dark-header-modalLabel" aria-hidden="true">
            <div class="modal-dialog ">
                 <div class="modal-content">
                     <div class="modal-header bg-dark">
