6060477ab9782cc1bcc1d933396ecd5603d8b9e1
luiscornejocornejo <kayser19992005@gmail.com>
2024-03-15 15:57:08 -0300

    eee

--- resources/views/sienna/ticketunico.blade.php
+++ resources/views/sienna/ticketunico.blade.php
@@ -488,6 +488,19 @@ function ng(ruta) {
     <br><br><br>
   
     
-
+    <div class="modal fade" id="bs-example-modal-img" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-sm">
+                <div class="modal-content">
+                    <div class="modal-headerbg-dark text-white">
+                        hola
+                   
+                    </div>
+                    <div class="modal-body">
+                    <div class="modal-body" id="vista2">
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+</div>
+</div>
    
     @include('facu.footer')
