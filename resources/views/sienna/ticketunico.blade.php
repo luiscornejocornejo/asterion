4249a1552dc7cd0474edfcbdb1f3002ab587dae5
luiscornejocornejo <kayser19992005@gmail.com>
2024-03-15 15:05:04 -0300

    dd

--- resources/views/sienna/ticketunico.blade.php
+++ resources/views/sienna/ticketunico.blade.php
@@ -187,7 +187,7 @@ function cerrar(result,dd, ee, ff,cliente){
                                     </span>
                                   </div>
                                 <div class="d-flex mt-2">
-                                    <i class="mdi mdi-map-marker"></i>&nbsp;Deuda:&nbsp;
+                                    <i class="mdi mdi-currency-usd"></i>&nbsp;Deuda:&nbsp;
                                     <span class="badge badge-secondary-lighten line-h">
                                     <?php  echo $resultados[0]->deuda;?>
                                     </span>
