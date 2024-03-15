63dc50b2ea9599608f399f840e215c5e8d92e37a
luiscornejocornejo <kayser19992005@gmail.com>
2024-03-15 16:27:13 -0300

    eee

--- resources/views/sienna/ticketunico.blade.php
+++ resources/views/sienna/ticketunico.blade.php
@@ -73,6 +73,46 @@ function cerrar(result,dd, ee, ff,cliente){
                 </div>
 
             </div>
+
+  <!-- Small modal Status-->
+  <div class="modal fade" id="bs-example-modal-sm-assign" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-sm">
+                            <div class="modal-content">
+                                <div class="modal-header bg-dark text-white">
+                                    <h4 class="modal-title" id="mySmallModalLabel">Asignar Ticket</h4>
+                                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-hidden="true"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="/api/pedir2" method="POST">
+                                        <input value="<?php echo $resultados[0]->ticketid;?>" type="hidden" name="idticketpedir" id="idticketpedir2">
+                                        <input value="<?php echo session('nombreusuario');?>" type="hidden" name="logeado" id="logeado">
+                                        
+                                            <div class="mt-3">
+                                                
+                                                <div v-for="department in departments ">
+                                                    <?php foreach($usersmerchant as $dep){?>
+                                                        <input value="<?php echo $dep->id;?>" class="form-radio" type="radio" name="usuarioticket">&nbsp;
+                                                    <span class=" fw-bold" style="color: #98a6ad;font-size: 12px;"><?php echo$dep->nombre." ".$dep->last_name;?></span>
+                                                        <br><br>
+
+                                                    <?php }?>   
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-light" data-bs-dismiss="modal">Cancelar</button>
+                                            <button type="submit" class="btn btn-success">Asignar</button>
+                                                </form>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div><!-- /.modal-content -->
+                        </div><!-- /.modal-dialog -->
+        </div>
+      <!-- End modal Status -->
+
+
             <div class="row">
                 <div class="col-sm-12 col-lg-8 col-xxl-9">
                     <div class="mt-2">
@@ -285,43 +325,7 @@ function cerrar(result,dd, ee, ff,cliente){
       </div>
       
       
-      <!-- Small modal Status-->
-       <div class="modal fade" id="bs-example-modal-sm-assign" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-sm">
-                            <div class="modal-content">
-                                <div class="modal-header bg-dark text-white">
-                                    <h4 class="modal-title" id="mySmallModalLabel">Asignar Ticket</h4>
-                                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-hidden="true"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <form action="/api/pedir2" method="POST">
-                                        <input value="<?php echo $resultados[0]->ticketid;?>" type="hidden" name="idticketpedir" id="idticketpedir2">
-                                        <input value="<?php echo session('nombreusuario');?>" type="hidden" name="logeado" id="logeado">
-                                        
-                                            <div class="mt-3">
-                                                
-                                                <div v-for="department in departments ">
-                                                    <?php foreach($usersmerchant as $dep){?>
-                                                        <input value="<?php echo $dep->id;?>" class="form-radio" type="radio" name="usuarioticket">&nbsp;
-                                                    <span class=" fw-bold" style="color: #98a6ad;font-size: 12px;"><?php echo$dep->nombre." ".$dep->last_name;?></span>
-                                                        <br><br>
-
-                                                    <?php }?>   
-                                                </div>
-                                            </div>
-                                            
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-light" data-bs-dismiss="modal">Cancelar</button>
-                                            <button type="submit" class="btn btn-success">Asignar</button>
-                                                </form>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div><!-- /.modal-content -->
-                        </div><!-- /.modal-dialog -->
-        </div>
-      <!-- End modal Status -->
+    
         <!-- Departament modal Status-->
        
       <!-- End modal Status -->
@@ -389,7 +393,8 @@ function cerrar(result,dd, ee, ff,cliente){
             </div>
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
+</div>
+<!-- /.modal -->
       
       <!-- End Reclamo Ticket -->
     
