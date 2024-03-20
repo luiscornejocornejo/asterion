a2a9e0ac0fd0ff29e5b2539192cb4be0122cfd88
luiscornejocornejo <kayser19992005@gmail.com>
2024-03-20 10:18:32 -0300

    555

--- app/Console/Commands/asignacion.php
+++ app/Console/Commands/asignacion.php
@@ -34,7 +34,7 @@ class asignacion extends Command
     {
 
         echo "entro asignacion";
-        $os = array("infitelecom", "soporte", "demo", "sidecom", "amecom2", "ispgroup", "futurity", "wiredcom", "wiber", "gandalf", "onfiber", "fibercom", "cyminternet","airfibra");
+        $os = array("infitelecom", "soporte", "demo", "sidecom", "amecom2", "ispgroup", "futurity", "wiredcom", "wiber", "gandalf", "onfiber", "fibercom", "cyminternet","airfibra","quierofibra");
 
         foreach($os as $val){
 
