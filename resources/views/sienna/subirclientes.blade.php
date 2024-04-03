9159dd5fa88124d326e8cf3e67f3dc5b544befb7
luiscornejocornejo <kayser19992005@gmail.com>
2024-04-03 18:47:04 -0300

    rrr

--- resources/views/sienna/subirclientes.blade.php
+++ resources/views/sienna/subirclientes.blade.php
@@ -36,19 +36,20 @@
                     <thead class=" bg-dark">
                         <tr class="text-center">
                             <th class="text-light">codigo</th>
-                            <th class="text-light">saldo_mes</th>
+                            <th class="text-light">documento</th>
                             <th class="text-light">nombre</th>
-                            <th class="text-light">numeros</th>
-                            <th class="text-light">servicios</th>
+                            <th class="text-light">apellido</th>
                             <th class="text-light">domicilio</th>
                             <th class="text-light">telefono</th>
                             <th class="text-light">email</th>
-                            <th class="text-light">clave</th>
-                            <th class="text-light">empresa</th>
-                            <th class="text-light">cuentas</th>
+                            <th class="text-light">servicios</th>
+
                             <th class="text-light">plan</th>
-                            <th class="text-light">valor</th>
                             <th class="text-light">nodo</th>
+
+                            <th class="text-light">complejo</th>
+                            <th class="text-light">empresa</th>
+                            <th class="text-light">saldo_mes</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -56,19 +57,21 @@
                         <tr class="text-center">
                            
                             <td><?php echo $value->codigo;?></td>
-                            <td><?php echo $value->saldo_mes;?></td>
+                            <td><?php echo $value->documento;?></td>
                             <td><?php echo $value->nombre;?></td>
-                            <td><?php echo $value->numeros;?></td>
-                            <td><?php echo $value->servicios;?></td>
+                            <td><?php echo $value->apellido;?></td>
                             <td><?php echo $value->domicilio;?></td>
                             <td><?php echo $value->telefono;?></td>
                             <td><?php echo $value->email;?></td>
-                            <td><?php echo $value->clave;?></td>
-                            <td><?php echo $value->empresa;?></td>
-                            <td><?php echo $value->cuentas;?></td>
+                            <td><?php echo $value->servicios;?></td>
                             <td><?php echo $value->plan;?></td>
-                            <td><?php echo $value->valor;?></td>
                             <td><?php echo $value->nodo;?></td>
+
+
+                            <td><?php echo $value->complejo;?></td>
+                            <td><?php echo $value->empresa;?></td>
+                            
+                            <td><?php echo $value->saldo_mes;?></td>
 
                         </tr>
 
