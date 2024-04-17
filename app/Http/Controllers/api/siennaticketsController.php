72e991668d17cbfd3d2a76164a637ca7583a7433
luiscornejocornejo <kayser19992005@gmail.com>
2024-04-17 00:20:55 -0300

    ddd

--- app/Http/Controllers/api/siennaticketsController.php
+++ app/Http/Controllers/api/siennaticketsController.php
@@ -1361,10 +1361,10 @@ class siennaticketsController extends Controller
          $zona=$request->zona;
        
         $fecha1 = Carbon::create(Carbon::now($zona) );
-      echo  $fecha11=$fecha1->format('H');
+      echo  $fecha11=$fecha1->format('Y-m-d h:i:s');
     echo "<br>";
         $fecha2 = Carbon::create(Carbon::now('UTC'));
-     echo  $fecha22=$fecha2->format('H');
+     echo  $fecha22=$fecha2->format('Y-m-d h:i:s');
      echo "<br>";
    //  $diferencia_horas=$this->dateDiff($fecha22, $fecha11);
 
