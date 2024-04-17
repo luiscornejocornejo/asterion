14781bc0fcac4e46346cbeefcd89ceff04201bb8
luiscornejocornejo <kayser19992005@gmail.com>
2024-04-16 23:05:12 -0300

    sss

--- app/Http/Controllers/siennaController.php
+++ app/Http/Controllers/siennaController.php
@@ -130,7 +130,7 @@ class siennaController extends Controller
 
       
 
-    INSERT INTO ".$val->Database.".prioridad ( nombre) VALUES( 'Media');
+    INSERT INTO ".$val->Database.".prioridad ( nombre) VALUES( 'Baja');
 
 
     
