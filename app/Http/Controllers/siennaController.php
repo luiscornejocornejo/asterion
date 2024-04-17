80eaca0400cef3bfe328004a257ec7d40ecefc8b
luiscornejocornejo <kayser19992005@gmail.com>
2024-04-16 23:04:23 -0300

    eee

--- app/Http/Controllers/siennaController.php
+++ app/Http/Controllers/siennaController.php
@@ -130,16 +130,9 @@ class siennaController extends Controller
 
       
 
+    INSERT INTO ".$val->Database.".prioridad ( nombre) VALUES( 'Alta');
 
 
-    CREATE TABLE ".$val->Database.".prioridad (
-      id INT auto_increment NOT NULL,
-      nombre varchar(100) NULL,
-      CONSTRAINT prioridad_PK PRIMARY KEY (id)
-    )
-    ENGINE=InnoDB
-    DEFAULT CHARSET=utf8mb4
-    COLLATE=utf8mb4_0900_ai_ci;
     
          ";
       // $query1="";
