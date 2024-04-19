8df8c0634059c1970a0b0cffd6906f9b4cf6bcfb
luiscornejocornejo <kayser19992005@gmail.com>
2024-04-19 12:43:46 -0300

    ddd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -78,22 +78,17 @@ if (isset($_GET['fecha'])) {
 
 </script>
 <!-- Begin page -->
-<script type="text/javascript">
-var areYouReallySure = false;
-function areYouSure() {
-    if(allowPrompt){
-        if (!areYouReallySure && true) {
-            areYouReallySure = true;
-            var confMessage = "***************************************nn E S P E R A !!! nnAntes de abandonar nuestra web, síguenos en nuestras redes sociales como Facebook, Twitter o Instagram.nnnYA PUEDES HACER CLIC EN EL BOTÓN CANCELAR SI QUIERES...nn***************************************";
-            return confMessage;
-        }
-    }else{
-        allowPrompt = true;
+<script language="JavaScript" type="text/javascript">
+     
+    var bPreguntar = true;
+     
+    window.onbeforeunload = preguntarAntesDeSalir;
+     
+    function preguntarAntesDeSalir()
+    {
+      if (bPreguntar)
+        return "¿Seguro que quieres salir?";
     }
-}
-
-var allowPrompt = true;
-window.onbeforeunload = areYouSure;
 </script>
 <div class="wrapper">
 
