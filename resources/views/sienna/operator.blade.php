68978180e7867fe3473fbef9d3476325e7fc9f12
luiscornejocornejo <kayser19992005@gmail.com>
2024-04-19 05:24:55 -0300

    eee

--- resources/views/sienna/operator.blade.php
+++ resources/views/sienna/operator.blade.php
@@ -89,6 +89,7 @@ function maxid() {
             }else{
               aviso='<button  onclick="vista(`' + response.data[i].conversation_url + '`,`' + response.data[i].cliente + '`,`' + result + '`)" class="btn btn-primary " type="button" data-bs-toggle="modal" data-bs-target="#bs-example-modal-lg"><i class="mdi mdi-wechat" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="mb-1" data-bs-title="Conversación."></i> </button>';
             }
+            colorestado=colorestado.replace("-bg", "");
             tt += '<tr class="text-center">' +
                 ' <td><a  target=blank href=/ticketunico?tick='+response.data[i].ticketid +'><i class="mdi '+im+'  '+im2+' me-1 "></i>' + response.data[i].ticketid + '</a></td>' +
                 ' <td>' + response.data[i].nya + '</td>' +
