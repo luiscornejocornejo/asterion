1de3c9a31d0e026f34bc4b439e4e9c6749058e2b
luiscornejocornejo <kayser19992005@gmail.com>
2024-04-24 22:54:22 -0300

    eee

--- resources/views/sienna/agentes.blade.php
+++ resources/views/sienna/agentes.blade.php
@@ -94,7 +94,7 @@ document.getElementById("user_id4").value = dd;
                                     </button> 
                                 </td>
                                 <?php 
-                                if($vueltas==10){
+                                if($vueltas==20){
                                     dd($val3);
                                 }
                                 $vueltas++;?>
