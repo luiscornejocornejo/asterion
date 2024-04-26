47219dbea10ccacdee9d471e898b59184ff48629
luiscornejocornejo <kayser19992005@gmail.com>
2024-04-26 05:37:44 -0300

    ddd

--- resources/views/sienna/empresa.blade.php
+++ resources/views/sienna/empresa.blade.php
@@ -12,39 +12,34 @@
 
               <!-- Start Content-->
                 <div class="container pt-5">
-                 <form action="/empresadatos" method="post">
-                 @csrf
-                 frecuencia:<input type="number" name="frecuencia">
-                 zona:<select name="zonahoraria">
-                    <?php foreach ($zonahoraria as $val){
-
-                        echo "<option value='".$val->nombre."'>".$val->nombre."</option>";
-                    }
-
-?>
-                 </select>
-                 
-                 <button type="submit" class="btn btn-primary">Modificar</button>
-
-                 </form>
+                 <?php
+                 foreach($empresa as $val){
+                  $fre=$val->frecuencia;
+                  $zona=$val->zona;
+                  $reabrir=$val->reabrir;
+                }?>
                  <form action="/empresadatos" method="post">
                  @csrf
                     <div class="form-group">
                       <label for="exampleFormControlInput1">Frecuencia</label>
-                      <input type="number" name="frecuencia" class="form-control" id="exampleFormControlInput1" placeholder="60000">
+                      <input value="<?php echo $fre;?>" type="number" name="frecuencia" class="form-control" id="exampleFormControlInput1" placeholder="60000">
                     </div>
                     <div class="form-group">
                       <label for="exampleFormControlSelect1">Zona Horaria </label>
                       <select  name="zonahoraria" class="form-control" id="exampleFormControlSelect1">
-                      <?php foreach ($zonahoraria as $val){
+                      <?php $selected=""; 
+                      foreach ($zonahoraria as $val){
+                        if($val->nombre==$zona){
+                          $selected="selected";
+                        }
 
-                          echo "<option value='".$val->nombre."'>".$val->nombre."</option>";
+                          echo "<option ".$val->selected." value='".$val->nombre."'>".$val->nombre."</option>";
                           }?>
                       </select>
                     </div>
                     <div class="form-group">
                       <label for="exampleFormControlInput1">Reabrir url broadcast</label>
-                      <input type="text" name="reabrir" class="form-control" id="exampleFormControlInput1" placeholder="http://">
+                      <input value="<?php echo $reabrir;?>" type="text" name="reabrir" class="form-control" id="exampleFormControlInput1" placeholder="http://">
                     </div>
                     <div class="form-group">
                     <label for="exampleFormControlInput1"></label>
