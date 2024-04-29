3024ef10dc13394a70539cf688dee9b5b326b385
luiscornejocornejo <kayser19992005@gmail.com>
2024-04-29 15:03:27 -0300

    dddd

--- resources/views/sienna/ticketunicoemail.blade.php
+++ resources/views/sienna/ticketunicoemail.blade.php
@@ -128,80 +128,30 @@ function coloriconos($iconos, $tipo)
                             {!! $b !!}
                             <hr>
 
-                            <h5 class="mb-3">Adjuntos (preparar condición para que en todo caso desaparezca esta linea y el hr)</h5>
+                            <h5 class="mb-3">Adjuntos</h5>
 
                             <div class="row">
+                                <?php foreach($segui as $adj){
+                                    if($adj->tipo==9){?>
                                 <div class="col-xl-4">
                                     <div class="card mb-1 shadow-none border">
                                         <div class="p-2">
                                             <div class="row align-items-center">
-                                                <div class="col-auto">
-                                                    <div class="avatar-sm">
-                                                        <span class="avatar-title bg-primary-lighten text-primary rounded">
-                                                            .ZIP
-                                                        </span>
-                                                    </div>
-                                                </div>
-                                                <div class="col ps-0">
-                                                    <a href="javascript:void(0);" class="text-muted fw-bold">Hyper-admin-design.zip</a>
-                                                    <p class="mb-0">2.3 MB</p>
-                                                </div>
+                                            
                                                 <div class="col-auto">
                                                     <!-- Button -->
-                                                    <a href="javascript:void(0);" class="btn btn-link btn-lg text-muted">
-                                                        <i class="ri-download-2-line"></i>
-                                                    </a>
+                                               <a target=_blank href="<?php echo $adj->descripcion;?>"><img  src='<?php echo $adj->descripcion;?>' width="40px;"></a>
+                                                    
+                                                   
+                                                       
+                                                   
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div> <!-- end col -->
-                                <div class="col-xl-4">
-                                    <div class="card mb-1 shadow-none border">
-                                        <div class="p-2">
-                                            <div class="row align-items-center">
-                                                <div class="col-auto">
-                                                    <img src="assets/images/projects/project-1.jpg" class="avatar-sm rounded" alt="file-image">
-                                                </div>
-                                                <div class="col ps-0">
-                                                    <a href="javascript:void(0);" class="text-muted fw-bold">Dashboard-design.jpg</a>
-                                                    <p class="mb-0">3.25 MB</p>
-                                                </div>
-                                                <div class="col-auto">
-                                                    <!-- Button -->
-                                                    <a href="javascript:void(0);" class="btn btn-link btn-lg text-muted">
-                                                        <i class="ri-download-2-line"></i>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div> <!-- end col -->
-                                <div class="col-xl-4">
-                                    <div class="card mb-0 shadow-none border">
-                                        <div class="p-2">
-                                            <div class="row align-items-center">
-                                                <div class="col-auto">
-                                                    <div class="avatar-sm">
-                                                        <span class="avatar-title text-bg-secondary rounded">
-                                                            .MP4
-                                                        </span>
-                                                    </div>
-                                                </div>
-                                                <div class="col ps-0">
-                                                    <a href="javascript:void(0);" class="text-muted fw-bold">Admin-bug-report.mp4</a>
-                                                    <p class="mb-0">7.05 MB</p>
-                                                </div>
-                                                <div class="col-auto">
-                                                    <!-- Button -->
-                                                    <a href="javascript:void(0);" class="btn btn-link btn-lg text-muted">
-                                                        <i class="ri-download-2-line"></i>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div> <!-- end col -->
+                                <?php }}?>
+                             
                             </div>
                             <!-- end row-->
                             <form action="" method="post">
