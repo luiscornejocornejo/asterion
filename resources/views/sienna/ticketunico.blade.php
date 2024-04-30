44102d99af59e4ed911204053c19e05744ac9be6
luiscornejocornejo <kayser19992005@gmail.com>
2024-04-30 17:56:08 -0300

    ddd

--- resources/views/sienna/ticketunico.blade.php
+++ resources/views/sienna/ticketunico.blade.php
@@ -186,15 +186,17 @@ function tituloiconos($iconos, $tipo)
                                     <!-- end row-->
                                    
                                         <div class="mt-5">
-                                            <input id="mailaeviar" readonly type="email" value="<?php echo $resultados[0]->emailcliente;?>">
+                                        <input id="mailaeviar" readonly type="email" value="<?php echo $resultados[0]->emailcliente;?>">
+                                        <input id="mailaeviarcc" readonly type="email" value="<?php echo $resultados[0]->cc;?>">
                                             <div  id="snow-editor" style="height: 300px;">
                                             </div>
-                                            <button onclick="enviaremail('<?php echo $resultados[0]->ticketid;?>','<?php echo $subdomain_tmp;?>')" type="button" class="btn me-2 mt-2 rounded-pill" style="background-color: #FFD193;">Responder</button> 
+                                            <button onclick="enviaremail('<?php echo $resultados[0]->ticketid;?>','<?php echo $subdomain_tmp;?>','<?php echo $resultados[0]->cc;?>',)" type="button" class="btn me-2 mt-2 rounded-pill" style="background-color: #FFD193;">Responder</button> 
                                         </div>
                                         <script>
-                                            function enviaremail(ticket,merchant){
+                                            function enviaremail(ticket,merchant,cc){
 
                                                 let mail=document.getElementById("mailaeviar").value;
+                                                let cc=document.getElementById("mailaeviarcc").value;
                                                 let texto=document.getElementById("snow-editor").innerHTML;
                                                 alert(mail);
                                                 alert(texto);
