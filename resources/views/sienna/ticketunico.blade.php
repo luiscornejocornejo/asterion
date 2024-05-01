1f38c681661f0ad2e39f248fbed9320b8b94dbf8
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-01 01:15:44 -0300

    eeee

--- resources/views/sienna/ticketunico.blade.php
+++ resources/views/sienna/ticketunico.blade.php
@@ -190,10 +190,10 @@ function tituloiconos($iconos, $tipo)
                                         <input id="mailaeviarcc" readonly type="email" value="<?php echo $resultados[0]->cc;?>">
                                             <div  id="snow-editor" style="height: 300px;">
                                             </div>
-                                            <button onclick="enviaremail('<?php echo $resultados[0]->ticketid;?>','<?php echo $subdomain_tmp;?>','gg')" type="button" class="btn me-2 mt-2 rounded-pill" style="background-color: #FFD193;">Responder</button> 
+                                            <button onclick="enviaremail2('<?php echo $resultados[0]->ticketid;?>','<?php echo $subdomain_tmp;?>','<?php echo $resultados[0]->cc;?>')" type="button" class="btn me-2 mt-2 rounded-pill" style="background-color: #FFD193;">Responder</button> 
                                         </div>
                                         <script>
-                                            function enviaremail(ticket,merchant,cc){
+                                            function enviaremail2(ticket,merchant,cc){
 
                                                 let mail=document.getElementById("mailaeviar").value;
                                                 let cc=document.getElementById("mailaeviarcc").value;
