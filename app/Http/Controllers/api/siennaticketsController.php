1bc092c68806d3aded70196f106b23df8be54b72
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-01 01:35:16 -0300

    ssss

--- app/Http/Controllers/api/siennaticketsController.php
+++ app/Http/Controllers/api/siennaticketsController.php
@@ -1623,7 +1623,8 @@ class siennaticketsController extends Controller
         $for="kayser95@hotmail.com";
         $cc="kayser1712@gmail.com";
 
-        Mail::send('mailsienna', ["fields2" => $texto], function ($msj) use ($subject,$for, $cc) {
+        Mail::mailer('suricata')
+                ->send('mailsienna', ["fields2" => $texto], function ($msj) use ($subject,$for, $cc) {
             $msj->from("prueba@siennasystem.com", "luis");
             $msj->subject($subject);
             $msj->to($for);
--- config/mail.php
+++ config/mail.php
@@ -44,7 +44,7 @@ return [
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
-        'rivernet' => [
+        'suricata' => [
             'transport' => 'smtp',
             'host' => 'mail.smtp2go.com',
             'port' =>'2525',
