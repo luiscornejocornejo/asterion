ac7670002c27ddc511d09613c3f7daa6784b901d
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-02 08:45:57 -0300

    eee

--- app/Http/Controllers/api/siennaticketsController.php
+++ app/Http/Controllers/api/siennaticketsController.php
@@ -1635,7 +1635,7 @@ class siennaticketsController extends Controller
 
 
         $si2 = siennatickets::find($ticket);
-        $si2->email = $si2->email ."<hr>".$texto;
+        $si2->email = $si2->email ."<hr><h1>Soporte</h1>".$texto;
         $si2->estadoconv =0;
         $si2->save();
         return $ticket;
