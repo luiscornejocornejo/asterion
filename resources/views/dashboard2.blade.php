c1d3fdebd66e7435f92155e679594b9756eef6e9
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-02 23:54:02 -0300

    rrrr

--- resources/views/dashboard2.blade.php
+++ resources/views/dashboard2.blade.php
@@ -12,7 +12,46 @@ if (isset($_SERVER['HTTP_HOST'])) {
     $subdomain_tmp =  array_shift($domainParts);
 }
 $subdomain_tmp = trim($subdomain_tmp);
+$query = "select count(*) as cantidaduser  from siennaloginxenioo where login=1 and  DATE(created_at) >= DATE(NOW())";
+$resultados = DB::select($query);
+$cantidaduser = 0;
+$cantidadtickets = 0;
+$cantidadtickets2 = 0;
+$listamensual = array();
+foreach ($resultados as $val) {
+    $cantidaduser = $val->cantidaduser;
+}
 
+if (isset($_GET['fecha'])) {
+
+    $queryfecha = "";
+    if ($_GET['fecha'] == "dia") {
+
+        $queryfecha = "DATE(created_at) >= DATE(NOW())";
+    }
+    if ($_GET['fecha'] == "semana") {
+
+        $queryfecha = "created_at>= (DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY))";
+    }
+    $listamensual = array();
+
+
+
+    $query = "select count(*) as cantidadtickets  from siennatickets where siennaestado=1 and  " . $queryfecha;
+    $resultados2 = DB::select($query);
+    $cantidadtickets = 0;
+    foreach ($resultados2 as $val) {
+        $cantidadtickets = $val->cantidadtickets;
+    }
+
+
+    $query = "select count(*) as cantidadtickets2  from siennatickets where siennaestado=4 and  " . $queryfecha;
+    $resultados2 = DB::select($query);
+    $cantidadtickets2 = 0;
+    foreach ($resultados2 as $val) {
+        $cantidadtickets2 = $val->cantidadtickets2;
+    }
+}
 ?>
 
 <script>
