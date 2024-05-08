cc712e4065701d784dd345e619c45691b25bff37
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-08 06:14:35 -0300

    uuu

--- resources/views/sienna/ticketunico.blade.php
+++ resources/views/sienna/ticketunico.blade.php
@@ -320,39 +320,51 @@ function tituloiconos($iconos, $tipo)
                                 <hr style="margin-top: 10px;" />
                                 <div class="row">
                                     <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
+                                        <?php if(isset($resultados[0]->cliente)){?>
                                             <div class="d-flex mt-2">
                                                 <i class="mdi mdi-card-account-details"></i>&nbsp;Numero cliente:&nbsp;
                                                 <span class="badge badge-secondary-lighten line-h">
                                                     <?php echo $resultados[0]->cliente; ?>
                                                 </span>
                                             </div>
+                                        <?php
+                                            }if(isset($resultados[0]->nya)){?>
                                             <div class="d-flex  mt-2">
                                                 <i class="mdi mdi-account"></i>&nbsp;Nombre:&nbsp;
                                                 <span class="badge badge-secondary-lighten hover-overlay line-h">
                                                     <?php echo $resultados[0]->nya; ?>
                                                 </span>
                                             </div>
+                                        <?php
+                                            }if(isset($resultados[0]->address)){?>
                                             <div class="d-flex mt-2">
                                                 <i class="mdi mdi-home"></i>&nbsp;Domicilio:&nbsp;
                                                 <span class="badge badge-secondary-lighten line-h">
                                                     <?php echo $resultados[0]->address; ?>
                                                 </span>
                                             </div>
+                                        <?php
+                                            }if(isset($resultados[0]->cel)){?>
                                             <div class="d-flex mt-2">
                                                 <i class="mdi mdi-whatsapp text"></i>&nbsp;Teléfono:&nbsp;
                                                 <span class="badge badge-secondary-lighten line-h">
                                                     <?php echo $resultados[0]->cel; ?>
                                                 </span>
                                             </div>
+                                        <?php
+                                            }if(isset($resultados[0]->email)){?>
                                             <div class="d-flex mt-2">
                                                 <i class="mdi mdi-email"></i>&nbsp;Email:&nbsp;
                                                 <span class="badge badge-secondary-lighten line-h">
                                                     <?php echo $resultados[0]->email; ?>
                                                 </span>
                                             </div>
+                                            <?php }?>
 
                                     </div>
                                     <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
+                                        <?php
+                                            if(isset($resultados[0]->a_status)){?>
                                             <div class="d-flex mt-2">
                                                 <i class="mdi mdi-account-cash"></i>&nbsp;Estado de cuenta:&nbsp;
                                                 <span class="badge badge-success-lighten line-h">
@@ -360,6 +372,8 @@ function tituloiconos($iconos, $tipo)
                                                 </span>
 
                                             </div>
+                                        <?php
+                                            }if(isset($resultados[0]->s_status)){?>
                                             <div class="d-flex mt-2">
                                                 <i class="mdi mdi-antenna"></i>&nbsp;Estado de servicio:&nbsp;
                                                 <span class="badge badge-success-lighten line-h">
@@ -367,24 +381,31 @@ function tituloiconos($iconos, $tipo)
                                                 </span>
 
                                             </div>
+                                        <?php
+                                            }if(isset($resultados[0]->nodo)){?>
                                             <div class="d-flex mt-2">
                                                 <i class="mdi mdi-switch"></i>&nbsp;Nodo:&nbsp;
                                                 <span class="badge badge-secondary-lighten line-h">
                                                     <?php echo $resultados[0]->nodo; ?>
                                                 </span>
                                             </div>
+                                        <?php
+                                            }if(isset($resultados[0]->ip)){?>
                                             <div class="d-flex mt-2">
                                                 <i class="mdi mdi-map-marker"></i>&nbsp;IP:&nbsp;
                                                 <span class="badge badge-secondary-lighten line-h">
                                                     <?php echo $resultados[0]->ip; ?>
                                                 </span>
                                             </div>
+                                        <?php
+                                            }if(isset($resultados[0]->deuda)){?>
                                             <div class="d-flex mt-2">
                                                 <i class="mdi mdi-currency-usd"></i>&nbsp;Deuda:&nbsp;
                                                 <span class="badge badge-secondary-lighten line-h">
                                                     <?php echo $resultados[0]->deuda; ?>
                                                 </span>
                                             </div>
+                                            <?php }?>
                                     </div>
                                 </div>                    
                             </div>
