8c7304c5fcb29c9846a56845d5f0b885a5933425
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-08 23:53:22 -0300

    ddd

--- app/Http/Controllers/siennaController.php
+++ app/Http/Controllers/siennaController.php
@@ -124,8 +124,18 @@ class siennaController extends Controller
     
       //
     echo   $query1="
-
-    ALTER TABLE ".$val->Database.".siennatickets ADD cc varchar(1000) NULL;
+    CREATE TABLE ".$val->Database.".siennamail (
+      id INT auto_increment NOT NULL,
+      cuerpo LONGTEXT NULL,
+      siennatickets SMALLINT NULL,
+      `from` varchar(1000) NULL,
+      autor varchar(100) NULL,
+      CONSTRAINT siennamail_pk PRIMARY KEY (id)
+    )
+    ENGINE=InnoDB
+    DEFAULT CHARSET=utf8mb4
+    COLLATE=utf8mb4_0900_ai_ci;
+    
 
 
   
