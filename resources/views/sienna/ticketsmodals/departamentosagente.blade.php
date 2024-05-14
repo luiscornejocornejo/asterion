8c5030ce7401ca5c9200213d889d0594c2bbde49
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-14 13:06:06 -0300

    rrr

--- resources/views/sienna/ticketsmodals/departamentosagente.blade.php
+++ resources/views/sienna/ticketsmodals/departamentosagente.blade.php
@@ -2,7 +2,7 @@
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header bg-dark text-white">
-                    <h5 class="modal-title">Cambiar Depto</h5>
+                    <h5 class="modal-title">Cambiar Depto Agente</h5>
                     <button type="button" class="btn-close text-white" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
