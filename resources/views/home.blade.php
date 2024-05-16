d6a844e038bcd796316178d73245c1e3e5646a52
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-16 02:15:50 -0300

    sss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -524,6 +524,73 @@ if (isset($_GET['fecha'])) {
                             document.getElementById("logeados").innerHTML = sd;
                             document.getElementById("tb").innerHTML = null;
                             document.getElementById("tb").innerHTML = tt;
+                            $('#example').dataTable({
+                                            "order": [[0, 'desc']],
+                                            "pageLength": 25,
+                                            select: true,
+                                "language" : {
+                                    "url": "//cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json"
+                                },
+                                dom: 'Bfrtip',
+                                initComplete: function () {
+                                    var api = this.api();
+                        
+                                    // For each column
+                                
+                                    api
+                                        .columns()
+                                        .eq(0)
+                                        .each(function (colIdx) {
+                                            // Set the header cell to contain the input element
+                                            var cell = $('.filters th').eq(
+                                                $(api.column(colIdx).header()).index()
+                                            );
+                                            var title = $(cell).text();
+                                            $(cell).html('<input type="text" placeholder="' + title + '" />');
+                        
+                                            // On every keypress in this input
+                                            $(
+                                                'input',
+                                                $('.filters th').eq($(api.column(colIdx).header()).index())
+                                            )
+                                                .off('keyup change')
+                                                .on('change', function (e) {
+                                                    // Get the search value
+                                                    $(this).attr('title', $(this).val());
+                                                    var regexr = '({search})'; //$(this).parents('th').find('select').val();
+                        
+                                                    var cursorPosition = this.selectionStart;
+                                                    // Search the column for that value
+                                                    api
+                                                        .column(colIdx)
+                                                        .search(
+                                                            this.value != ''
+                                                                ? regexr.replace('{search}', '(((' + this.value + ')))')
+                                                                : '',
+                                                            this.value != '',
+                                                            this.value == ''
+                                                        )
+                                                        .draw();
+                                                })
+                                                .on('keyup', function (e) {
+                                                    e.stopPropagation();
+                        
+                                                    $(this).trigger('change');
+                                                    $(this)
+                                                        .focus()[0]
+                                                        .setSelectionRange(cursorPosition, cursorPosition);
+                                                });
+                                        });
+                                },
+                                        buttons: [
+                                            'copy', 'csv', 'excel', 'pdf', 'print'
+                                        ]
+                                });
+
+                                $('#example thead tr')
+                                .clone(true)
+                                .addClass('filters')
+                                .appendTo('#example thead');
                         })
                         .catch(function(error) {
                             // función para capturar el error
