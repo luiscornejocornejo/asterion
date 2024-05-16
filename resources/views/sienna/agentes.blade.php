0e9273b7fdd1e1978fc1c6d578fe39906275a74c
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-15 22:12:23 -0300

    ddd

--- resources/views/sienna/agentes.blade.php
+++ resources/views/sienna/agentes.blade.php
@@ -99,6 +99,11 @@ document.getElementById("user_id4").value = dd;
                                     <i class="mdi mdi-office-building-cog" data-bs-toggle="tooltip" data-bs-placement="top"
                                         data-bs-custom-class="mb-1" data-bs-title="Departamento"></i>
                                     </button> 
+
+                                    <button onclick="eliminar(`{{$val3->idusu}}`)"  data-bs-toggle="modal" data-bs-target="#standard-modalticketeliminar"  class="btn btn-danger rounded" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="mdi mdi-office-building-cog" data-bs-toggle="tooltip" data-bs-placement="top"
+                                        data-bs-custom-class="mb-1" data-bs-title="Departamento"></i>
+                                    </button> 
                                 </td>
                                 <?php 
                                 if($vueltas==23){
