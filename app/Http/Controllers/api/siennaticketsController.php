40c599b8b139d8b6ef5e239583477ecdd98cdf4f
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-17 01:09:30 -0300

    eee

--- app/Http/Controllers/api/siennaticketsController.php
+++ app/Http/Controllers/api/siennaticketsController.php
@@ -554,12 +554,14 @@ class siennaticketsController extends Controller
 
         $query = "select *,a.conversation_id,a.user_id,concat(e.nombre,' ',e.last_name) as nombreagente,
         b.nombre as depto,b.id as iddepto,d.nombre topicnombre,convertirTiempo(a.created_at) as creado,
-        a.id as ticketid,c.nombre estadoname,d.nombre topicname,a.cel numerocel,a.asignado 
+        a.id as ticketid,c.nombre estadoname,d.nombre topicname,a.cel numerocel,a.asignado,f.nombre as pri 
         from ".$merchant.".siennatickets a
         left join ".$merchant.".siennadepto b on b.id=a.siennadepto 
         left join  ".$merchant.".siennaestado c on c.id=a.siennaestado
         left join  ".$merchant.".siennatopic d on d.id=a.siennatopic
         left join  ".$merchant.".users e on e.id=a.asignado
+        left join  ".$merchant.".prioridad f on f.id=a.prioridad
+
         where a.siennaestado not in('3','4')  
          
 
@@ -568,13 +570,15 @@ class siennaticketsController extends Controller
          select *,a.conversation_id,a.user_id,concat(e.nombre,' ',e.last_name) as nombreagente,
         b.nombre as depto,b.id as iddepto,d.nombre topicnombre,convertirTiempo(a.created_at)  as creado,
 
-        a.id as ticketid,c.nombre estadoname,d.nombre topicname,a.cel numerocel,a.asignado 
+        a.id as ticketid,c.nombre estadoname,d.nombre topicname,a.cel numerocel,a.asignado ,f.nombre as pri 
         from ".$merchant.".siennatickets a
         left join ".$merchant.".siennadepto b on b.id=a.siennadepto 
         left join  ".$merchant.".siennaestado c on c.id=a.siennaestado
         left join  ".$merchant.".siennatopic d on d.id=a.siennatopic
 
         left join  ".$merchant.".users e on e.id=a.asignado
+        left join  ".$merchant.".prioridad f on f.id=a.prioridad
+
         where a.siennaestado not in('3','4')  
          
 
