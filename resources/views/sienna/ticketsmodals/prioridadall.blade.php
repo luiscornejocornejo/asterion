c7046faa3b7d759df16b0e9e869d680c98284736
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-16 22:49:27 -0300

    jjj

--- resources/views/sienna/ticketsmodals/prioridadall.blade.php
+++ resources/views/sienna/ticketsmodals/prioridadall.blade.php
@@ -1,32 +1,33 @@
-<div class="modal fade" id="bs-example-modal-prioridadall" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-sm">
+
+
+<div id="bs-example-modal-prioridadall" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="dark-header-modalLabel" aria-hidden="true">
+           <div class="modal-dialog">
                 <div class="modal-content">
-                    <div class="modal-header bg-dark text-white">
-                        <h4 class="modal-title" id="mySmallModalLabel">Cambiar Prioridad</h4>
+                    <div class="modal-header bg-dark">
+                        <h4 class="modal-title text-light" id="dark-header-modalLabel">Asignar ticket</h4>
                         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-hidden="true"></button>
                     </div>
                     <div class="modal-body">
-                        <form action="/prioridadsiennaall" method="post">
-                        @csrf
+                            Cambiar la Prioridad de los tickets             
+                                </div>
+                    <div class="modal-footer mx-auto">
+                        <form action="/prioridadsiennaall" method="POST">
+                            <input value="" type="hidden" name="idticketpedir" id="idticketpedir2">
                             <input value="<?php echo session('nombreusuario');?>" type="hidden" name="logeado" id="logeado">
-                            <input value="<?php echo $subdomain_tmp;?>" type="hidden" name="idbot" id="idbot">
-                            <input value="" type="hidden" name="tp" id="tp">
-    
-                            <input  type="hidden" name="merchant" id="merchant" value=" <?php echo $subdomain_tmp;?> ">
+                            <input value="" readonly type="hidden" name="ticketss" id="idticketpedir20">
 
                             <div v-for="department in departments ">
-                            <?php foreach($prioridades as $dep){?>
-                                <input value="<?php echo $dep->id;?>" class="form-radio" type="radio" name="statos">&nbsp;
-                            <span class=" fw-bold" style="color: #98a6ad;font-size: 12px;"><?php echo$dep->nombre;?></span>
-                                <br><br>
+                                <?php foreach($prioridades as $dep){?>
+                                    <input value="<?php echo $dep->id;?>" class="form-radio" type="radio" name="usuarioticket">&nbsp;
+                                <span class=" fw-bold" style="color: #98a6ad;font-size: 12px;"><?php echo$dep->nombre." ".$dep->last_name;?></span>
+                                    <br><br>
 
-                                <?php }?>   
-                                                </div>
-                            <button type="submit" class="btn btn-success
-                                        waves-effect waves-light">Cambiar</button>
-
+                                    <?php }?>   
+                            </div>
+                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">No</button>
+                            <button type="submit" class="btn btn-success">Si</button>
                         </form>
                     </div>
                 </div><!-- /.modal-content -->
             </div><!-- /.modal-dialog -->
-</div>
+</div><!-- /.modal -->
