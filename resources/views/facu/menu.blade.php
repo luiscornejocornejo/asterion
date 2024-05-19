7057f257b938f61a8853b55b17b69e76827f7947
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-19 18:24:31 -0300

    sss

--- resources/views/facu/menu.blade.php
+++ resources/views/facu/menu.blade.php
@@ -347,9 +347,7 @@ if ($categoria == 1) {?>
 
                 if($saliente ==1){?>
                  
-    <div class="whatsapp" data-bs-toggle="modal" data-bs-target="#warning-alert-modal">
-    <i class="mdi mdi-send ms-1" style="font-size: 25px;margin-left: 0.785rem!important;"></i>
-</div>
+    
 <script>
         function toggleMenu() {
             var menu = document.getElementById("menu");
@@ -372,13 +370,7 @@ if ($categoria == 1) {?>
       <span class="mdi mdi-plus"></span>
     </button>
     <div id="menu" class="hidden">
-      <button 
-        id="button-2" 
-        class="mdi mdi-ticket-account bg-warning text-light"
-        data-bs-toggle="modal" href="#exampleModalToggle"
-       >
-    </button>
-      <button id="button-3" class="mdi mdi-send bg-success text-light "></button>
+      <button  id="button-2" class="mdi mdi-ticket-account bg-warning text-light" data-bs-toggle="modal" href="#exampleModalToggle" ><button id="button-3" class="mdi mdi-send bg-success text-light "></button>
     </div>
 </div>
 <!-- Modal for Create Ticket -->
@@ -415,103 +407,101 @@ if ($categoria == 1) {?>
 
     <!-- Between Modal -->
     <!-- Modal -->
-<div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel"
-tabindex="-1">
-<div class="modal-dialog modal-dialog-centered modal-lg">
-    <div class="modal-content">
-        <div class="modal-header bg-dark">
-            <h5 class="modal-title text-white" id="exampleModalToggleLabel">Crear ticket</h5>
-            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <form>
-                <div class="row">
-                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                        <label for="type-user" class="form-label">Tipo de busqueda</label>
-                        <select class="form-select" id="type-user">
-                            <option value="id">Cliente</option>
-                            <option value="id">DNI o CUIT</option>
-                            <option value="id">Cédula o RUC</option>
-                            <option value="id">Telefono</option>
-                        </select>
-                    </div>                                                                                            
-                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                        <label for="number-client" class="form-label">Número registrado en la cuenta</label>
-                        <input type="number" id="number-client" name="number-client" class="form-control" required>
+<div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel"tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content">
+            <div class="modal-header bg-dark">
+                <h5 class="modal-title text-white" id="exampleModalToggleLabel">Crear ticket</h5>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="row">
+                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                            <label for="type-user" class="form-label">Tipo de busqueda</label>
+                            <select class="form-select" id="type-user">
+                                <option value="id">Cliente</option>
+                                <option value="id">DNI o CUIT</option>
+                                <option value="id">Cédula o RUC</option>
+                                <option value="id">Telefono</option>
+                            </select>
+                        </div>                                                                                            
+                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                            <label for="number-client" class="form-label">Número registrado en la cuenta</label>
+                            <input type="number" id="number-client" name="number-client" class="form-control" required>
+                        </div>
+                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 mt-1">
+                            <label for="department" class="form-label">Departamento</label>
+                            <select class="form-select">
+                                <option id="1">Administración</option>
+                                <option id="2">Instalaciones</option>
+                                <option id="3">Soporte</option>
+                                <option id="4">Ventas</option>
+                            </select>
+                        </div>
+                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 mt-1">
+                            <label for="reason" class="form-label">Motivo</label>
+                            <input type="text" class="form-control" id="reason" name="reason">
+                        </div>
                     </div>
-                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 mt-1">
-                        <label for="department" class="form-label">Departamento</label>
-                        <select class="form-select">
-                            <option id="1">Administración</option>
-                            <option id="2">Instalaciones</option>
-                            <option id="3">Soporte</option>
-                            <option id="4">Ventas</option>
-                        </select>
-                    </div>
-                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 mt-1">
-                        <label for="reason" class="form-label">Motivo</label>
-                        <input type="text" class="form-control" id="reason" name="reason">
-                    </div>
-                </div>
-            </form>
-        </div>
-        <div class="modal-footer mt-2">
-            <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal"
-                data-bs-dismiss="modal">No es cliente</button>
-                <button class="btn btn-success" type="submit" onclick="">Crear</button>
-        </div>
-    </div><!-- /.modal-content -->
-</div><!-- /.modal-dialog -->
+                </form>
+            </div>
+            <div class="modal-footer mt-2">
+                <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal"
+                    data-bs-dismiss="modal">No es cliente</button>
+                    <button class="btn btn-success" type="submit" onclick="">Crear</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
             
 <!-- Modal -->
-<div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2"
-tabindex="-1">
-<div class="modal-dialog modal-dialog-centered modal-lg">
-    <div class="modal-content">
-        <div class="modal-header bg-dark">
-            <h5 class="modal-title text-light" id="exampleModalToggleLabel2">Crear Ticket</h5>
-            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <div class="row">
-                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                    <label for="fullname" class="form-label">Nombre completo</label>
-                    <input type="text" class="form-control" id="fullname" name="fullname">
+<div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2"tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+                <div class="modal-header bg-dark">
+                    <h5 class="modal-title text-light" id="exampleModalToggleLabel2">Crear Ticket</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                    <label for="phone" class="form-label">Telefono de contacto</label>
-                    <input type="text" class="form-control" id="phone" name="phone">
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                            <label for="fullname" class="form-label">Nombre completo</label>
+                            <input type="text" class="form-control" id="fullname" name="fullname">
+                        </div>
+                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                            <label for="phone" class="form-label">Telefono de contacto</label>
+                            <input type="text" class="form-control" id="phone" name="phone">
+                        </div>
+                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 mt-1">
+                            <label for="address" class="form-label">Dirección</label>
+                            <input type="text" class="form-control" id="address" name="address">
+                        </div>
+                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 mt-1">
+                            <label for="city" class="form-label">Localidad</label>
+                            <input type="text" class="form-control" id="city" name="city">
+                        </div>
+                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 mt-1">
+                            <label for="reason-prospect" class="form-label">Motivo</label>
+                            <input type="text" class="form-control" id="reason-prospect" name="reason-prospect">
+                        </div>
+                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 mt-1">
+                            <label for="department-prospect" class="form-label">Departamento</label>
+                            <select class="form-select" id="department-prospect" name="department-prospect">
+                                <option id="1">Administración</option>
+                                <option id="2">Instalaciones</option>
+                                <option id="3">Soporte</option>
+                                <option id="4">Ventas</option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 mt-1">
-                    <label for="address" class="form-label">Dirección</label>
-                    <input type="text" class="form-control" id="address" name="address">
+                <div class="modal-footer mt-2">
+                    <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal"
+                        data-bs-dismiss="modal">Volver</button>
                 </div>
-                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 mt-1">
-                    <label for="city" class="form-label">Localidad</label>
-                    <input type="text" class="form-control" id="city" name="city">
-                </div>
-                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 mt-1">
-                    <label for="reason-prospect" class="form-label">Motivo</label>
-                    <input type="text" class="form-control" id="reason-prospect" name="reason-prospect">
-                </div>
-                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 mt-1">
-                    <label for="department-prospect" class="form-label">Departamento</label>
-                    <select class="form-select" id="department-prospect" name="department-prospect">
-                        <option id="1">Administración</option>
-                        <option id="2">Instalaciones</option>
-                        <option id="3">Soporte</option>
-                        <option id="4">Ventas</option>
-                    </select>
-                </div>
-            </div>
-        </div>
-        <div class="modal-footer mt-2">
-            <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal"
-                data-bs-dismiss="modal">Volver</button>
-        </div>
-    </div><!-- /.modal-content -->
-</div><!-- /.modal-dialog -->
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
 <script src="flotante.js"></script>
 
