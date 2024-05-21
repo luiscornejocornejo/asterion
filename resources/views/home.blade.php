841dab57e023ec64df8a7ae31481a94a524af1e4
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-21 15:48:45 -0300

    ddd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,14 @@
         if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
             console.log("Estás usando un dispositivo móvil!!");
             tipo=<?php  echo $tipodemenu = session('tipodemenu'); ?>;
+            var url = window.location.href;
+
             if(tipo==4){
-                alert("supervisor");
+                location.href = url+'/supervisor';
+
 
             }else{
-                alert("agente");
+                location.href = url+'/operador';
 
             }
         } else {
