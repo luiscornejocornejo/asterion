12dbdcf823dc18951a34573dd4f22b29661007eb
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-21 15:27:57 -0300

    cfc

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,14 @@
         let navegador = navigator.userAgent;
         if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
             console.log("Estás usando un dispositivo móvil!!");
+            tipo=<?php  $tipodemenu = session('tipodemenu'); echo $tipodemenu;?>
+            if(tipo=="4"){
+                alert("supervisor");
 
-            alert("mobile");
+            }else{
+                alert("agente");
+
+            }
         } else {
             console.log("No estás usando un móvil");
         }
