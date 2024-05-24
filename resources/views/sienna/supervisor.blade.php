7a14b3672d96606e1cd01a38dda2a590289da123
Ficuu <facuu.paez95@gmail.com>
2024-05-24 12:15:06 -0300

    adding tooltip to buttons actions

--- resources/views/sienna/supervisor.blade.php
+++ resources/views/sienna/supervisor.blade.php
@@ -1,5 +1,52 @@
 @include('facu.header2')
 
+<style>
+  .tooltip-button {
+  position: relative;
+  display: inline-block;
+}
+
+/* Tooltip text */
+.tooltip-button .tooltiptext {
+  visibility: hidden;
+  width: 120px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  padding: 5px 0;
+  border-radius: 6px;
+
+  /* Position the tooltip text */
+  position: absolute;
+  z-index: 1;
+  bottom: 125%;
+  left: 50%;
+  margin-left: -60px;
+
+  /* Fade in tooltip */
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+
+/* Tooltip arrow */
+.tooltip-button .tooltiptext::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+}
+
+/* Show the tooltip text when you mouse over the tooltip container */
+.tooltip-button:hover .tooltiptext {
+  visibility: visible;
+  opacity: 1;
+}
+
+</style>
 
 <script>
  
@@ -181,9 +228,9 @@
             .then(function (response) {
                 // función que se ejecutará al recibir una respuesta
             
-                sd='<button  onclick="pedirall()"  class="btn btn-info" type="button" data-bs-toggle="modal" data-bs-target="#standard-modal-reclamo"><i class="mdi mdi-account-arrow-left" data-bs-toggle="tooltip" data-bs-placement="top"  data-bs-custom-class="" data-bs-title="Reclamar ticket."></i></button> '+
-                '<button onclick="areaall()"  class="btn btn-info me-1" type="button" data-bs-toggle="modal" data-bs-target="#bs-example-modal-sm2"><i class="mdi mdi-account-group" data-bs-toggle="tooltip" data-bs-placement="top"     data-bs-custom-class="" data-bs-title="Asignar departamento."></i> </button>'+
-                '<button onclick="cerrarall()"  class="btn btn-success  " type="button" data-bs-toggle="modal" data-bs-target="#bs-example-modal-smcerrarall">  <i class="mdi mdi-check-circle" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="" data-bs-title="Cambiar cerrar."></i></button> '+
+                sd='<button  onclick="pedirall()"  class="btn btn-info tooltip-button" type="button" data-bs-toggle="modal" data-bs-target="#standard-modal-reclamo"><span class="mdi mdi-account-arrow-left"><span class="tooltiptext">Reclamar tickets</span></span></button> '+
+                '<button onclick="areaall()"  class="btn btn-info me-1" type="button" data-bs-toggle="modal" data-bs-target="#bs-example-modal-sm2"><i class="mdi mdi-account-group"Asignar departamento."></i> </button>'+
+                '<button onclick="cerrarall()"  class="btn btn-success  " type="button" data-bs-toggle="modal" data-bs-target="#bs-example-modal-smcerrarall">  <i class="mdi mdi-check-circle"  data-bs-custom-class="" data-bs-title="Cambiar cerrar."></i></button> '+
                 '<button onclick="prioridadall()"  class="btn btn-success" type="button" data-bs-toggle="modal" data-bs-target="#bs-example-modal-prioridadall"><i class="mdi mdi-priority-high"></i></button> '+
                 '  <button type="button" onclick="selects()" class="btn btn-primary"><i class="mdi mdi-check-all" ></i></button>'+  
                 ' <button type="button" onclick="deSelect()" class="btn btn-info"><i class="mdi mdi-checkbox-blank-outline"></i></button>'+
