adfb9f2da118167d13f9a91dc26b64bec65ef95e
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-29 08:47:25 -0300

    ffff

--- resources/views/sienna/ticketunico.blade.php
+++ resources/views/sienna/ticketunico.blade.php
@@ -318,9 +318,9 @@ function tituloiconos($iconos, $tipo)
                                         <div class="mt-5">
                                         <form method='post'action='/api/mandarmail'>
                                          @csrf
-                                        <input id="subject" class="d-none" readonly type="email" value="<?php echo $resultados[0]->emailcliente;?>">
-                                        <input id="cc2" class="d-none" readonly type="text" value="<?php echo $resultados[0]->cc;?>">
-                                        <input id="mailaeviar" class="d-none" readonly type="text" value="<?php echo $resultados[0]->emailnom;?>">
+                                        <input id="subject" class="" readonly type="email" value="<?php echo $resultados[0]->emailcliente;?>">
+                                        <input id="cc2" class="" readonly type="text" value="<?php echo $resultados[0]->cc;?>">
+                                        <input id="mailaeviar" class="" readonly type="text" value="<?php echo $resultados[0]->emailnom;?>">
                                             <div id="snow-editor" style="height: 300px;">
                                             </div>
                                             <button onclick="enviaremail2('<?php echo $resultados[0]->ticketid;?>','<?php echo $subdomain_tmp;?>','<?php echo $resultados[0]->cc;?>','<?php echo $resultados[0]->emailnom;?>')" type="button" class="btn me-2 mt-2 rounded-pill" style="background-color: #FFD193;">Responder</button> 
