17913f9713bb6a65e57574466ea188dd2c638352
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-30 21:31:48 -0300

    jjj

--- resources/views/sienna/tareas.blade.php
+++ resources/views/sienna/tareas.blade.php
@@ -118,8 +118,7 @@ document.getElementById("idagente").value = dd;
         
 
 <!-- Modal users register -->
- 
-<div id="standard-modalticketeliminar" class="modal fade standard-modalticketeliminar" tabindex="-1" role="dialog" aria-hidden="true">
+
                         <div class="modal-dialog modal-dialog-centered">
                             <div class="modal-content">
                                 <div class="modal-header">
