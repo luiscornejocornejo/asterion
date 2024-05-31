aaaa07e3dd70bd4fb55a843932bfd961e664344d
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-31 14:54:20 -0300

    eee

--- resources/views/sienna/getdata.blade.php
+++ resources/views/sienna/getdata.blade.php
@@ -50,7 +50,7 @@
                             </div>
                             <div class="container">
                                 <div class="row">
-                                    <div class="border" class="col-xs-8">
+                                    <div class="border" class="col-xs-4">
                                     <?php if(isset($datosonline)){?>
 
                                         <pre id="json"></pre>
