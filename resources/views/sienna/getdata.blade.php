f3d6cc2eb33315fe165dad2e63fba36bdb8599ab
luiscornejocornejo <kayser19992005@gmail.com>
2024-05-31 14:46:53 -0300

    hhhh

--- resources/views/sienna/getdata.blade.php
+++ resources/views/sienna/getdata.blade.php
@@ -50,7 +50,7 @@
                             </div>
                             <div class="container">
                                 <div class="row">
-                                    <div class="col-xs-6">
+                                    <div class="col-xs-12">
                                     <?php if(isset($datosonline)){?>
 
                                         <pre id="json"></pre>
@@ -61,7 +61,7 @@
                                         </script>     
                                      <?php }?>
                                     </div>
-                                    <div class="col-xs-6">
+                                    <div class="col-xs-12">
                                         TWO
                                     </div>
                                 </div>
