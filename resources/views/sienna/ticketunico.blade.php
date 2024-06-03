d5052d6db79574ad9cc4b7da60aed8544d645d7c
luiscornejocornejo <kayser19992005@gmail.com>
2024-06-03 12:15:58 -0300

    www

--- resources/views/sienna/ticketunico.blade.php
+++ resources/views/sienna/ticketunico.blade.php
@@ -580,30 +580,7 @@ function tituloiconos($iconos, $tipo)
 
                         </div>
                     </div>
-                    <div class="mt-2">
-                        <div class="card widget-flat d-none">
-                            <div class="card-body">
-                                <div class="d-flex justify-content-between">
-                                    <div>
-                                        <h4 class="fw-normal text-dark" title="Number of Customers">Notas internas</h4>
-                                    </div>
-                                    <div>
-                                    </div>
-                                </div>
-                                <hr style="margin-top: 10px;" />
-                                <div class="row">
-
-                                    
-                                       
-                                              
-                                    </div>
-                                    
-                                </div>                    
-                            </div>
-
-
-                        </div>
-                    </div>
+               
                     <?php if($urlinte2<>"luis"){?>
                         <div class="mt-2">
                         <div class="card widget-flat d-none">
