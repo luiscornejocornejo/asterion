33b4f6be7a164b1dcd5ad1f3eede7c3388380766
luiscornejocornejo <kayser19992005@gmail.com>
2024-06-13 19:40:17 -0300

    jjj

--- resources/views/facu/header2.blade.php
+++ resources/views/facu/header2.blade.php
@@ -52,32 +52,33 @@
         }
 
         function  notifyMe()  {  
-    if  (!("Notification"  in  window))  {   
-        alert("Este navegador no soporta notificaciones de escritorio");  
-    }  
-    else  if  (Notification.permission  ===  "granted")  {
-        var  options  =   {
-            body:   "Descripción o cuerpo de la notificación",
-            icon:   "url_del_icono.jpg",
-            dir :   "ltr"
-        };
-        var  notification  =  new  Notification("Hola :D", options);
-    }  
-    else  if  (Notification.permission  !==  'denied')  {
-        Notification.requestPermission(function (permission)  {
-            if  (!('permission'  in  Notification))  {
-                Notification.permission  =  permission;
+              if  (!("Notification"  in  window))  {   
+                  alert("Este navegador no soporta notificaciones de escritorio");  
+              }  
+              else  if  (Notification.permission  ===  "granted")  {
+                  var  options  =   {
+                      body:   "Descripción o cuerpo de la notificación",
+                      icon:   "url_del_icono.jpg",
+                      dir :   "ltr"
+                  };
+                  var  notification  =  new  Notification("Hola :D", options);
+              }  
+              else  if  (Notification.permission  !==  'denied')  {
+                  Notification.requestPermission(function (permission)  {
+                      if  (!('permission'  in  Notification))  {
+                          Notification.permission  =  permission;
+                      }
+                      if  (permission  ===  "granted")  {
+                          var  options  =   {
+                              body:   "Descripción o cuerpo de la notificación",
+                          icon:   "url_del_icono.jpg",
+                          dir :   "ltr"
+                          };     
+                          var  notification  =  new  Notification("Hola :)", options);
+                      }   
+                  });  
+              }
             }
-            if  (permission  ===  "granted")  {
-                var  options  =   {
-                    body:   "Descripción o cuerpo de la notificación",
-		            icon:   "url_del_icono.jpg",
-		            dir :   "ltr"
-                };     
-                var  notification  =  new  Notification("Hola :)", options);
-            }   
-        });  
-    }
 </script>
   <!-- Icons css -->
   <link href="assetsfacu/css/icons.min.css" rel="stylesheet" type="text/css">
