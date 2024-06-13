4ecec3e112a2b16968834fb0f1315a3ede97fae7
luiscornejocornejo <kayser19992005@gmail.com>
2024-06-13 19:42:00 -0300

    jjj

--- resources/views/facu/header.blade.php
+++ resources/views/facu/header.blade.php
@@ -46,34 +46,7 @@
             button.classList.toggle('rotated'); // Agrega o quita la clase 'rotated' al hacer clic
         }
 
-        function  notifyMe()  {  
-    if  (!("Notification"  in  window))  {   
-        alert("Este navegador no soporta notificaciones de escritorio");  
-    }  
-    else  if  (Notification.permission  ===  "granted")  {
-        var  options  =   {
-            body:   "Descripción o cuerpo de la notificación",
-            icon:   "url_del_icono.jpg",
-            dir :   "ltr"
-        };
-        var  notification  =  new  Notification("Hola :D", options);
-    }  
-    else  if  (Notification.permission  !==  'denied')  {
-        Notification.requestPermission(function (permission)  {
-            if  (!('permission'  in  Notification))  {
-                Notification.permission  =  permission;
-            }
-            if  (permission  ===  "granted")  {
-                var  options  =   {
-                    body:   "Descripción o cuerpo de la notificación",
-		            icon:   "url_del_icono.jpg",
-		            dir :   "ltr"
-                };     
-                var  notification  =  new  Notification("Hola :)", options);
-            }   
-        });  
-    }
-}
+      
 </script>
 <style id="apexcharts-css">@keyframes opaque {
 0% {
