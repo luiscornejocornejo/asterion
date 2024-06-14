9b41875c5b397f90e8860970a6ff37a00668b0c3
Ficuu <facuu.paez95@gmail.com>
2024-06-14 12:09:42 -0300

    giving love to its section

--- resources/views/sienna/tu/informaciononline.blade.php
+++ resources/views/sienna/tu/informaciononline.blade.php
@@ -1,25 +1,18 @@
 <div class="card widget-flat d-none">
-                            <div class="card-body">
-                                <div class="d-flex justify-content-between">
-                                    <div>
-                                        <h4 class="fw-normal text-dark" title="Number of Customers">Datos Online</h4>
-                                    </div>
-                                    <div>
-                                    </div>
-                                </div>
-                                <hr style="margin-top: 10px;" />
-                                <div class="row">
-                                <button onclick="extraordinario('<?php echo $resultados[0]->cliente;?>')" type="button" class="btn btn-success">traer datos</button>
+    <div class="card-body">
+        <div class="d-flex justify-content-between">
+            <div>
+                <h4 class="fw-normal text-dark" title="Number of Customers">Datos del usuario ERP</h4>
+            </div>
+            <div>
+                <button onclick="extraordinario('<?php echo $resultados[0]->cliente;?>')" type="button" class="btn btn-success"><i class="mdi mdi-account-search me-1"></i> Buscar datos</button>
+            </div>
+        </div>
+        <hr style="margin-top: 10px;" />
+        <div class="row">
+            <div id="datosonline">
 
-                                    <div id="datosonline" class="">
-
-                                    
-                                       
-                                              
-                                    </div>
-                                    
-                                </div>                    
-                            </div>
-
-
-                        </div>
+            </div>
+        </div>                    
+    </div>
+</div>
