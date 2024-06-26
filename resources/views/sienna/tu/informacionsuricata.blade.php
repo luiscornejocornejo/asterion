94a71e311ec2166eb261da109b129dd8af06ee4c
luiscornejocornejo <kayser19992005@gmail.com>
2024-06-26 05:26:43 -0300

    eee

--- resources/views/sienna/tu/informacionsuricata.blade.php
+++ resources/views/sienna/tu/informacionsuricata.blade.php
@@ -16,7 +16,7 @@
 
 <div class="card ">
                                 <div class="card-body">
-                                <h5 class="font-18 mb-2">Asunto: <?php echo $resultados[0]->emailcliente; ?></h5>
+                                <h5 class="font-18 mb-2">Cliente: <?php echo $resultados[0]->merchant; ?></h5>
                                     <ul class="conversation-list p-0" data-simplebar="init">
                                         <?php foreach ($resultadossuri as $valor):
                                            
