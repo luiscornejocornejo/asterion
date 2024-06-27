9cf52867d82cb30567ac6147d977a834d521f3f1
luiscornejocornejo <kayser19992005@gmail.com>
2024-06-27 09:18:13 -0300

    www

--- app/Http/Controllers/TicketdatosController.php
+++ app/Http/Controllers/TicketdatosController.php
@@ -963,7 +963,8 @@ class TicketdatosController extends Controller
             );
          
                  $url="https://suricata4.com.ar/api/closechat?token=EDElDqlQf3RDP5EDK1pHhugV9M6aCXtwAm57SD0G5JYZjw7RxwZbbfdKMhWYdUUM&idbot=".$idbot."&idconv=".$idconv."&bot_channel=".$bot_channel;
-            // Set options for the cURL request
+            dd($url);
+                 // Set options for the cURL request
             $options = array(
                 CURLOPT_URL => $url,
                 CURLOPT_RETURNTRANSFER => true,
