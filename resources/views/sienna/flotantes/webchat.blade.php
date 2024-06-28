436367d461ca879f4a805e4d934596aa68b19875
luiscornejocornejo <kayser19992005@gmail.com>
2024-06-28 11:52:34 -0300

    sss

---
+++ resources/views/sienna/flotantes/webchat.blade.php
@@ -0,0 +1,58 @@
+<!-- Modal for Create Ticket -->
+<?php
+$querygenerico="select * from empresa";
+$siennadeptosgenericos = DB::select($querygenerico);
+$uri="";
+foreach($siennadeptosgenericos as $val){
+    $uri=$val->botfrontdesk;
+}
+?>
+
+<script>
+
+    function topics(id){
+        var URLactual = window.location.href;
+        var porciones = URLactual.split('.');
+        let result = porciones[0].replace("https://", "");
+        let opt="";
+        url = "https://"+result+".suricata.cloud/api/topicxdepto?depto=" + id + "";
+        console.log(url);
+        axios.get(url)
+            .then(function (response) {
+                console.log(response);
+               
+                tt = "";
+                for (i = 0; i < response.data.length; i++) {
+                        console.log(response.data[i].nombre);
+                        tt+='<option  value='+response.data[i].id+'>'+response.data[i].nombre+'</option>';
+
+                } 
+                document.getElementById("top").innerHTML = null;
+                document.getElementById("top").innerHTML = tt;
+            })
+            .catch(function (error) {
+                // función para capturar el error
+                console.log(error);
+            })
+            .then(function () {
+                // función que siempre se ejecuta
+            });
+    }
+</script>
+    
+<div class="modal fade" id="warning-alert-modal2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel"tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content">
+            <div class="modal-header bg-dark">
+                <h5 class="modal-title text-white" id="exampleModalToggleLabel">Web Chat</h5>
+               
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+           
+           <?php echo $uri;?>
+        </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+            
--- resources/views/sienna/supervisor.blade.php
+++ resources/views/sienna/supervisor.blade.php
@@ -661,6 +661,7 @@
                     <div id="menu" class="hidden">
                     <button  id="button-2" class="mdi mdi-ticket-account bg-warning text-light" data-bs-toggle="modal" href="#exampleModalToggle" >
                     <button id="button-3" class="mdi mdi-send bg-success text-light " href="#warning-alert-modal" data-bs-toggle="modal"></button>
+                    <button id="button-4" class="mdi mdi-send bg-success text-light " href="#warning-alert-modal2" data-bs-toggle="modal"></button>
                     </div>
                 </div>
                
@@ -683,6 +684,7 @@
       
       @include('sienna.flotantes.creartickets')
       @include('sienna.flotantes.crearticketsnc')
+      @include('sienna.flotantes.webchat')
       @include('sienna.flotantes.salientes')
     </div>
   <!-- END wrapper -->
