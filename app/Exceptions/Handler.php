7cbc00462512613d6b62e47cc9aef62355eaecc4
luiscornejocornejo <kayser19992005@gmail.com>
2024-06-27 23:47:19 -0300

    sss

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,4 +47,15 @@ class Handler extends ExceptionHandler
             //
         });
     }
+    public function render($request, Exception $exception)
+{
+    // Render well-known exceptions here
+
+    // Otherwise display internal error message
+    if(!env('APP_DEBUG', false)){
+        return view('errors.500');
+    } else {
+        return parent::render($request, $exception);
+    }
+}
 }
