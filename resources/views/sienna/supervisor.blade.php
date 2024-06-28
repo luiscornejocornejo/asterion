cde2566a921f1da9c2fdb43e6b79f73fb7bcaae9
luiscornejocornejo <kayser19992005@gmail.com>
2024-06-28 12:10:40 -0300

    hhh

--- resources/views/sienna/supervisor.blade.php
+++ resources/views/sienna/supervisor.blade.php
@@ -661,7 +661,17 @@
                     <div id="menu" class="hidden">
                     <button  id="button-2" class="mdi mdi-ticket-account bg-warning text-light" data-bs-toggle="modal" href="#exampleModalToggle" >
                     <button id="button-3" class="mdi mdi-send bg-success text-light " href="#warning-alert-modal" data-bs-toggle="modal"></button>
+                    <?php
+                        $querygenerico="select * from empresa";
+                        $siennadeptosgenericos = DB::select($querygenerico);
+                        $uribotfrontdesk="";
+                        foreach($siennadeptosgenericos as $val){
+                            $uribotfrontdesk=$val->botfrontdesk;
+                        }
+                        ?>
+                        <?php if($botfrontdesk<>""){?>
                     <button id="button-4" class="mdi mdi-storefront bg-primary text-light " href="#warning-alert-modal2" data-bs-toggle="modal"></button>
+                    <?php }?>
                     </div>
                 </div>
                
