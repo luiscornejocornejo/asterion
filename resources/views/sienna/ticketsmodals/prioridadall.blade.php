bf460e09f6f2dcb5f4c1c84b23ea4fa1f4ef0555
Ficuu <facuu.paez95@gmail.com>
2024-07-03 15:46:02 -0300

    Changing style on priorities

--- resources/views/sienna/ticketsmodals/prioridadall.blade.php
+++ resources/views/sienna/ticketsmodals/prioridadall.blade.php
@@ -1,37 +1,29 @@
-
-
-
 <div class="modal fade" id="bs-example-modal-prioridadall" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-sm">
-            <div class="modal-content">
-                <div class="modal-header bg-dark text-white">
-                    <h4 class="modal-title" id="mySmallModalLabel">Prioridad ticket all</h4>
-                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-hidden="true"></button>
-                </div>
-                <form method="post" action="/prioridadsiennaall">
-                    <div class="modal-body">
-                    Cambiar la Prioridad de los tickets             
-                      
-                  
-                    <div class="modal-footer">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header bg-dark text-white">
+                <h4 class="modal-title" id="mySmallModalLabel">Cambiar prioridad</h4>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-hidden="true"></button>
+            </div>
+            <form method="post" action="/prioridadsiennaall">
+                <div class="modal-body">
                     @csrf
                     <input value="" type="hidden" name="idticketpedir" id="idticketpedir2">
-                            <input value="<?php echo session('nombreusuario');?>" type="hidden" name="logeado" id="logeado">
-                            <input value="" readonly type="hidden" name="ticketss" id="tp">
+                    <input value="<?php echo session('nombreusuario'); ?>" type="hidden" name="logeado" id="logeado">
+                    <input value="" readonly type="hidden" name="ticketss" id="tp">
 
-                            <div v-for="department in departments ">
-                            <?php foreach($prioridades as $dep){?>
-                                <input value="<?php echo $dep->id;?>" class="form-radio" type="radio" name="statos">&nbsp;
-                            <span class=" fw-bold" style="color: #98a6ad;font-size: 12px;"><?php echo$dep->nombre;?></span>
-                                <br><br>
-
-                                <?php }?>   
-                                </div>
-                                </div>
-                            
+                    <div v-for="department in departments ">
+                        <?php foreach ($prioridades as $dep) { ?>
+                            <input value="<?php echo $dep->id; ?>" id="<?php echo $dep->id; ?>" class="form-check-input" type="radio" name="statos">
+                            <label for="<?php echo $dep->id; ?>" class="form-check-label"><?php echo $dep->nombre; ?></label>
+                        <?php } ?>
+                    </div>
+                    <div class="modal-footer">
                         <button type="submit" class="btn btn-success">Si</button>
                     </div>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
-        </div></div>
+    </div>
+</div>
+</div>
