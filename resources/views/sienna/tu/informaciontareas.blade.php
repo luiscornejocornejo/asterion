8cf0e28be8002b89f2f2f989c3cc8c0b9a07a7d2
Ficuu <facuu.paez95@gmail.com>
2024-07-03 12:10:50 -0300

    change position button for create task

--- resources/views/sienna/tu/informaciontareas.blade.php
+++ resources/views/sienna/tu/informaciontareas.blade.php
@@ -1,48 +1,47 @@
 <div class="card widget-flat">
-                            <div class="card-body">
-                                <div class="d-flex justify-content-between">
-                                    <div>
-                                        <h4 class="fw-normal text-dark" title="Number of Customers">Tareas del tickets</h4>
-                                    </div>
-                                    <div>
-                                        <i class="mdi mdi-history widget-icon bg-secondary-lighten text-secondary"></i>
-                                    </div>
-                                </div>
-                                <hr style="margin-top: 10px;" />
-                                <div class="row">
-                                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-sm-12">
-                                        
-                                    <button  class="btn btn-success" type="button" data-bs-toggle="modal" data-bs-target="#create-task">
-                            <i class="mdi mdi-check-circle" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="mb-1" data-bs-title="Crear Tarea."></i>
-                        </button>
-                                        <table id="example"  class="table table-striped dt-responsive nowrap w-100 text-light">
-                                            <thead>
-                                                    <tr class="text-center bg-dark" >                             
-                                                        <th class="text-light">Nombre</th>
-                                                        <th class="text-light">Descripcion</th>
-                                                        <th class="text-light">Estado</th>
-                                                        <th class="text-light">Usuario</th>
-                                                        <th class="text-light">Fecha Limite</th>
-                                                    </tr>
-                                            </thead>
-                                            <tbody id="tb">
-                                                    <?php foreach($resultadostareas as $valh){?>
-                                                        <tr class="text-center">
-                                                        <td><?php echo $valh->nombre;?></td>
-                                                        <td><?php echo $valh->descripcion;?></td>
-                                                        <td><?php echo $valh->estadoname;?></td>
-                                                        <td><?php echo $valh->usuario;?></td>
-                                                        <td><?php echo $valh->fechalimite;?></td>
-                                                    </tr>
-                                                        <?php  }?>
-                                            </tbody>
-                                            </table>
-                                              
-                                    </div>
-                                    
-                                </div>                    
-                            </div>
+    <div class="card-body">
+        <div class="d-flex justify-content-between">
+            <div>
+                <h4 class="fw-normal text-dark" title="Number of Customers">Tareas del tickets</h4>
+            </div>
+            <div>
+                <button class="btn btn-secondary" type="button" data-bs-toggle="modal" data-bs-target="#create-task">
+                    <i class="mdi mdi-clipboard-list me-1" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="mb-1" data-bs-title="Crear Tarea."></i>
+                </button>
+                Crear tarea
+            </div>
+        </div>
+        <hr style="margin-top: 10px;" />
+        <div class="row">
+            <div class="col-xxl-12 col-xl-12 col-lg-12 col-sm-12">
+                <table id="example" class="table table-striped dt-responsive nowrap w-100 text-light">
+                    <thead>
+                        <tr class="text-center bg-dark">
+                            <th class="text-light">Nombre</th>
+                            <th class="text-light">Descripcion</th>
+                            <th class="text-light">Estado</th>
+                            <th class="text-light">Usuario</th>
+                            <th class="text-light">Fecha Limite</th>
+                        </tr>
+                    </thead>
+                    <tbody id="tb">
+                        <?php foreach ($resultadostareas as $valh) { ?>
+                            <tr class="text-center">
+                                <td><?php echo $valh->nombre; ?></td>
+                                <td><?php echo $valh->descripcion; ?></td>
+                                <td><?php echo $valh->estadoname; ?></td>
+                                <td><?php echo $valh->usuario; ?></td>
+                                <td><?php echo $valh->fechalimite; ?></td>
+                            </tr>
+                        <?php  } ?>
+                    </tbody>
+                </table>
 
-                            @include('sienna.tu.creartareaticket')
+            </div>
 
-                        </div>
+        </div>
+    </div>
+
+    @include('sienna.tu.creartareaticket')
+
+</div>
