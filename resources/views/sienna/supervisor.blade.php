cefb1b9b8038d3779eb33253ddbe4771e27abf9f
Ficuu <facuu.paez95@gmail.com>
2024-07-04 11:36:55 -0300

    show buttons to export and rollback pageLength to 25 tickets

--- resources/views/sienna/supervisor.blade.php
+++ resources/views/sienna/supervisor.blade.php
@@ -585,6 +585,7 @@
                     "language" : {
                       "url": "//cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json"
                     },
+                    dom: 'Bfrtip',
                     buttons: [
                         'copy', 'csv', 'excel', 'pdf', 'print'
                     ],
