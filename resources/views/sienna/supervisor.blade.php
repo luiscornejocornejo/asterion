ef348171bf999ee6d9467eb4d0940f5039a8c662
luiscornejocornejo <kayser19992005@gmail.com>
2024-07-03 22:17:26 -0300

    jjj

--- resources/views/sienna/supervisor.blade.php
+++ resources/views/sienna/supervisor.blade.php
@@ -58,6 +58,7 @@
         let iconos = {!! json_encode($iconos,JSON_FORCE_OBJECT) !!};
         let usersmerchant = {!! json_encode($usersmerchant,JSON_FORCE_OBJECT) !!};
         let pri = {!! json_encode($prioridades,JSON_FORCE_OBJECT) !!};
+        let extras = {!! json_encode($resultadosextras,JSON_FORCE_OBJECT) !!};
 
 
 
