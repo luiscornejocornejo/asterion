7687102dddad779e486c22ea58389a764d22c8df
Ficuu <facuu.paez95@gmail.com>
2024-07-10 11:59:48 -0300

    creating component about_task

--- resources/views/sienna/tareas/abouttask.blade.php
+++ resources/views/sienna/tareas/abouttask.blade.php
@@ -1,70 +1,58 @@
-<?php 
-                    
-                    foreach ($datos2 as $val){
-                        echo "<p>id: ".$val->id."</p>";
-                        echo "<br>";
-                        echo "<p>nombre: ".$val->nombre."</p>";
-                        echo "<br>";
-                        echo "<p>Descripcion: ".$val->descripcion."</p>";
-                        echo "<br>";
-                        echo "<p>usuario: ".$val->users."</p>";
-                        echo "<br>";
-                        echo "<p>ticket: ".$val->siennatickets."</p>";
-                        echo "<br>";
-                        echo "<p>F. limite: ".$val->fechalimite."</p>";
-                        echo "<br>";
-                        echo "<p>creado: ".$val->created_at."</p>";
-                        echo "<br>";
-
-                        echo "<select name='estado'>";
-                        foreach($datos3 as $val3){
-                            if($val3->id==$val->estadotarea){
-                                $selected=" selected ";
-
-                            }else{
-                                $selected="";
-
-                            }
-                            echo "<option ".$selected."  value='".$val3->id."'>".$val3->nombre."</option>";
-                        }
-                        echo "</select>";
-                        
-                    }?>
-
 <div class="card widget-flat">
     <div class="card-body">
         <div class="d-flex justify-content-between">
-            <div>
-                <h4 class="fw-normal text-dark" title="Number of Customers">Información del Ticket #<?php echo "1"; ?></h4>
-            </div>
-            <div>
-                <i class="mdi mdi-note-text widget-icon bg-secondary-lighten text-secondary"></i>
-            </div>
+            <?php foreach ($datos2 as $val) { ?>
+                <div>
+                    <h4 class="fw-normal text-dark" title="Number of Customers">Información de tarea #<?php echo $val->id; ?></h4>
+                </div>
+                <div>
+                    <i class="mdi mdi-note-text widget-icon bg-secondary-lighten text-secondary"></i>
+                </div>
         </div>
         <hr style="margin-top: 10px;" />
         <div class="d-flex justify-content-between">
             <div>
                 <div class="mb-1">
-                    <i class="mdi mdi-list-status"></i> <strong>Estado: </strong><?php echo "2"; ?>
+                    <i class="mdi mdi-list-status"></i> <strong>Estado: </strong><?php echo $val->nombre; ?>
                 </div>
                 <div class="mb-1">
-                    <i class="mdi mdi-office-building"></i> <strong>Departamento: </strong><?php echo "3"; ?>
+                    <i class="mdi mdi-office-building"></i> <strong>Departamento: </strong><?php echo $val->descripcion; ?>
                 </div>
                 <div class="mb-1">
-                    <i class="mdi mdi-account-voice"></i> <strong>Asignado a: </strong><?php echo "4"; ?>
+                    <i class="mdi mdi-account-voice"></i> <strong>Asignado a: </strong><?php echo $val->created_at; ?>
                 </div>
 
             </div>
 
             <div>
                 <div class="mb-1">
-                    <i class="mdi mdi-calendar"></i> <strong>Creado: </strong><?php echo "5"; ?>
+                    <i class="mdi mdi-calendar"></i> <strong>Ticket de referencia: </strong><?php echo $val->siennatickets; ?>
                 </div>
-                
+                <div class="mb-1">
+                    <i class="mdi mdi-calendar"></i> <strong>Expira el: </strong><?php echo $val->fechalimite; ?>
+                </div>
+                <div class="mb-1">
+                    <i class="mdi mdi-calendar"></i> <strong>Creado: </strong><?php echo $val->fechalimite; ?>
+                </div>
+                <div class="mb-1">
+                    <i class="mdi mdi-calendar"></i> <strong>Estado: </strong>
+                    <select name='estado' class='form-control'>
+                        <?php
+                        foreach ($datos3 as $val3) {
+                            if ($val3->id == $val->estadotarea) {
+                                $selected = "selected";
+                            } else {
+                                $selected = "";
+                            }
+                            echo "<option " . $selected . "  value='" . $val3->id . "'>" . $val3->nombre . "</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+
             </div>
         </div>
-
-
+    <?php } ?>
     </div>
 
 </div>
