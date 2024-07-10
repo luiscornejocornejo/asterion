6db33d762a04874187c4b63cc6f26318751f3409
Ficuu <facuu.paez95@gmail.com>
2024-07-10 12:15:04 -0300

    icons changes and texts of task

--- resources/views/sienna/tareas/abouttask.blade.php
+++ resources/views/sienna/tareas/abouttask.blade.php
@@ -13,29 +13,29 @@
         <div class="d-flex justify-content-between">
             <div>
                 <div class="mb-1">
-                    <i class="mdi mdi-list-status"></i> <strong>Estado: </strong><?php echo $val->nombre; ?>
+                    <i class="mdi mdi-list-status"></i> <strong>Nombre: </strong><?php echo $val->nombre; ?>
                 </div>
                 <div class="mb-1">
-                    <i class="mdi mdi-office-building"></i> <strong>Departamento: </strong><?php echo $val->descripcion; ?>
+                    <i class="mdi mdi-format-list-numbered"></i> <strong>Descripción: </strong><?php echo $val->descripcion; ?>
                 </div>
                 <div class="mb-1">
-                    <i class="mdi mdi-account-voice"></i> <strong>Asignado a: </strong><?php echo $val->created_at; ?>
+                    <i class="mdi mdi-account-voice"></i> <strong>Agente asignado: </strong><?php echo $val->users; ?>
                 </div>
 
             </div>
 
             <div>
                 <div class="mb-1">
-                    <i class="mdi mdi-calendar"></i> <strong>Ticket de referencia: </strong><?php echo $val->siennatickets; ?>
+                    <i class="mdi mdi-ticket-confirmation"></i> <strong>Ticket de referencia: </strong><?php echo $val->siennatickets; ?>
                 </div>
                 <div class="mb-1">
-                    <i class="mdi mdi-calendar"></i> <strong>Expira el: </strong><?php echo $val->fechalimite; ?>
+                    <i class="mdi mdi-calendar-clock-outline"></i> <strong>Expira el: </strong><?php echo $val->fechalimite; ?>
                 </div>
                 <div class="mb-1">
-                    <i class="mdi mdi-calendar"></i> <strong>Creado: </strong><?php echo $val->fechalimite; ?>
+                    <i class="mdi mdi-calendar"></i> <strong>Creado el </strong><?php echo $val->created_at; ?>
                 </div>
                 <div class="mb-1">
-                    <i class="mdi mdi-calendar"></i> <strong>Estado: </strong>
+                    <i class="mdi-format-list-checks"></i> <strong>Estado: </strong>
                     <select name='estado' class='form-control'>
                         <?php
                         foreach ($datos3 as $val3) {
