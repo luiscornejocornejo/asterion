9cb2e56d3f77481a4b841dbf4f321da5b0c0f0a9
Ficuu <facuu.paez95@gmail.com>
2024-07-11 16:33:21 -0300

    add modal and button for create ticket

--- resources/views/sienna/soporte.blade.php
+++ resources/views/sienna/soporte.blade.php
@@ -1,15 +1,31 @@
-
-
-
 @include('facu.header')
-
-
-
 <div class="wrapper menuitem-active">
     @include('facu.menu')
-    <div class="content-page" style="padding: 0!important;">
+    <style>
+        .background-buttons :hover{
+        background-color: #FFD193!important;
+        color: #3C4665!important;
+        }
+        .newTicket {
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        z-index: 9999;
+        cursor: pointer;
+        color: #7a7a7a;
+        background-color: #FFD193;
+        border-radius: 50%;
+        text-align: center;
+        width: 50px;
+        height: 50px;
+        line-height: 50px;
+        font-size: 16px;
+        box-shadow: 2px 2px 3px #999;
+        }
+    </style>
+    <div class="content-page">
 
-        <div class="container-fluid pt-5">
+        <div class="container-fluid">
             <style>
                 .conversation-list .odd .conversation-text {
                     float: right !important;
@@ -26,37 +42,35 @@
                 }
             </style>
 
-                <h1>Soporte</h1>
-
-                <table id="example" class="table dt-responsive nowrap w-100">
-                    <thead class="bg-dark">
+            <h2>Ticket Soporte Suricata</h2>
+            <table id="example" class="table dt-responsive nowrap w-100">
+                <thead class="bg-dark">
+                    <tr class="text-center">
+                        <th class="text-light">#</th>
+                        <th class="text-light">Motivo</th>
+                        <th class="text-light">Descripcion</th>
+                        <th class="text-light">Estado</th>
+                        <th class="text-light">Fecha de creación</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($tsoporte as $val3) { ?>
                         <tr class="text-center">
-                            <th class="text-light">#</th>
-                            <th class="text-light">Motivo</th>
-                            <th class="text-light">Descripcion</th>
-                            <th class="text-light">Estado</th>
-                            <th class="text-light">Fecha de creación</th>
+                            <td><a href="{{ url('soporte?ticket=' . $val3->id) }}" target="_blank">{{$val3->id}}</a></td>
+                            <td>{{$val3->nya}}</td>
+                            <td>{{$val3->cliente}}</td>
+                            <td>{{$val3->siennaestado}}</td>
+                            <td>{{$val3->siennatopic}}</td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($tsoporte as $val3) { ?>
-                            <tr class="text-center">
-                                <td><a href="{{ url('soporte?ticket=' . $val3->id) }}" target="_blank">{{$val3->id}}</a></td>
-                                <td>{{$val3->nya}}</td>
-                                <td>{{$val3->cliente}}</td>
-                                <td>{{$val3->siennaestado}}</td>
-                                <td>{{$val3->siennatopic}}</td>
-                            </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
+                    <?php } ?>
+                </tbody>
+            </table>
 
         </div>
-        <div class="newAgent" data-bs-toggle="modal" data-bs-target="#create-ticket">
+        <div class="newTicket" data-bs-toggle="modal" data-bs-target="#create-ticket">
             <i class="ri-chat-new-fill" style="font-size: 25px;"></i>
         </div>
 
     </div>
 </div>
-<br><br><br>
 @include('facu.footer')
