c7606b6c747d9fd30b78cd31c965240965379130
Facundo Paez <facuupaez95@gmail.com>
2024-07-16 15:17:55 -0300

    add controller update task

--- routes/web.php
+++ routes/web.php
@@ -269,7 +269,7 @@ Route::get('/envios','App\Http\Controllers\cloudtickets@envios');
 Route::get('/enviolistado','App\Http\Controllers\cloudtickets@enviolistado');
 Route::get('/getsaliente','App\Http\Controllers\cloudtickets@getsaliente');
 
-Route::post('/actualizartarea','App\Http\Controllers\cloudtickets@actualizatarea');
+Route::post('/actualizartarea','App\Http\Controllers\cloudtickets@actualizartarea');
 
 Route::post('/nuevost','App\Http\Controllers\cloudtickets@nuevost');
 
