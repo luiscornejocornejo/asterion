3e58ff2e1c9176549c1a50af4715d82ff2324ac8
Facundo Paez <facuupaez95@gmail.com>
2024-07-16 16:45:41 -0300

    add salientes

--- resources/views/sienna/salientesfacu.blade.php
+++ resources/views/sienna/salientesfacu.blade.php
@@ -3,77 +3,112 @@
 
 
 <div class="wrapper menuitem-active">
-@include('facu.menu')
+    @include('facu.menu')
     <div class="content-page" style="padding: 0!important;">
-    @if ($message = Session::get('success'))
+        @if ($message = Session::get('success'))
         <div class="alert alert-success alert-dismissible fade
                             show" role="alert">
             {{ $message }}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
         @endif
-        <div class="container-fluid pt-2">
-
-                        <div class="row">
-                        
-                            <div class="col-4 mb-2">
-                                <label for="example-select" class="form-label">Template</label>
-                                <select onchange="campos(this)" class="form-select" name="estado" id="example-select">
-                                <option >seleccionar</option>
-                                <?php foreach($listadopadre as $ll){?>
-                                    <option value="<?php echo $ll->id."|".$ll->parametros;?>"><?php echo $ll->nombre;?></option>
-                                <?php }?>
-                                </select>
-                                <script>
-                                    function campos(id){
-                                        separar=id.value.split('|');
-                                        var template = document.getElementById('template');
-                                        template.value=separar[0];
-                                        var element = document.getElementById('carga');
-                                        element.classList.remove('d-none');
-                                        var headerInput = document.getElementById('headerInput');
-                                        const columnas = separar[1].replace(/;/g, ',');
-
-                                        headerInput.value="cel,"+columnas;
-
-                                    }
-
-                                </script>
-                            </div>
-                        
-                            
-
+        <div class="container pt-5">
+            <div class="">
+                <div class="card">
+                    <h6 class="card-header bg-light">Saliente Masivo</h6>
+                    <div class="card-body">
+                        <h5 class="card-title">1. ¿Qué variables tiene tu documento?</h5>
+                        <p class="card-text">
+                            Antes de que realices la carga del documento debes indicar las variables (nombre, teléfono, etc.), el dato que contiene y a qué columna pertenece. Las variables marcadas con <span class="text-primary">*</span> son obligatorias<br>
+                            <a href="#" rel="no-referrer">En este enlace puedes consultar el siguiente artículo para completar los campos en el documento</a>.
+                        </p>
+                        <p class="card-text mt-3">2. Seleccione la plantilla que deseas enviar.</p>
+                        <select class="form-select w-25">
+                            <option selected disabled>Listado de plantillas</option>
+                            <option value="$id">Bienvenida</option>
+                        </select>
+                        <p class="card-text mt-3">Puedes descargar el modelo de planilla de datos desde <a href="#">aquí</a>.</p>
+                        <table class="table table-centered mb-0">
+                            <thead>
+                                <tr>
+                                    <th>Columna en documento</th>
+                                    <th>Dato</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Columna A:</td>
+                                    <td>data</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div>
+                            <p class="card-text mt-3">3. Suba el documento con el listado de los usuarios a contactar.</p>
+                            <label for="inputFile" class="btn btn-primary rounded-pill">
+                                <i class="fas fa-upload"></i> Documento requerido
+                                <input type="file" id="inputFile" class="d-none">
+                            </label>
+                            <span id="fileName" class="ms-1"></span>
+                            <p class="card-text text-black mt-3"><strong>Resumen:</strong></p>
+                            <p>Total de usuarios en el documento: <span id="rowCount"></span></p>
+                            <a role="button" data-bs-toggle="modal" data-bs-target="#preview" class="text-primary">Ver listado de usuarios cargados</a>
                         </div>
-                        <div id="carga" class="row d-none">
-
-                                <form  enctype="multipart/form-data" action="/salientesb" method="post" class="dropzone" id="myAwesomeDropzone" data-plugin="dropzone" data-previews-container="#file-previews"
-                                    data-upload-preview-template="#uploadPreviewTemplate">
-                                    @csrf
-
-                                    <div class="fallback">
-                                        <input name="logo" id="inputFile" type="file" multiple />
-                                    </div>
-                                    <span id="valoresview"></span>
-                                    <input type="hidden" name="cantvalores" value="" id="headerInput" placeholder="Nombres de las cabeceras (separados por comas)" />
-                                    <input type="hidden" value="" name="template" id="template" placeholder="Nombres de las cabeceras (separados por comas)" />
-                                    <div id="recordCount"></div>
-                                    <table class="table table-striped display responsive nowrap w-100 table-bordered" id="excelTable" >
-                                    </table>
-                                    <div class="col-lg-4 col-sm-12">
-                                                            <div class="mb-2 position-relative">
-                                                                <label class="form-label">&nbsp;</label>
-                                                                <input type="submit" type="button" class="form-control w-25 bg-success text-light" value="Cambiar">
-                                                            </div>
-                                                        </div>
-                                
-                                </form>
-
-                        </div> 
-        </div> 
+                        <div class="container d-flex justify-content-end">
+                            <button class="btn btn-light rounded me-2 d-none">Cancelar</button>
+                            <button class="btn btn-primary rounded" data-bs-toggle="modal" data-bs-target="#confirm-outbound">Enviar</button>
+                        </div>
+                    </div> <!-- end card-body-->
+                </div> <!-- end card-->
+            </div>
+        </div>
     </div>
-    
+
+    <!-- Modal Preview -->
+    <div class="modal fade" id="preview" tabindex="-1" role="dialog" aria-labelledby="modalTitlePreview" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header bg-dark text-light">
+                    <h5 class="modal-title" id="modalTitlePreview">Listado de usuarios</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-hidden="true"></button>
+                </div>
+                <div class="modal-body">
+                    <table id="example" class="table table-bordered table-centered table-hover  responsive nowrap w-100">
+                        <thead id="tableHead"></thead>
+                        <tbody id="tableBody"></tbody>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-light border" data-bs-dismiss="modal">Cerrar</button>
+
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div>
+    <!-- End Modal Preview -->
+
+    <!-- Modal Confirm Outbound -->
+
+    <div class="modal fade" id="confirm-outbound" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-sm">
+            <div class="modal-content">
+                <div class="modal-header bg-dark text-light">
+                    <h4 class="modal-title" id="mySmallModalLabel">Confirmación de saliente</h4>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-hidden="true"></button>
+                </div>
+                <div class="modal-body">
+                    <p>¿Confirma el envío masivo?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-light border" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-primary" id="send-outbound">Si, enviar</button>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div>
+    <!-- End of Modal Confirm Outbound -->
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.0/xlsx.full.min.js"></script>
- 
+
     <script>
         document.getElementById('inputFile').addEventListener('change', function(event) {
             const file = event.target.files[0];
@@ -81,42 +116,58 @@
 
             reader.onload = function(event) {
                 const data = new Uint8Array(event.target.result);
-                const workbook = XLSX.read(data, { type: 'array' });
+                const workbook = XLSX.read(data, {
+                    type: 'array'
+                });
 
+                // Supongamos que queremos leer la primera hoja
                 const firstSheetName = workbook.SheetNames[0];
                 const worksheet = workbook.Sheets[firstSheetName];
 
-                const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-
-                const table = document.getElementById('excelTable');
-                table.innerHTML = ''; // Limpiar tabla antes de agregar datos
-
-                const headerInput = document.getElementById('headerInput').value;
-                const headers = headerInput ? headerInput.split(',') : jsonData[0];
-
-                const headerRow = document.createElement('tr');
-                headers.forEach(header => {
-                    const th = document.createElement('th');
-                    th.textContent = header.trim();
-                    headerRow.appendChild(th);
-                });
-                table.appendChild(headerRow);
-
-                jsonData.slice(1).forEach(row => {
-                    const tr = document.createElement('tr');
-                    row.forEach(cell => {
-                        const td = document.createElement('td');
-                        td.textContent = cell;
-                        tr.appendChild(td);
-                    });
-                    table.appendChild(tr);
+                // Convertir la hoja a JSON
+                const json = XLSX.utils.sheet_to_json(worksheet, {
+                    header: 1
                 });
 
-                document.getElementById('recordCount').textContent = `Cantidad de registros: ${jsonData.length - 1}`;
+                // Crear la tabla
+                createTableFromExcel(json);
+
+                // Mostrar la cantidad de filas
+                const rowCount = json.length - 1; // Restamos 1 para no contar el encabezado
+                document.getElementById('rowCount').textContent = `${rowCount}`;
             };
 
             reader.readAsArrayBuffer(file);
-            document.getElementById('valoresview').textContent=document.getElementById('headerInput').value;
         });
+
+        function createTableFromExcel(data) {
+            const tableHead = document.getElementById('tableHead');
+            const tableBody = document.getElementById('tableBody');
+
+            // Limpiar la tabla existente
+            tableHead.innerHTML = '';
+            tableBody.innerHTML = '';
+
+            // Crear el encabezado de la tabla
+            const headerRow = document.createElement('tr');
+            data[0].forEach(headerText => {
+                const header = document.createElement('th');
+                header.textContent = headerText;
+                headerRow.appendChild(header);
+            });
+            tableHead.appendChild(headerRow);
+
+            // Crear el cuerpo de la tabla
+            data.slice(1).forEach(rowData => {
+                const row = document.createElement('tr');
+                rowData.forEach(cellData => {
+                    const cell = document.createElement('td');
+                    cell.textContent = cellData;
+                    row.appendChild(cell);
+                });
+                tableBody.appendChild(row);
+            });
+        }
     </script>
-@include('facu.footer')
+
+    @include('facu.footer')
