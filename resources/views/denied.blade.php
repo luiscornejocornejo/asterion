46514bafc0cfaf8fa70c0fdb8b9d4b117d608809
luiscornejocornejo <kayser19992005@gmail.com>
2024-07-17 02:16:59 -0300

    eee

--- resources/views/denied.blade.php
+++ resources/views/denied.blade.php
@@ -32,8 +32,8 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="text-center mb-5">
-                            <h1 class="display-1 fw-semibold">4<span class="text-primary mx-2">0</span>4</h1>
-                            <h4 class="text-uppercase">Sorry, page not found</h4>
+                            <h1 class="display-1 fw-semibold">4<span class="text-primary mx-2">0</span></h1>
+                            <h4 class="text-uppercase">No tenes permiso para ver esta pagina</h4>
                             <div class="mt-5 text-center">
                                 <a class="btn btn-primary waves-effect waves-light" href="/">Back to Dashboard</a>
                             </div>
