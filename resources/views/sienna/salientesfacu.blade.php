d80afadfbf2f2aa1ee8021ee225d5bd20201a918
Ficuu <facuu.paez95@gmail.com>
2024-07-17 15:12:56 -0300

    check button for upload file and show name file

--- resources/views/sienna/salientesfacu.blade.php
+++ resources/views/sienna/salientesfacu.blade.php
@@ -95,6 +95,8 @@
                                 <script>
                                     document.getElementById('inputFile').addEventListener('change', function(event) {
                                         const file = event.target.files[0];
+                                        document.getElementById('fileName').textContent = file
+                                        
                                         const reader = new FileReader();
 
                                         reader.onload = function(event) {
