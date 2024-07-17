7c93845f37000ac0dcd14ec8852b02fc4514d7af
luiscornejocornejo <kayser19992005@gmail.com>
2024-07-17 16:39:53 -0300

    eee

--- resources/views/sienna/extrastickets.blade.php
+++ resources/views/sienna/extrastickets.blade.php
@@ -7,15 +7,15 @@
             <h2>extrastickets</h2>
            
                     <?php
-                    foreach ($tsoporte as $val3) {
+                   // foreach ($tsoporte as $val3) {
 
                     ?>
                        
-                       <p>extra1: <input type="text" name="extra1" value="{{$val3->extra1}}"></p>
-                       <p>extra2: <input type="text" name="extra2" value="{{$val3->extra2}}"></p>
-                       <p>extra3: <input type="text" name="extra3" value="{{$val3->extra3}}"></p>
+                       <p>extra1: <input type="text" name="extra1" value="{{$tsoporte[0]->nombre}}"></p>
+                       <p>extra2: <input type="text" name="extra2" value="{{$tsoporte[1]->nombre}}"></p>
+                       <p>extra3: <input type="text" name="extra3" value="{{$tsoporte[2]->nombre}}"></p>
                         
-                    <?php } ?>
+                    <?php //} ?>
               
         </div>
     </div>
