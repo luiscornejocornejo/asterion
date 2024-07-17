de68f0c5bafc975618b4dda6f66bd3a4c52584c5
Ficuu <facuu.paez95@gmail.com>
2024-07-17 18:02:59 -0300

    upload view salienteWispro

--- routes/web.php
+++ routes/web.php
@@ -279,7 +279,7 @@ Route::get('/salientesc','App\Http\Controllers\cloudtickets@salientesc')->middle
 Route::get('/extrastickets','App\Http\Controllers\cloudtickets@extrastickets')->middleware('adminsienna');
 
 Route::get('/salientes/wispro', function () {
-    return view('/sienna/salienteWispro')
+    return view('/sienna/salienteWispro');
 });
 
 Route::get('/userprofile', function () {
