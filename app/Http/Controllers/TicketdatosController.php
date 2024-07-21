78ca4dbc6450339d9255ddae9f2ca4bbc0586d08
luiscornejocornejo <kayser19992005@gmail.com>
2024-07-21 06:13:44 -0300

    hhh

--- app/Http/Controllers/TicketdatosController.php
+++ app/Http/Controllers/TicketdatosController.php
@@ -1620,7 +1620,7 @@ class TicketdatosController extends Controller
         $tick=$request->tick;
         $query="select *,a.conversation_id,a.user_id,concat(e.nombre,' ',e.last_name) as nombreagente,
         b.nombre as depto,b.id as iddepto,g.nombre as nombreprioridad,g.colore as colorprioridad,
-        a.id as ticketid,c.nombre estadoname,d.nombre topicname,a.cel numerocel,a.asignado,a.email eltexto,a.nya emailnom,a.cliente emailcliente,
+        a.id as ticketid,c.nombre estadoname,d.nombre topicname,a.cel numerocel,a.asignado,a.email as asunto,a.nya emailnom,a.cliente emailcliente,
         convertirTiempo(a.created_at  ) as creacion from 
         ".$subdomain_tmp.".siennatickets a
         left join ".$subdomain_tmp.".siennadepto b on b.id=a.siennadepto 
