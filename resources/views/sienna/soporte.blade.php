ea76918f6bfc7fdfb5de5ea354a808d18412d318
luiscornejocornejo <kayser19992005@gmail.com>
2024-07-22 14:25:24 -0300

    eee

--- resources/views/sienna/soporte.blade.php
+++ resources/views/sienna/soporte.blade.php
@@ -49,7 +49,7 @@
                     <tr class="text-center">
                         <th class="text-light">#</th>
                         <th class="text-light">Motivo</th>
-                        <th class="text-light">Descripcion</th>
+                        <th class="text-light">Departamento</th>
                         <th class="text-light">Fecha de creación</th>
                     </tr>
                 </thead>
@@ -58,7 +58,7 @@
                         <tr class="text-center">
                             <td><a href="{{ url('createticketsoportecliente?ticket=' . $val3->id) }}" target="_blank">{{$val3->id}}</a></td>
                             <td>{{$val3->nya}}</td>
-                            <td>{{$val3->cliente}}</td>
+                            <td>{{$val3->depto}}</td>
                             <td>{{$val3->created_at}}
                                 <?php if($val3->estadoconv==3){
                                     echo '<span class=" translate-middle p-1 bg-danger border border-light rounded-circle">
