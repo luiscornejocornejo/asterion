02634a403a4ac9a0c7e3f2c5b358d9b9e01af941
luiscornejocornejo <kayser19992005@gmail.com>
2024-07-25 11:21:48 -0300

    www

--- resources/views/sienna/pruebatelefonia.blade.php
+++ resources/views/sienna/pruebatelefonia.blade.php
@@ -19,6 +19,14 @@
 
               <!-- Start Content-->
                prueba telefonia
+               <script type="application/javascript">
+  function getIP(json) {
+    document.write("My public IP address is: ", json.ip);
+  }
+</script>
+
+<script type="application/javascript" src="https://api.ipify.org?format=jsonp&callback=getIP"></script>
+
               <!-- container -->
           </div>
           <!-- content -->
