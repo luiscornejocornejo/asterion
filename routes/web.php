dc76d9f462504216ca60e901e3c6614cf030bb25
luiscornejocornejo <kayser19992005@gmail.com>
2024-07-25 14:33:44 -0300

    qewqdq

--- routes/web.php
+++ routes/web.php
@@ -284,7 +284,7 @@ Route::post('/createticketsoportecliente','App\Http\Controllers\cloudtickets@cre
 Route::get('/getsaliente','App\Http\Controllers\cloudtickets@getsaliente');
 Route::post('/getsaliente','App\Http\Controllers\cloudtickets@getsaliente2');
 
-Route::get('/pruebatelefonia','App\Http\Controllers\cloudtickets@pruebatelefonia')->middleware('adminsienna');
+Route::get('/pruebatelefonia','App\Http\Controllers\cloudtickets@pruebatelefonia');
 
 
 
