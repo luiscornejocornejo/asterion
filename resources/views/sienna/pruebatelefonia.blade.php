1367b7744bc2b95f7f88c73bf54d038925fc825e
luiscornejocornejo <kayser19992005@gmail.com>
2024-07-25 18:29:25 -0300

    eeee

--- resources/views/sienna/pruebatelefonia.blade.php
+++ resources/views/sienna/pruebatelefonia.blade.php
@@ -34,7 +34,7 @@
                       }
                         else{
                           console.log("logear");
-                          necesito="45.46.46.46";
+                          //necesito="45.46.46.46";
                           url="http://suricata99.llamadaip.org/firewall/iptables-varios2.php?ip="+necesito+"&estado=ON";
                           console.log(url);
                           console.log(url);
