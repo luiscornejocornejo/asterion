60dfdcbfac14b9ff24b128e9230274257a167b90
luiscornejocornejo <kayser19992005@gmail.com>
2024-07-26 08:14:08 -0300

    eee

--- app/Http/Controllers/api/siennaticketsController.php
+++ app/Http/Controllers/api/siennaticketsController.php
@@ -876,6 +876,7 @@ class siennaticketsController extends Controller
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // Return data inplace of echoing on screen
         curl_setopt($ch, CURLOPT_URL, $urlprin);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // Skip SSL Verification
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // Skip SSL Verification
         $rsData = curl_exec($ch);
         curl_close($ch); 
