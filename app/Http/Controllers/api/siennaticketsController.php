b382f47df5d35207e260ab2de67a894fd6ef99bc
luiscornejocornejo <kayser19992005@gmail.com>
2024-07-26 08:10:18 -0300

    hhh

--- app/Http/Controllers/api/siennaticketsController.php
+++ app/Http/Controllers/api/siennaticketsController.php
@@ -879,6 +879,7 @@ class siennaticketsController extends Controller
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // Skip SSL Verification
         $rsData = curl_exec($ch);
         curl_close($ch); 
+        dd($rsData);
         return $rsData ;
      }
     public function valida($cliente,$merchant){
