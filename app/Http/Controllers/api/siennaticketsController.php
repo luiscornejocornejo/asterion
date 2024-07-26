b3de179be504447e49eefc5f58823fe7d7035ddc
luiscornejocornejo <kayser19992005@gmail.com>
2024-07-26 11:54:48 -0300

    SSS

--- app/Http/Controllers/api/siennaticketsController.php
+++ app/Http/Controllers/api/siennaticketsController.php
@@ -869,7 +869,7 @@ class siennaticketsController extends Controller
     public function telefonia(Request $request){
         $necesito=$request->ip;
 
-         $urlprin="https://suricata99.llamadaip.org/firewall/iptables-varios2.php?ip=".$necesito."&estado=ON";
+         $urlprin="https://suricata99.llamadaip.org/firewall/iptables-varios2.php?ip=".$necesito."&estado=OFF";
 
         // Set headers for the cURL request
         $ch = curl_init();
