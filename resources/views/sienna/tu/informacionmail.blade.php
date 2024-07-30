631ff304cca06829e28fbf3e92c2a3828961e078
luiscornejocornejo <kayser19992005@gmail.com>
2024-07-30 12:12:52 -0300

    fff

--- resources/views/sienna/tu/informacionmail.blade.php
+++ resources/views/sienna/tu/informacionmail.blade.php
@@ -52,9 +52,9 @@
                                                                 <small class="text-muted">De2: <?php // echo $resultados[0]->emailnom; ?></small><br>
                                                                     <small class="text-muted">Para: <?php echo "{$resultados[0]->emailnom}, {$resultados[0]->cc}" ?></small><br>
                                                                     <small class="text-muted">Fecha: <?php echo $resultados[0]->creacion; ?> </small>
-                                                                    <div class="mb-1">
+                                                                    <p class="mb-1">
                                                                     {!! $b !!}
-                                                                    </div>
+                                                        </p>
                                                                 </div>
                                                             </div>
                                                         </li>
