2e385a3e167b7953c74f3daa9d1ea3da993b34c4
luiscornejocornejo <kayser19992005@gmail.com>
2024-07-30 21:25:53 -0300

    eee

--- resources/views/sienna/getsaliente.blade.php
+++ resources/views/sienna/getsaliente.blade.php
@@ -78,7 +78,7 @@ document.getElementById("phone").value = phone;
             <div class="border" class="col-2">
                 <?php if (isset($datosonline)) {
                     $datosonline2 = json_decode($datosonline);
-                    dd($datosonline2 );
+                  //  dd($datosonline2 );
                 ?>
 
 
