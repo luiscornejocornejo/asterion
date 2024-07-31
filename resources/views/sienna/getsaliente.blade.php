13ba4da551044ca59e3d657157cf01990bea6341
luiscornejocornejo <kayser19992005@gmail.com>
2024-07-31 02:18:09 -0300

    www

--- resources/views/sienna/getsaliente.blade.php
+++ resources/views/sienna/getsaliente.blade.php
@@ -92,8 +92,8 @@ document.getElementById("phone").value = phone;
                             }
                         if($tipointegracion=="ispcube"){
                             $tablaname=$datosonline2->name;
-                            $tablaphone=$datosonline2->phones.number;
-                            $tablaphone_mobile=$datosonline2->phones.number;
+                            $tablaphone=$datosonline2->phones['number'];
+                            $tablaphone_mobile=$datosonline2->phones['number'];
                         }
                     
                 ?>
