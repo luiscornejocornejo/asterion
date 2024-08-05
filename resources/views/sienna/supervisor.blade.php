4d924afbc053ac6219d853324ebcb2664dbefcff
luis cornejo <kayser1712@gmail.com>
2024-08-05 12:06:02 -0300

    eee

--- resources/views/sienna/supervisor.blade.php
+++ resources/views/sienna/supervisor.blade.php
@@ -586,8 +586,8 @@
 
                         ' <td>' + response.data[i].cel + '</td>' +
                         ' <td>' + response.data[i].creado + '</td>' +
-                        '<td> <span style="font-size:medium;" class="badge '+colorestado+'" onclick="estado2(`' + result + '`,`' + response.data[i].ticketid + '`,`' + response.data[i].conversation_id + '`,`' + response.data[i].iddepto + '`)"  class="badge bg-'+colorestado2+' " style="font-size: medium" role="button" data-bs-toggle="modal" data-bs-target="#bs-example-modal-sm">' + response.data[i].estadoname + ' </span></td> ' +
 
+                        ' <td><span  onclick="estado2(`' + result + '`,`' + response.data[i].ticketid + '`,`' + response.data[i].conversation_id + '`,`' + response.data[i].iddepto + '`)"  style="font-size:medium;" class="badge '+colorestado+'">' + response.data[i].estadoname + '</span></td>' +
                         <?php foreach($resultadosextras as $valo){
                                       $busco=$valo->nombre;
                                       echo "'<td class=>' + response.data[i].".$busco." + '</td>'+";
