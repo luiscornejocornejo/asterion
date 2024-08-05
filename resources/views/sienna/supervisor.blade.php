856caf1f46b49b7fc1e3203e17e73e2346ec078b
luis cornejo <kayser1712@gmail.com>
2024-08-05 11:58:36 -0300

    eee

--- resources/views/sienna/supervisor.blade.php
+++ resources/views/sienna/supervisor.blade.php
@@ -581,8 +581,8 @@
                         ' <td>' + response.data[i].nya + '</td>' + 
                         ' <td>' + $nombreamostrar+ '</td>' + 
                         '<td><span class="badge '+colordepto+'" style="font-size:medium;" onclick="area(`' + response.data[i].ticketid + '`,`' + response.data[i].conversation_id + '`,`' + response.data[i].user_id + '`)"  class="badge bg-info" style="font-size: medium" role="button" data-bs-toggle="modal" data-bs-target="#bs-example-modal-sm2">'+response.data[i].depto+' </span></td>' +
-       
-                        ' <td><span style="font-size:medium;" class="'+colorpri+'">' + response.data[i].topicnombre + '</span></td>' +
+                        '<td><span style="font-size:medium;" class="'+colorpri+'"> onclick="topic(`' + result + '`,`' + response.data[i].ticketid + '`,`' + response.data[i].conversation_id + '`,`' + response.data[i].iddepto + '`)"  class="badge badge-info-lighten border" style="font-size: medium" role="button" data-bs-toggle="modal" data-bs-target="#bs-example-modal-smtopic"> ' + response.data[i].topicnombre + '</span></td> ' +
+
                         ' <td><span style="font-size:medium;" class="'+colorpri+'">' + response.data[i].pri + '</span></td>' +
                         ' <td>' + response.data[i].cel + '</td>' +
                         ' <td>' + response.data[i].creado + '</td>' +
