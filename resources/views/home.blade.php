2552939772d0b18407bd4f7a6b2a15a408672197
luis cornejo <kayser1712@gmail.com>
2024-08-07 14:25:15 -0300

    eee

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 
 @include('facu.header2')
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+
 <?php   
                $tokeninterno = session('tokeninterno');
                $miip=request()->ip();
