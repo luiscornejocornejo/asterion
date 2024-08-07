1fca7d66006d750013ddb376d572a27d6fd8c876
luis cornejo <kayser1712@gmail.com>
2024-08-07 14:45:09 -0300

    sss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 
-
+@include('facu.header2')
 <?php   
                $tokeninterno = session('tokeninterno');
                $miip=request()->ip();
@@ -34,8 +34,6 @@
 
     }
 </script>
-@include('facu.header2')
-
 <script>
         let navegador = navigator.userAgent;
         if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
