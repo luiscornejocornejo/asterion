49e4b9414b6f8eb8531ab16afbfca967f549635b
luis cornejo <kayser1712@gmail.com>
2024-08-09 11:42:43 -0300

    rrr

--- resources/views/sienna/tu/informacionmail.blade.php
+++ resources/views/sienna/tu/informacionmail.blade.php
@@ -103,14 +103,14 @@
                                             <div class="mt-2 ">
                                                 <div class="mb-2 mt-2">
                                                     <div class="form-group">
-                                                        <label for="comentarios">cc</label>
-                                                        <label for="comentarios">Destinatario</label>
-                                                        <input type="hidden" name="ticket" value="<?php echo $resultados[0]->ticketid; ?>">
+                                                        
                                                         <label >Asunto: </label>
                                                         <span><?php echo $resultados[0]->asunto; ?></span>
+                                                        <br> 
                                                         <input name="subject" id="subject" class=""  type="text" value="<?php echo $resultados[0]->asunto; ?>">
 
-                         <br>                               <label for="comentarios">CC:</label>
+                                                            <br>                             
+                                                      <label for="comentarios">CC:</label>
                          <input name="cc"  id="cc" class=""  type="text" value="<?php echo $resultados[0]->cc; ?>">
                            <br>                             <label for="comentarios">Destinatario</label>
                                                         <span name="mailaeviar" id="mailaeviar" class="badge text-bg-secondary rounded-pill" ><?php echo $resultados[0]->emailnom; ?></span>
