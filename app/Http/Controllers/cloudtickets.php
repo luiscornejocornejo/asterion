41684cf33c280fc231ed0c33b680d7ca397a0281
Ficuu <facuu.paez95@gmail.com>
2024-08-09 12:16:58 -0300

    push mail style

--- app/Http/Controllers/cloudtickets.php
+++ app/Http/Controllers/cloudtickets.php
@@ -1453,16 +1453,17 @@ class cloudtickets extends Controller
 
       if (isset($request->logo)) {
         $rutasimagenes="<br> Archivos Adjuntos:<br>";
+        $cont = 1;
         foreach($request->logo as $image) {
           $path = $image->getClientOriginalName();
          echo  $name = time() . '-' . $path;
          echo "<br>";
          $domi=$this->dominio();
-
+         $cont++;   
          $ruta=$domi."/mail";
 
          $logo= Storage::disk('do')->put($ruta, $image);
-         $rutasimagenes = "<a href='https://sienamedia.sfo3.digitaloceanspaces.com/{$logo}'>{$logo}</a><br>";
+         $rutasimagenes.="<a href='https://sienamedia.sfo3.digitaloceanspaces.com/".$logo."'>Adjunto {$cont} </a><br>";
          $se2 = new siennaseguimientos();
             $se2->ticket = $ticket;
             $se2->logo = $logo;
