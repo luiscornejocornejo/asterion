39856f1c9a1ebeb548dd5bfab90e1eb5fcfe5ca5
Ficuu <facuupaez95@gmail.com>
2024-08-21 11:49:34 -0300

    check without  buttons wsp and NPS

--- resources/views/sienna/userprofile.blade.php
+++ resources/views/sienna/userprofile.blade.php
@@ -16,28 +16,26 @@ foreach ($resultados as $valuee) {
 
 <style>
   .tooltip-button {
-    position: relative;
-  display: inline-block;
-  border-bottom: 1px dotted black;
+     position: relative;
+    display: inline-block;
+    border-bottom: 1px dotted #ccc;
+    color: #006080;
     
   }
   
   /* Tooltip text */
   .tooltip-button .tooltiptext {
     visibility: hidden;
-  width: 120px;
-  background-color: black;
-  color: #fff;
-  text-align: center;
-  border-radius: 6px;
-  padding: 5px 0;
-  
-  /* Position the tooltip */
-  position: absolute;
-  z-index: 1;
-  top: 100%;
-  left: 50%;
-  margin-left: -60px;
+    position: absolute;
+    width: 100px;
+    background-color: #555;
+    color: #fff;
+    text-align: center;
+    padding: 5px 0;
+    border-radius: 6px;
+    z-index: 1;
+    opacity: 0;
+    transition: opacity .6s;
   }
   
   /* Tooltip arrow */
@@ -57,17 +55,6 @@ foreach ($resultados as $valuee) {
   top: 135%;
   left: 50%;  
   margin-left: -60px;
-}
-
-.tooltip-bottom::after {
-    content: "";
-    position: absolute;
-    bottom: 100%;
-    left: 50%;
-    margin-left: -5px;
-    border-width: 5px;
-    border-style: solid;
-    border-color: transparent transparent #555 transparent;
 }
   
   /* Show the tooltip text when you mouse over the tooltip container */
