3058635c6005f3bef1776e04ad197a3d9ed63176
Ficuu <facuupaez95@gmail.com>
2024-08-21 12:16:29 -0300

    check without  buttons wsp and NPS

--- resources/views/sienna/userprofile.blade.php
+++ resources/views/sienna/userprofile.blade.php
@@ -227,8 +227,8 @@ foreach ($resultados as $valuee) {
         </div>
         <!-- end row -->
       
-        <div class="floating-button ">
-            <button id="button-3" class="mdi mdi-send bg-success text-light floatButton" data-bs-toggle="modal" data-bs-target="#warning-alert-modal2"></button>
+        <div class="floating-button">
+            <span class="mdi mdi-send bg-success text-light floatButton" role="button" data-bs-toggle="modal" data-bs-target="#warning-alert-modal2"></span>
             
         </div> 
     </div> <!-- container-fluid -->
