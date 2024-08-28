7c2093e97f93e24843928063541c9e8337eee547
luis cornejo <kayser1712@gmail.com>
2024-08-28 07:36:06 -0300

    eee

--- resources/views/sienna/tu/informacionpagoralia.blade.php
+++ resources/views/sienna/tu/informacionpagoralia.blade.php
@@ -5,7 +5,22 @@
                 <h4 class="fw-normal text-dark" title="Number of Customers">Registrar Orden en Pagoralia</h4>
             </div>
             <div>
-                <button onclick="extraordinario('<?php echo $resultados[0]->cliente;?>')" type="button" class="btn btn-success"><i class="mdi mdi-account-search me-1"></i> Buscar datos</button>
+            <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
             </div>
         </div>
         <hr style="margin-top: 10px;" />
