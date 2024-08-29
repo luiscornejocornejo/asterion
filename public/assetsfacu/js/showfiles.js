e1e0ab30eaa5e55afa76f4844317abf0bd93324a
luis cornejo <kayser1712@gmail.com>
2024-08-29 09:49:48 -0300

    eee

--- public/assetsfacu/js/showfiles.js
+++ public/assetsfacu/js/showfiles.js
@@ -1,5 +1,5 @@
 function init() {
-    document.getElementById('exampleInputPassword1').addEventListener('change', showName, false);
+   // document.getElementById('exampleInputPassword1').addEventListener('change', showName, false);
   }
   function showName (event) {
     document.getElementById('fileName').innerHTML = event.target.files[0].name
