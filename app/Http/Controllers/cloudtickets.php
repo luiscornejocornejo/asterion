0c6e304383598813a49c05512e270385c98eabd3
D-pereyra <dario.pereyra@suricata.la>
2024-09-02 15:17:44 -0300

    telef abminternos 08

--- app/Http/Controllers/cloudtickets.php
+++ app/Http/Controllers/cloudtickets.php
@@ -1686,9 +1686,9 @@ WHERE ticket IN (
         $domi=$this->dominio();
         $query="select * from ".$domi.".siennainternos";
         $sint = DB::select($query);
-        $master=array();
-        $master["crear"] = 1;
-        $master["modificar"] = 1;
+        $master=(object)[];
+        $master->crear = 1;
+        $master->modificar = 1;
         return view('sienna/abminternos')
         ->with('internos', $sint)
         ->with('master', $master);
