efff1fd10787b8a85898536e4056c8f080070ae7
D-pereyra <dario.pereyra@suricata.la>
2024-09-09 20:49:30 -0300

    Cabase multiple select feature 03

--- resources/views/sienna/siennacreate.blade.php
+++ resources/views/sienna/siennacreate.blade.php
@@ -174,7 +174,35 @@
                                         </select>
                                     </div>
 
+                                <?php
 
+
+                                } elseif ($tipo == "multiselect") {
+
+                                ?>
+
+                                    <div class="form-check form-switch">
+                                        <select class="form-select" aria-label="Default select multiple" name="<?php echo $Fieldarray[$i]; ?>" multiple>
+                                            <?php
+                                            $querysoption = "select * from " . $Fieldarray[$i] . " ";
+                                            if ($base == 1) {
+
+                                            $resultadosoption = DB::select($querysoption);
+                                            }else{
+
+                                                {{ App\Http\Controllers\siennaController::conectar2($base); }}
+                                            // $prueba = conectar($dbexterna);
+                                                $resultadosoption = DB::connection('mysql2')->select($querysoption);
+                                            }
+                                            foreach ($resultadosoption as $resultoption) {
+
+                                                $idoption = $resultoption->id;
+                                                $nombreoption = $resultoption->nombre;
+                                                echo "<option value='" . $idoption . "' >" . $nombreoption . "</option>";
+                                            } ?>
+
+                                        </select>
+                                    </div>
 
                                 <?php
 
