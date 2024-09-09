6573591980d7a027ad451a3293a4acb0672bf98a
luiscornejocornejo <kayser19992005@gmail.com>
2024-09-09 11:52:58 -0300

    aaa

--- resources/views/sienna/abm.blade.php
+++ resources/views/sienna/abm.blade.php
@@ -80,7 +80,7 @@ if (isset($_SERVER['HTTP_HOST'])) {
                                         <?php } ?>
                                         <?php if ($master->tickets == 1) { ?>
 
-                                        <button type="button" onclick="borrar(<?php echo $resultado->$registro; ?>)" class="btn btn-danger waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#ctabm"><i data-feather="delete"></i></button>
+                                        <button type="button" onclick="borrar(<?php echo $resultado->$registro; ?>)" class="btn btn-danger waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#ctabm">Ticket</button>
 
                                         <?php } ?>
                                         
