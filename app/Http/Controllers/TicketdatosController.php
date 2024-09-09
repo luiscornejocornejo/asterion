bc2f7ec54ceac0073c2173854c52865acc96607a
luis cornejo <kayser1712@gmail.com>
2024-09-09 18:07:39 -0300

    eee

--- app/Http/Controllers/TicketdatosController.php
+++ app/Http/Controllers/TicketdatosController.php
@@ -918,7 +918,7 @@ class TicketdatosController extends Controller
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
+            CURLOPT_TIMEOUT => 30,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'GET',
