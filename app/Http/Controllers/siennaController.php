d2b58f5c6ee6b7230fdfe512814cd118e1dcb08b
D-pereyra <dario.pereyra@suricata.la>
2024-09-12 09:59:35 -0300

    Cabase multiple select feature 27

--- app/Http/Controllers/siennaController.php
+++ app/Http/Controllers/siennaController.php
@@ -364,7 +364,7 @@ class siennaController extends Controller
             }
           }
 
-          
+          var_dump($Fieldarray);die;
           for ($i = 0; $i < sizeof($Fieldarray); $i++) {
             
             $arrayno = array('created_at', 'id', 'updated_at', 'email_verified_at', 'remember_token');
