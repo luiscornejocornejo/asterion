c9e1665d05a1f460eaef623a58f2731bd08650b7
luiscornejocornejo <kayser19992005@gmail.com>
2024-09-14 02:12:07 -0300

    fff

--- resources/views/sienna/getsaliente.blade.php
+++ resources/views/sienna/getsaliente.blade.php
@@ -94,7 +94,8 @@ document.getElementById("phone").value = phone;
 
                         if($tipointegracion=="futu"){
                                 $tablaname=$datosonline2->nombres;
-                                $tablaphone= $datosonline2['infocontratos'][0]['telefonolocal']  ;
+                                $tablaphone= $datosonline2->infocontratos;//[0]['telefonolocal']  ;
+                                dd($tablaphone);
                                 $tablaphone_mobile= $datosonline2['infocontratos'][0]['telefonolocal']  ;
                                 }
 
