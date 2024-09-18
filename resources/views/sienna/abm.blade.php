c65f52ce10448dc15256a24d5c1b872622bc6934
Ficuu <facuu.paez95@gmail.com>
2024-09-18 18:48:33 -0300

    styling to abm

--- resources/views/sienna/abm.blade.php
+++ resources/views/sienna/abm.blade.php
@@ -153,8 +153,8 @@ if (isset($_SERVER['HTTP_HOST'])) {
 <div class="modal fade bs-example-modal-center" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title bg-dark text-white">Eliminar</h5>
+            <div class="modal-header bg-dark">
+                <h5 class="modal-title text-white">Eliminar</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
