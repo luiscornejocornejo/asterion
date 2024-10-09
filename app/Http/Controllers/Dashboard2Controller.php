69a755ecd0f0527fdf9b6cdb7d7ecb9eecfe2964
luis cornejo <kayser1712@gmail.com>
2024-10-09 17:30:24 -0300

    eee

--- app/Http/Controllers/Dashboard2Controller.php
+++ app/Http/Controllers/Dashboard2Controller.php
@@ -326,6 +326,85 @@ class Dashboard2Controller extends Controller
     }
     public function getTicketPerAgent($source,$department,$agent,$periodo) 
     {
+        $subquery = " WHERE 1=1"; 
+        if($source<>null){
+            $source = implode(', ', $source);
+           $subquery.=" and siennatickets_view.siennasource in(".$source.")";
+        }
+        if($department<>null){
+            $department = implode(', ', $department);
+           $subquery.=" and siennatickets_view.siennadepto in(".$department.")";
+        }
+        if ($periodo !== null) {
+           // $department = implode(', ', $department);
+        
+            $daterange = null;
+           switch ($periodo) {
+            case '0':  // Hoy
+                $daterange = [
+                    'start' => Carbon::today()->toDateString(),
+                    'end' => Carbon::today()->toDateString()
+                ];
+                break;
+        
+                case '1':  // Ayer
+                    $daterange = [
+                        'start' => Carbon::yesterday()->toDateString(),
+                        'end' => Carbon::yesterday()->toDateString()
+                    ];
+                    break;
+            
+                case '2':  // Últimos 7 días
+                    $daterange = [
+                        'start' => Carbon::today()->subDays(7)->toDateString(),
+                        'end' => Carbon::today()->toDateString()
+                    ];
+                    break;
+            
+                case '3':  // Últimos 30 días
+                    $daterange = [
+                        'start' => Carbon::today()->subDays(30)->toDateString(),
+                        'end' => Carbon::today()->toDateString()
+                    ];
+                    break;
+            
+                case '4':  // Mes actual
+                    $daterange = [
+                        'start' => Carbon::now()->startOfMonth()->toDateString(),
+                        'end' => Carbon::now()->toDateString()
+                    ];
+                    break;
+            
+                case '5':  // Mes anterior
+                    $daterange = [
+                        'start' => Carbon::now()->subMonth()->startOfMonth()->toDateString(),
+                        'end' => Carbon::now()->subMonth()->endOfMonth()->toDateString()
+                    ];
+                    break;
+            
+                case '6':  // Rango personalizado
+                    // En este caso, deberías capturar las fechas de inicio y fin de un formulario adicional
+                    // Aquí se debe manejar las entradas del usuario
+                    $daterange = [
+                        'start' => $_POST['start_date'],  // Capturar el valor del input de inicio
+                        'end' => $_POST['end_date']  // Capturar el valor del input de fin
+                    ];
+                    break;
+            
+                default:
+                    // No hacer nada si no hay selección válida
+                    $daterange = null;
+                    break;
+            }
+            if (isset($daterange['start']) && isset($daterange['end'])) {
+
+           $subquery .= " AND siennatickets_view.created_at > '".$daterange['start']." 00:00:00' AND siennatickets_view.created_at < '".$daterange['end']." 23:59:59'";
+            }
+        }
+        if($agent<>null){
+            $agent = implode(', ', $agent);
+           $subquery.=" and siennatickets_view.asignado in(".$agent.")";
+        }
         $queryPerAgent = "SELECT `Users - Asignado`.`last_name` AS `Users - Asignado__last_name`, COUNT(*) AS `count`
         FROM
         `siennatickets_view`
