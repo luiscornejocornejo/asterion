c9741757cf30cda473707947ddaa89f0a842f206
Ficuu <facuu.paez95@gmail.com>
2024-10-10 11:39:09 -0300

    categories ok

--- resources/views/sienna/dash.blade.php
+++ resources/views/sienna/dash.blade.php
@@ -103,7 +103,7 @@
                                 </div>
 
                                 <div class="col-xxl-4 col-xl-4 col-lg-8 col-sm-12 mt-2">
-                                    <div class="border rounded" style="min-height: 200px!important;">
+                                    <div class="border rounded" style="min-height: 380px!important;">
                                         <p class="m-1">Ticket por estado</p>
                                         @php
                                             $series = array_map(function ($item) {
