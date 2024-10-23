aa3bb12143c7ba1502389b87caec41464e7be530
Ficuu <facuu.paez95@gmail.com>
2024-10-23 12:39:34 -0300

    changing the day by defaul to today

--- resources/views/sienna/dash.blade.php
+++ resources/views/sienna/dash.blade.php
@@ -35,13 +35,13 @@ if (isset($_SERVER['HTTP_HOST'])) {
             <div class="container">
                 <ul class="nav nav-pills bg-nav-pills nav-justified mb-3">
                     <li class="nav-item">
-                        <a href="#home1" data-bs-toggle="tab" aria-expanded="false" class="nav-link rounded-0 active">
+                        <a href="#home1" data-bs-toggle="tab" aria-expanded="true" class="nav-link rounded-0 active">
                             <i class="mdi mdi-home-variant d-md-none d-block"></i>
                             <span class="d-none d-md-block">Dashboard</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="#profile1" data-bs-toggle="tab" aria-expanded="true" class="nav-link rounded-0">
+                        <a href="#profile1" data-bs-toggle="tab" aria-expanded="false" class="nav-link rounded-0">
                             <i class="mdi mdi-account-circle d-md-none d-block"></i>
                             <span class="d-none d-md-block">CSAT</span>
                         </a>
