c512da4a95e9289046ebbfdf0f223d7fadc116b1
Ficuu <facuu.paez95@gmail.com>
2024-10-24 11:42:53 -0300

    check business

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 
 @include('facu.header2')
-<?php echo session('empresa');?>
+<?php echo "Juanito" . session('empresa');?>
 <script>
         let navegador = navigator.userAgent;
         if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
