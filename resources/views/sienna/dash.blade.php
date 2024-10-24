0cf766fb615dd4db3d1e44261719bdce5845180a
Ficuu <facuu.paez95@gmail.com>
2024-10-24 10:44:43 -0300

    add CSAT to dash section

--- resources/views/sienna/dash.blade.php
+++ resources/views/sienna/dash.blade.php
@@ -1,5 +1,5 @@
 @include('facu.header')
-<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+<script src="assetsfacu/vendor/apexcharts/apexcharts.min.js"></script>
 
 <script>
     console.log(@json($csatViewExist))
