a88a0b566be7fe07c4a49c82f47d0b72ece99445
Ficuu <facuu.paez95@gmail.com>
2024-10-25 21:10:49 -0300

    check CSAT Hold

--- resources/views/sienna/dash.blade.php
+++ resources/views/sienna/dash.blade.php
@@ -1072,7 +1072,7 @@ if (isset($_SERVER['HTTP_HOST'])) {
                 @else
                     <div class="container">
                         <div class="mt-2">
-                            <div class="card">
+                            <div class="bg-white">
                                 <div class="border rounded text-center" style="min-height: 250px!important;">
                                     <div class="my-5">
                                         <img src="assetsfacu/images/svg/file-searching.svg" height="90"
