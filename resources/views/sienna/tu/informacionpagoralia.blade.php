c18f72b04469187603abc933a83750f1ae0fad5f
Ficuu <facuu.paez95@gmail.com>
2024-10-28 16:23:44 -0300

    check filters

--- resources/views/sienna/tu/informacionpagoralia.blade.php
+++ resources/views/sienna/tu/informacionpagoralia.blade.php
@@ -12,34 +12,34 @@
         <div class="row">
             <form method="get" action="/pagoraliaorden">
                 <div class="row">
-                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
+                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12 mt-2">
                         <label class="form-label" id="invoice_number">Invoice:</label>
                         <input required name="invoice" type="text" class="form-control" id="invoice_number"
                             aria-describedby="emailHelp" placeholder="fb101232">
                     </div>
-                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
+                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12 mt-2">
                         <label class="form-label" for="description">Descripción</label>
                         <input required name="descripcion" type="text" class="form-control" id="description"
                             placeholder="pago por servicio de inet">
                     </div>
-                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
+                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12 mt-2">
                         <label class="form-label" for="amount">Monto:</label>
                         <input required name="monto" type="number" step="0.01" class="form-control"
                             id="amount" placeholder="50.00">
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
+                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12 mt-2">
                         <label class="form-label" for="customer">Número de cliente</label>
                         <input required name="cliente" type="text" class="form-control" id="customer"
                             placeholder="cliente">
                     </div>
-                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
+                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12 mt-2">
                         <label class="form-label" for="nameUser">Nombre:</label>
                         <input required name="nombre" type="text" class="form-control" id="nameUser"
                             placeholder="nombre">
                     </div>
-                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
+                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12 mt-2">
                       <label class="form-label" for="lastNameUser">Apellido:</label>
                         <input required name="apellido" type="text" class="form-control" id="lastNameUser"
                             placeholder="apellido">
