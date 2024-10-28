8d553541a3cf8cb964ff04693c7d396aae76f54f
Ficuu <facuu.paez95@gmail.com>
2024-10-28 16:22:45 -0300

    check filters

--- resources/views/sienna/tu/informacionpagoralia.blade.php
+++ resources/views/sienna/tu/informacionpagoralia.blade.php
@@ -13,34 +13,36 @@
             <form method="get" action="/pagoraliaorden">
                 <div class="row">
                     <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
-                        <label for="exampleInputEmail1">Invoice</label>
-                        <input required name="invoice" type="text" class="form-control" id="exampleInputEmail1"
+                        <label class="form-label" id="invoice_number">Invoice:</label>
+                        <input required name="invoice" type="text" class="form-control" id="invoice_number"
                             aria-describedby="emailHelp" placeholder="fb101232">
                     </div>
                     <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
-                        <label for="exampleInputPassword1">Descripcion</label>
-                        <input required name="descripcion" type="text" class="form-control"
-                            id="exampleInputPassword1" placeholder="pago por servicio de inet">
+                        <label class="form-label" for="description">Descripción</label>
+                        <input required name="descripcion" type="text" class="form-control" id="description"
+                            placeholder="pago por servicio de inet">
                     </div>
                     <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
-                        <label for="exampleInputPassword1">Monto</label>
+                        <label class="form-label" for="amount">Monto:</label>
                         <input required name="monto" type="number" step="0.01" class="form-control"
-                            id="exampleInputPassword1" placeholder="50.00">
+                            id="amount" placeholder="50.00">
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
-                        <label for="exampleInputPassword1">Cliente</label>
-                        <input required name="cliente" type="text" class="form-control" id="exampleInputPassword1"
+                        <label for="customer">Número de cliente</label>
+                        <input required name="cliente" type="text" class="form-control" id="customer"
                             placeholder="cliente">
                     </div>
                     <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
-                    <input required name="nombre" type="text" class="form-control" id="exampleInputPassword1"
-                        placeholder="nombre">
+                        <label class="form-label" for="nameUser">Nombre:</label>
+                        <input required name="nombre" type="text" class="form-control" id="nameUser"
+                            placeholder="nombre">
                     </div>
                     <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
-                    <input required name="apellido" type="text" class="form-control" id="exampleInputPassword1"
-                        placeholder="apellido">
+                      <label class="form-label" for="lastNameUser">Apellido:</label>
+                        <input required name="apellido" type="text" class="form-control" id="lastNameUser"
+                            placeholder="apellido">
                     </div>
                     <hr style="margin-top: 10px;" />
                 </div>
