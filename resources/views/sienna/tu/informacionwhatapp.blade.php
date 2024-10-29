25d73b81db8c73666d0cdc69286fab8aaa8736a9
luiscornejocornejo <kayser19992005@gmail.com>
2024-10-28 22:09:02 -0300

    jjjj

--- resources/views/sienna/tu/informacionwhatapp.blade.php
+++ resources/views/sienna/tu/informacionwhatapp.blade.php
@@ -6,12 +6,12 @@
         $tipobot=$val->tipo;
         $bp_bot_id=$val->bp_botid;
         $bp_ws_id=$val->bp_wsid;
-        $bt_webhook_id=$val->bt_webhook_id;
+        $bp_webhook_id=$val->bt_webhook_id;
     }
     if(($resultados[0]->ticketid==317)or($resultados[0]->ticketid==320)){
    // if($tipobot==1){
             ?>
-<iframe allow="camera;microphone" src="https://frontend-botpress-q74s5.ondigitalocean.app/?conversationId=<?php echo $resultados[0]->conversation_id; ?>&ticketId=<?php echo $resultados[0]->ticketid;?>&agentName=<?php echo session('nombreusuario');?>&bp_botid=<?php echo $bp_botid;?>&bp_wsid=<?php echo $bp_wsid;?>&bt_webhook_id=<?php echo $bt_webhook_id;?> " width="100%" class="border rounded-3" style="height: 650px!important;"></iframe>
+<iframe allow="camera;microphone" src="https://frontend-botpress-q74s5.ondigitalocean.app/?conversationId=<?php echo $resultados[0]->conversation_id; ?>&ticketId=<?php echo $resultados[0]->ticketid;?>&agentName=<?php echo session('nombreusuario');?>&bp_bot_id=<?php echo $bp_bot_id;?>&bp_ws_id=<?php echo $bp_ws_id;?>&bp_webhook_id=<?php echo $bp_webhook_id;?> " width="100%" class="border rounded-3" style="height: 650px!important;"></iframe>
 <?php }
 else{?>
 <iframe allow="camera;microphone" src="<?php echo $resultados[0]->conversation_url; ?>" width="100%" class="border rounded-3" style="height: 650px!important;"></iframe>
