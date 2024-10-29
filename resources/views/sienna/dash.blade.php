3e017c43ae25fc625283f498a85ecd194b97f72e
luiscornejocornejo <kayser19992005@gmail.com>
2024-10-29 10:26:43 -0300

    sss

--- resources/views/sienna/dash.blade.php
+++ resources/views/sienna/dash.blade.php
@@ -7,18 +7,10 @@
         let navegador = navigator.userAgent;
         if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
             tipo=<?php  echo $tipodemenu = session('tipodemenu'); ?>;
-            var url = window.location.href;
             console.log("Estás usando un dispositivo móvil!!"+tipo);
 
 
-            if(tipo=="2"){
-                location.href = url+'viewtickets';
-
-
-            }else{
-                location.href = url+'viewtickets';
-
-            }
+           
         } else {
             console.log("No estás usando un móvil");
         }
