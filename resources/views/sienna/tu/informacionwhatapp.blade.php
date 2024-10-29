28dbd0e81b56ed4f147a19e84363e5bae7acab6d
luiscornejocornejo <kayser19992005@gmail.com>
2024-10-28 21:33:49 -0300

    feee

--- resources/views/sienna/tu/informacionwhatapp.blade.php
+++ resources/views/sienna/tu/informacionwhatapp.blade.php
@@ -1,5 +1,10 @@
-<iframe allow="camera;microphone" src="<?php echo $resultados[0]->conversation_url; ?>" width="100%" class="border rounded-3" style="height: 650px!important;"></iframe>
 
+<?php if($resultados[0]->ticketid==317){?>
+<iframe allow="camera;microphone" src="https://frontend-botpress-q74s5.ondigitalocean.app/?conversationId=<?php echo $resultados[0]->conversation_id; ?>&ticketId=<?php echo $resultados[0]->ticketid;?>&agentName=<?php echo session('nombreusuario');?> " width="100%" class="border rounded-3" style="height: 650px!important;"></iframe>
+<?php }
+else{?>
+<iframe allow="camera;microphone" src="<?php echo $resultados[0]->conversation_url; ?>" width="100%" class="border rounded-3" style="height: 650px!important;"></iframe>
+<?php }?>
 <div class="<?php echo $vero; ?> d-flex justify-content-between mt-2 mb-2">
     <div></div>
     <div class="me-2">
