8d6d38cb27a2b82fd773517da5125a5d3a178786
luiscornejocornejo <kayser19992005@gmail.com>
2024-10-30 21:49:22 -0300

    sfgsgdsg

--- resources/views/sienna/tu/informacionwhatapp.blade.php
+++ resources/views/sienna/tu/informacionwhatapp.blade.php
@@ -15,7 +15,7 @@
    // if($tipobot==1){
             ?>
 <iframe allow="camera;microphone" 
-src="https://conversations.suricata.chat/?conversationId=<?php echo $resultados[0]->conversation_id; ?>&ticketId=<?php echo $resultados[0]->ticketid;?>&agentName=<?php echo session('nombreusuario');?>&bp_bot_id=<?php echo $bp_bot_id;?>&bp_ws_id=<?php echo $bp_ws_id;?>&bp_webhook_id=<?php echo $bp_webhook_id;?>&merchantId=<?php echo $merchantId;?>" width="100%" class="border rounded-3" style="height: 650px!important;"></iframe>
+src="https://conversations.suricata.chat/?ticketId=<?php echo $resultados[0]->ticketid;?>&agentName=<?php echo session('nombreusuario');?>&merchantId=<?php echo $merchantId;?>" width="100%" class="border rounded-3" style="height: 650px!important;"></iframe>
 <?php }
 else{?>
 <iframe allow="camera;microphone" src="<?php echo $resultados[0]->conversation_url; ?>" width="100%" class="border rounded-3" style="height: 650px!important;"></iframe>
