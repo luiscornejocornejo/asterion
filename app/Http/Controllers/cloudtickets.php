d3efffa4669c5373d488fc6c15205db68e3e59c1
luiscornejocornejo <kayser19992005@gmail.com>
2024-11-06 16:40:59 -0300

    eee

--- app/Http/Controllers/cloudtickets.php
+++ app/Http/Controllers/cloudtickets.php
@@ -1923,7 +1923,7 @@ WHERE ticket IN (
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => $method,
-            CURLOPT_POSTFIELDS => $datosjson3,
+            CURLOPT_POSTFIELDS => $datosjson2,
             CURLOPT_HTTPHEADER => $header,
         ));
 
