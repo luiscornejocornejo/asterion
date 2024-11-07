983483891a9738c55614ffe3b95daa6930da49e3
Ficuu <facuu.paez95@gmail.com>
2024-11-07 17:23:36 -0300

    favicon ok and hide toast

--- resources/views/sienna/tu/informacionpagoralia.blade.php
+++ resources/views/sienna/tu/informacionpagoralia.blade.php
@@ -136,9 +136,9 @@
         </div>
 
     </div>
-    <div class="toast fade show" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast fade" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-header bg-dark">
-            <img src="assets/images/logo-dark-sm.png" alt="brand-logo" height="12" class="me-1" />
+            <img src="assetsfacu/images/favicom_suricata.png" alt="brand-logo" height="12" class="me-1" />
             <strong class="me-auto text-light">Copiado!</strong>
             <small class="text-light">Ahora</small>
             <button type="button" class="ms-2 btn-close btn-close-white" data-bs-dismiss="toast" aria-label="Close"></button>
