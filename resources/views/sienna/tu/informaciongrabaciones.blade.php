b103945cc65adabf26d0499076b30dff86af9ce4
Ficuu <facuu.paez95@gmail.com>
2024-11-08 13:30:23 -0300

    styling of component recordings phone

--- resources/views/sienna/tu/informaciongrabaciones.blade.php
+++ resources/views/sienna/tu/informaciongrabaciones.blade.php
@@ -1,39 +1,37 @@
-<div class="card widget-flat">
-                            <div class="card-body">
-                              
-                            <h5 class="mb-3">Grabaciones</h5>
+<?php 
+    foreach ($segui as $adj) {
+        if ($adj->tipo == 12) {
+?>
 
-                                <div class="row">
-                                    <?php foreach ($segui as $adj) {
-                                                if ($adj->tipo == 12) {?>
-                                                                    <div class="col-xl-2">
-                                                                        <div class="card mb-1 ">
-                                                                            <div class="p-2">
-                                                                                <div class="row align-items-center">
-
-                                                                                    <div class="col-auto">
-                                                                                        <!-- Button -->
-                                                                                        <?php $ht = 'https://recordingsuricata.sfo3.digitaloceanspaces.com/'. $adj->descripcion;?>
-
-                                                                                        <audio controls>
-                                                                                            <source src="<?php echo $ht; ?>" type="audio/ogg">
-                                                                                            <source src="<?php echo $ht; ?>" type="audio/mpeg">
-                                                                                            Your browser does not support the audio element.
-                                                                                            </audio>
-
-
-
-
-                                                                                    </div>
-                                                                                </div>
-                                                                            </div>
-                                                                        </div>
-                                                                    </div> <!-- end col -->
-                                                <?php }
-                                    }?>
-
-                                </div>
-
-                            </div>
-
-                        </div>
+<div class="card shadow-sm">
+    <div class="d-flex justify-content-between mx-3 mt-1">
+        <div>
+            <h5 class="mb-3">Grabaciones</h5>
+        </div>
+        <div>
+            <i class="mdi mdi-headphones widget-icon bg-secondary-lighten text-secondary"></i>
+        </div>
+    </div>
+    <hr class="mx-3 my-0">
+    <div class="card-body d-flex align-items-center">
+        <span role="button" id="playPauseBtn" class="border btn btn-light me-3">
+            <i class="mdi mdi-play" id="playIcon" style="font-size: 30px;"></i>
+            <i class="mdi mdi-pause d-none" id="pauseIcon" style="font-size: 30px;"></i>
+        </span>
+        <div class="flex-grow-1">
+            <input type="range" id="progressBar" class="form-range" value="0" min="0" max="100">
+        </div>
+        <?php $ht = 'https://recordingsuricata.sfo3.digitaloceanspaces.com/' . $adj->descripcion; ?>
+        <span id="currentTime" class="ms-3">00:00</span>
+        <span>/</span>
+        <span id="totalDuration" class="ms-1">00:00</span>
+        <audio id="audioPlayer">
+            <source src="<?php echo $ht; ?>" type="audio/ogg">
+            <source src="<?php echo $ht; ?>" type="audio/mpeg">
+        </audio>
+    </div>
+</div>
+<?php 
+        }
+    }
+?>
