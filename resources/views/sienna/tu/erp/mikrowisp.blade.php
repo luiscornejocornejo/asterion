18e8ca50a9b29904a11ac0c729bbc39b23d4f342
luiscornejocornejo <kayser19992005@gmail.com>
2024-11-24 13:32:59 -0300

    eee

--- resources/views/sienna/tu/erp/mikrowisp.blade.php
+++ resources/views/sienna/tu/erp/mikrowisp.blade.php
@@ -6,12 +6,12 @@
             <a class="nav-link active show" id="v-pills-home-tab" data-bs-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home"
                 aria-selected="true">
                 <i class="mdi mdi-home-variant d-md-none d-block"></i>
-                <span class="d-none d-md-block">Datos</span>
+                <span class="d-none d-md-block">Tickets</span>
             </a>
             <a class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile"
                 aria-selected="false">
                 <i class="mdi mdi-account-circle d-md-none d-block"></i>
-                <span class="d-none d-md-block">Tickets</span>
+                <span class="d-none d-md-block">Datos</span>
             </a>
             <a class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings"
                 aria-selected="false">
@@ -24,7 +24,122 @@
     <div class="col-sm-9">
         <div class="tab-content" id="v-pills-tabContent">
             <div class="tab-pane fade active show" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-                <p class="mb-0">...</p>
+
+
+                <div class="row">
+                    <form method="post" action="/crearispkipper">
+                        @csrf
+                        <div class="row">
+                            <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12 mt-2">
+                                <label class="form-label" id="invoice_number">Invoice:</label>
+                                <input required name="invoice" type="text" class="form-control" id="invoice_number"
+                                    aria-describedby="emailHelp" placeholder="fb101232">
+                            </div>
+                            <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12 mt-2">
+                                <label class="form-label" for="description">Descripción</label>
+                                <input required name="descripcion" type="text" class="form-control" id="description"
+                                    placeholder="pago por servicio de inet">
+                            </div>
+                            <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12 mt-2">
+                                <label class="form-label" for="amount">Monto:</label>
+                                <input required name="monto" type="number" step="0.01" class="form-control" id="amount"
+                                    placeholder="50.00">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12 mt-2">
+                                <label class="form-label" for="customer">Número de cliente</label>
+                                <input required name="cliente" type="text" class="form-control" id="customer"
+                                    placeholder="cliente">
+                            </div>
+                            <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12 mt-2">
+                                <label class="form-label" for="nameUser">Nombre:</label>
+                                <input required name="nombre" type="text" class="form-control" id="nameUser"
+                                    placeholder="nombre">
+                            </div>
+                            <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12 mt-2">
+                                <label class="form-label" for="lastNameUser">Apellido:</label>
+                                <input required name="apellido" type="text" class="form-control" id="lastNameUser"
+                                    placeholder="apellido">
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-success mt-3 mb-2">Generar orden</button>
+                        <hr class="mx-1" />
+                    </form>
+                </div>
+        <div class="row">
+            <script>
+                function copyToClipboard(text) {
+                    navigator.clipboard.writeText(text)
+                        .then(() => {
+                            console.log("Contenido copiado al portapapeles!");
+                            let toastEl = document.getElementById('liveToast')
+                            toastEl.querySelector('.toast-body').innerHTML =
+                                'Orden copiada al portapapeles.'
+                            let toast = new bootstrap.Toast(toastEl)
+                            toast.show()
+                        })
+                        .catch(err => {
+                            console.error("Error al copiar el contenido: ", err);
+                        });
+                }
+
+                url =
+                    "https://<?php echo $subdomain_tmp; ?>.pagoralia.com/api/listadocliente?&token=elmasgrandesiguesiendoriverplate&cliente=<?php echo $resultados[0]->iddelcliente; ?>"
+                axios.get(url)
+                    .then(function(response) {
+                        res = '';
+                        console.log(response.data);
+                        for (i = 0; i < response.data.length; i++) {
+                            let badge = response.data[i].estado === 'paid' ?
+                                'badge bg-success' :
+                                response.data[i].estado === 'pending' ?
+                                'badge bg-warning' :
+                                '';
+                            console.log(badge)
+                            res += `<tr class="text-center">
+                                <td>${response.data[i].recibo}</td>
+                                <td>${response.data[i].detalle}</td>
+                                <td>${response.data[i].total}</td>
+                                <td><span class="${badge}">${response.data[i].estado}</span></td>
+                                <td>
+                                    <button class="btn btn-success custom-tooltip" onclick="copyToClipboard('${response.data[i].realink}')">
+                                        <i class="mdi mdi-content-copy text-light"></i>
+                                        <span class="tooltiptext">Copiar link.</span>
+                                    </button>
+                                </td>
+                            </tr>`;
+                        }
+
+                        document.getElementById("log").innerHTML = null;
+
+                        document.getElementById("log").innerHTML = res;
+
+                    })
+                    .catch(function(error) {
+                        // función para capturar el error
+                        console.log(error);
+                    })
+                    .then(function() {
+                        // función que siempre se ejecuta
+                    });
+            </script>
+            <table id="casadepapel" class="table table-striped dt-responsive nowrap w-100 text-light">
+                <thead>
+                    <tr class="text-center bg-dark">
+                        <th class="text-light">Invoice</th>
+                        <th class="text-light">Detalle</th>
+                        <th class="text-light">Total</th>
+                        <th class="text-light">Estado</th>
+                        <th class="text-light">Link</th>
+                    </tr>
+                </thead>
+                <tbody id="log">
+
+                </tbody>
+            </table>
+        </div>
+
             </div>
             <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
                 <p class="mb-0">...</p>
