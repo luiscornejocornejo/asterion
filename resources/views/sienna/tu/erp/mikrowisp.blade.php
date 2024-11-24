08fd84cdfbd284c8412064b7e1e940f00a490221
luiscornejocornejo <kayser19992005@gmail.com>
2024-11-24 15:24:49 -0300

    333

--- resources/views/sienna/tu/erp/mikrowisp.blade.php
+++ resources/views/sienna/tu/erp/mikrowisp.blade.php
@@ -40,7 +40,7 @@ $ticketserp= file_get_contents("https://wiber.suricata-ispkeeper.com.ar/api/tick
                         <div class="row">
                         <div class="col-xxl-3 col-xl-3 col-lg-4 col-sm-6">
                                         <label for="agent" class="form-label">Usuario</label>
-                                        <?php for($i;$i<sizeof($usuariosserp2);$i++){
+                                        <?php for($i=0;$i<sizeof($usuariosserp2);$i++){
                                             var_dump($usuariosserp2[$i]);
 
                                         }
