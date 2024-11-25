bc6d1afd2a3530fd4ad9a75f65dcb7b3170af1e2
Ficuu <facuu.paez95@gmail.com>
2024-11-25 11:42:08 -0300

    asd

--- resources/views/sienna/tu/erp/mikrowisp.blade.php
+++ resources/views/sienna/tu/erp/mikrowisp.blade.php
@@ -23,13 +23,13 @@ $ticketserp2 = json_decode($ticketserp, true);
         </div>
         <ul class="nav nav-pills bg-nav-pills nav-justified mb-3">
             <li class="nav-item">
-                <a href="#ticket" data-bs-toggle="tab" aria-expanded="false" class="nav-link rounded-0">
+                <a href="#ticket" data-bs-toggle="tab" aria-expanded="true" class="nav-link rounded-0 active">
                     <i class="mdi mdi-home-variant d-md-none d-block"></i>
                     <span class="d-none d-md-block">Tickets</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a href="#data" data-bs-toggle="tab" aria-expanded="true" class="nav-link rounded-0 active">
+                <a href="#data" data-bs-toggle="tab" aria-expanded="false" class="nav-link rounded-0">
                     <i class="mdi mdi-account-circle d-md-none d-block"></i>
                     <span class="d-none d-md-block">Datos</span>
                 </a>
@@ -42,31 +42,31 @@ $ticketserp2 = json_decode($ticketserp, true);
             </li>
         </ul>
         <div class="tab-content">
-            <div class="tab-pane" id="ticket">
-               
-                    <form method="post" action="api/crearispkipper">
-                        @csrf
-                        <div class="row">
-                            <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
-                                <label for="agent" class="form-label">Usuario</label>
-                                <select name="usuarios" id="agent" class="form-select">
+            <div class="tab-pane show active" id="ticket">
 
-                                    <?php for($i=0;$i<sizeof($usuariosserp2);$i++){
+                <form method="post" action="api/crearispkipper">
+                    @csrf
+                    <div class="row">
+                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
+                            <label for="agent" class="form-label">Usuario</label>
+                            <select name="usuarios" id="agent" class="form-select">
+
+                                <?php for($i=0;$i<sizeof($usuariosserp2);$i++){
                                         ?>
-                                    <option value="{{ $usuariosserp2[$i]['usuario_id'] }}">
-                                        {{ $usuariosserp2[$i]['usuario_nombre'] }}
-                                        {{ $usuariosserp2[$i]['usuario_apellido'] }}
-                                    </option>
-                                    <?php }
+                                <option value="{{ $usuariosserp2[$i]['usuario_id'] }}">
+                                    {{ $usuariosserp2[$i]['usuario_nombre'] }}
+                                    {{ $usuariosserp2[$i]['usuario_apellido'] }}
+                                </option>
+                                <?php }
                                     
                                     ?>
-                                </select>
-                            </div>
-                            <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
-                                <label class="form-label" for="description">Subcategoria</label>
-                                <select name="subcategorias" id="agent" class="form-select">
+                            </select>
+                        </div>
+                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-sm-12">
+                            <label class="form-label" for="description">Subcategoria</label>
+                            <select name="subcategorias" id="agent" class="form-select">
 
-                                    <?php
+                                <?php
                                     function buscarNombreSubcategoria($subserp2, $idBuscado) {
                                         for($j=0;$j<sizeof($subserp2);$j++){
                                             if ($subserp2[$j]["ticket_subcategoria_id"]== $idBuscado) {
@@ -77,70 +77,69 @@ $ticketserp2 = json_decode($ticketserp, true);
                                     }
                                     for($i=0;$i<sizeof($subserp2);$i++){
                                         ?>
-                                    <option value="<?php echo $subserp2[$i]['ticket_subcategoria_id']; ?>">
-                                        <?php echo $subserp2[$i]['ticket_subcategoria_nombre']; ?>
+                                <option value="<?php echo $subserp2[$i]['ticket_subcategoria_id']; ?>">
+                                    <?php echo $subserp2[$i]['ticket_subcategoria_nombre']; ?>
 
-                                    </option>
-                                    <?php }
+                                </option>
+                                <?php }
                                     
                                     ?>
-                                </select>
-                            </div>
-                            <div class="mb-3">
-                                <label for="example-textarea" class="form-label">Descripcion</label>
-                                <textarea name="detalle" class="form-control" id="example-textarea" rows="4"></textarea>
-                            </div>
-                            <div class="mb-3">
-                                <label for="example-textarea" class="form-label">Cliente</label>
-                                <input required name="cliente" type="text" class="form-control" id="lastNameUser"
-                                    value="<?php echo $resultadoscliente[0]->cliente; ?>">
-                                <input name="prioridad" type="hidden" class="form-control" id="lastNameUser"
-                                    value="3">
-                            </div>
-
-
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="example-textarea" class="form-label">Descripcion</label>
+                            <textarea name="detalle" class="form-control" id="example-textarea" rows="4"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="example-textarea" class="form-label">Cliente</label>
+                            <input required name="cliente" type="text" class="form-control" id="lastNameUser"
+                                value="<?php echo $resultadoscliente[0]->cliente; ?>">
+                            <input name="prioridad" type="hidden" class="form-control" id="lastNameUser"
+                                value="3">
                         </div>
 
-                        <button type="submit" class="btn btn-success mt-3 mb-2">Crear Ticket</button>
-                        <hr class="mx-1" />
-                    </form>
-                
-                    <table id="casadepapel" class="table table-striped dt-responsive nowrap w-100 text-light">
-                        <thead>
-                            <tr class="text-center bg-dark">
-                                <th class="text-light">ticket_id</th>
-                                <th class="text-light">ticket_dia</th>
-                                <th class="text-light">ticket_estado</th>
-                                <th class="text-light">ticket_subcategoria</th>
-                            </tr>
-                        </thead>
-                        <tbody id="log">
-                            <?php for($i=0;$i<sizeof($ticketserp2);$i++){
+
+                    </div>
+
+                    <button type="submit" class="btn btn-success mt-3 mb-2">Crear Ticket</button>
+                    <hr class="mx-1" />
+                </form>
+
+                <table id="casadepapel" class="table table-striped dt-responsive nowrap w-100 text-light">
+                    <thead>
+                        <tr class="text-center bg-dark">
+                            <th class="text-light">ticket_id</th>
+                            <th class="text-light">ticket_dia</th>
+                            <th class="text-light">ticket_estado</th>
+                            <th class="text-light">ticket_subcategoria</th>
+                        </tr>
+                    </thead>
+                    <tbody id="log">
+                        <?php for($i=0;$i<sizeof($ticketserp2);$i++){
                                         ?>
-                            <tr class="text-center">
-                                <td>{{ $ticketserp2[$i]['ticket_id'] }} </td>
-                                <td>{{ $ticketserp2[$i]['ticket_dia'] }} {{ $ticketserp2[$i]['ticket_hora'] }}
-                                </td>
-                                <td>{{ $ticketserp2[$i]['ticket_estado'] }} </td>
-                                <td> <?php echo $nomsub = buscarNombreSubcategoria($subserp2, $ticketserp2[$i]['ticket_subcategoria']); ?> </td>
+                        <tr class="text-center">
+                            <td>{{ $ticketserp2[$i]['ticket_id'] }} </td>
+                            <td>{{ $ticketserp2[$i]['ticket_dia'] }} {{ $ticketserp2[$i]['ticket_hora'] }}
+                            </td>
+                            <td>{{ $ticketserp2[$i]['ticket_estado'] }} </td>
+                            <td> <?php echo $nomsub = buscarNombreSubcategoria($subserp2, $ticketserp2[$i]['ticket_subcategoria']); ?> </td>
 
 
-                            </tr>
-                            <?php }?>
+                        </tr>
+                        <?php }?>
 
-                        </tbody>
-                    </table>
-                
+                    </tbody>
+                </table>
+
+
+            </div>
+            <div class="tab-pane " id="data">
+                Aqui va información de datos
+            </div>
+            <div class="tab-pane" id="extra">
+                <div data-tf-live="01JDFRQDH03PQAH7HE59FRXFB0"></div>
+                <script src="//embed.typeform.com/next/embed.js"></script>
             </div>
         </div>
-        <div class="tab-pane show active" id="data">
-            Aqui va información de datos
-        </div>
-        <div class="tab-pane" id="extra">
-            <div data-tf-live="01JDFRQDH03PQAH7HE59FRXFB0"></div>
-            <script src="//embed.typeform.com/next/embed.js"></script>
-        </div>
-
-
     </div>
 </div>
