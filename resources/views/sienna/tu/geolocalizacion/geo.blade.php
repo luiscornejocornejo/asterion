47f3213c4ea6a982e8ed617f4405f93ea103dad9
luiscornejocornejo <kayser19992005@gmail.com>
2024-11-29 08:11:46 -0300

    dsf

--- resources/views/sienna/tu/geolocalizacion/geo.blade.php
+++ resources/views/sienna/tu/geolocalizacion/geo.blade.php
@@ -1,15 +1,14 @@
 
 
 
-<div class="modal fade" id="bs-example-modal-sm-departament" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-sm">
-        <div class="modal-content">
-            <div class="modal-header bg-dark text-white">
-                <h4 class="modal-title" id="mySmallModalLabel">Geolocalizacion</h4>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-hidden="true"></button>
-            </div>
-            <div class="modal-body">
-            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" />
+<div class="row">
+    <div class="col-md-4">
+        <!-- Portlet card -->
+        <div class="card mb-md-0 mb-3">
+            <div class="card-body">
+               
+                <h5 class="card-title mb-0">Geolocalizacion</h5>
+                <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" />
 <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"></script>
 
 <div id="map" style="height: 400px; width: 100%;"></div>
@@ -27,8 +26,14 @@
     L.marker([lat, lng]).addTo(map)
         .bindPopup('Ubicación seleccionada.')
         .openPopup();
-</script>
+</script>       
+                
             </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
+        </div> <!-- end card-->
+    </div><!-- end col -->
+                                
+   
+                                
+  
 </div>
+<!-- end row -->
