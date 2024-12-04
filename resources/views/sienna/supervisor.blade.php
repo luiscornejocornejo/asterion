5a440d8bac5414dbfdf765c36ffbc8e92c5eae6e
luiscornejocornejo <kayser19992005@gmail.com>
2024-12-04 11:10:25 -0300

    www

--- resources/views/sienna/supervisor.blade.php
+++ resources/views/sienna/supervisor.blade.php
@@ -704,7 +704,7 @@ function obtenerFechaUTC() {
                         '<td><span class="badge '+colordepto+'" style="font-size:medium;" onclick="area2(`' + response.data[i].ticketid + '`,`' + response.data[i].conversation_id + '`,`' + response.data[i].user_id + '`)"  class="badge bg-info" style="font-size: medium" role="button" data-bs-toggle="modal" data-bs-target="#bs-example-modal-sm202">'+response.data[i].depto+' </span></td>' +
                         '<td><span style="font-size:medium;" class="'+colorpri+'" onclick="topic(`' + result + '`,`' + response.data[i].ticketid + '`,`' + response.data[i].conversation_id + '`,`' + response.data[i].iddepto + '`)"  class="badge badge-info-lighten border" style="font-size: medium" role="button" data-bs-toggle="modal" data-bs-target="#bs-example-modal-smtopic"> ' + response.data[i].topicnombre + '</span></td> ' +
                         ' <td><span style="font-size:medium;" class="'+colorpri+'">' + response.data[i].pri + '</span></td>' +
-
+ 
                         ' <td>' + response.data[i].cel + '</td>' +
                         ' <td>' + response.data[i].nuevotiempo + '</td>' +
                         '<td> <span onclick="estado2(`' + result + '`,`' + response.data[i].ticketid + '`,`' + response.data[i].conversation_id + '`,`' + response.data[i].iddepto + '`)"  class="badge '+colorestado+' " style="font-size: medium" role="button" data-bs-toggle="modal" data-bs-target="#bs-example-modal-sm">' + response.data[i].estadoname + ' </span></td> ' +
