c3ebc194c3a11ff323042816537bf5303375bad8
luiscornejocornejo <kayser19992005@gmail.com>
2024-12-10 16:11:52 -0300

    eee

--- app/Http/Controllers/cloudtickets.php
+++ app/Http/Controllers/cloudtickets.php
@@ -881,7 +881,8 @@ class cloudtickets extends Controller
     {   
         $merchant=$this->dominio();
 
-        $query ="select a.id,a.created_at,a.estadoconv,b.nombre as deptoname ,c.nombre as topicname from soporte.siennatickets as a 
+        $query ="select a.id, a.siennaestado,
+        a.created_at,a.estadoconv,b.nombre as deptoname ,c.nombre as topicname from soporte.siennatickets as a 
         left join soporte.siennadepto b
         on b.id=a.siennadepto
         left join soporte.siennatopic c
