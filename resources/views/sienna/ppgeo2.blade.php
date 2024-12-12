ed17489bf7142c107524998b17148b4ca4c4fb53
luiscornejocornejo <kayser19992005@gmail.com>
2024-12-12 05:12:16 -0300

    eee

--- resources/views/sienna/ppgeo2.blade.php
+++ resources/views/sienna/ppgeo2.blade.php
@@ -38,7 +38,7 @@ const iconNormal = L.icon({
         });
 
         const iconAlerta = L.icon({
-            iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-red.png',
+            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
             iconSize: [25, 41],
             iconAnchor: [12, 41],
             popupAnchor: [1, -34],
