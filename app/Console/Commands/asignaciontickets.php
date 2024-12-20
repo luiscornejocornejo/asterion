e79cb30df97c53fb2c7ce5aa67155e1d827165eb
luiscornejocornejo <kayser19992005@gmail.com>
2024-12-20 11:42:03 -0300

    fff

--- app/Console/Commands/asignaciontickets.php
+++ app/Console/Commands/asignaciontickets.php
@@ -105,7 +105,10 @@ class asignaciontickets extends Command
 
         try {
         $fec=explode("-",$fecha);
-
+        echo $horaLocal;
+        echo $fecha;
+        echo $merchant;
+        echo $area;
         if(($horaLocal>=$fec[0]) and ($horaLocal<$fec[1])){
             return true;
         }else{
@@ -141,7 +144,7 @@ class asignaciontickets extends Command
 
              $enhora=$this->enhora($merchant,$area);
              if($enhora){
-                $query2="select s.idusuario as uu,(select count(*) from ".$merchant.".siennatickets s2  
+             echo    $query2="select s.idusuario as uu,(select count(*) from ".$merchant.".siennatickets s2  
                 where s2.asignado=s.idusuario and s2.siennaestado not in('3','4'))as cantidad from ".$merchant.".siennaloginxenioo s
                 join ".$merchant.".users s3 on s3.id=s.idusuario 
                 where
