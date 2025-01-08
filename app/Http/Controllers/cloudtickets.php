8ac205f2923aa8134df7fdc3ccdd00f52c6bf08d
luiscornejocornejo <kayser19992005@gmail.com>
2025-01-08 12:26:00 -0300

    ee

--- app/Http/Controllers/cloudtickets.php
+++ app/Http/Controllers/cloudtickets.php
@@ -429,7 +429,7 @@ class cloudtickets extends Controller
             'Cookie: xenioo-id=Bearer+ZlHPzQ0ZfubwcHXAjjXMG0hDlJI22S1S0dqgKs0H7O06PghfV3BRy6Wxmn7PLb6RUmfIXRXiijo5X8E7%2flsAUV24IzaB28PYO%2bw90fEOTrp8Hx0WQCQ%2btq69lwpWUZpCg0ga2p%2bQD%2bI9KFMrCB6Ht%2bJM4ZOuekNf%2bYWtUBQ%2bm1prYPb8nDXWuRnU6qgtzr7zInbdRjyNhsdg41gTr7AstZ3sLt2wAXQS%2ba8zSGYe1UZY7gvoYm%2fGKj6TbvAdWnO0WXTVkwnB1jhMbWDX38PYGt2jkNoaUXRWxncuQSxJRzUIBWuTJGju%2b7EZOaoK07cXNk%2bUPBMSV1Q9gV6Gzc8CkA%3d%3d',
         );
 
-        $url="https://suricata4.com.ar/api/closechat?token=EDElDqlQf3RDP5EDK1pHhugV9M6aCXtwAm57SD0G5JYZjw7RxwZbbfdKMhWYdUUM&idbot=".$idbot."&idconv=".$idconv."&bot_channel=".$source;
+         echo $url="https://suricata4.com.ar/api/closechat?token=EDElDqlQf3RDP5EDK1pHhugV9M6aCXtwAm57SD0G5JYZjw7RxwZbbfdKMhWYdUUM&idbot=".$idbot."&idconv=".$idconv."&bot_channel=".$source;
         // Set options for the cURL request
         $options = array(
             CURLOPT_URL => $url,
@@ -449,6 +449,7 @@ class cloudtickets extends Controller
         $response = curl_exec($curl);
         curl_close($curl);
 
+        dd($response);
     }
 
     
