d1e2d56becc7cbd513222590ce8ee656b201eb6d
Ficuu <facuu.paez95@gmail.com>
2025-01-10 15:22:29 -0300

    asd

--- resources/views/sienna/ticketunico2.blade.php
+++ resources/views/sienna/ticketunico2.blade.php
@@ -325,7 +325,7 @@ function tituloiconos($iconos, $tipo)
                             }
                         }?>
 
-                        <div class="accordion mb-3" id="accordionPanelsStayOpenExample">
+                        <div class="accordion mb-3 mt-3" id="accordionPanelsStayOpenExample">
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="panelsStayOpen-headingOne">
                                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
