de14decc83399347faeb61cea84bd733c8839a9f
Ficuu <facuu.paez95@gmail.com>
2025-01-17 17:28:24 -0300

    asd

--- resources/views/sienna/tu/informacionticket.blade.php
+++ resources/views/sienna/tu/informacionticket.blade.php
@@ -1,4 +1,4 @@
-<div class="card widget-flat" id="infoTicket">
+<div class="card widget-flat mt-2" id="infoTicket">
                             <div class="card-body">
                                 <div class="d-flex justify-content-between">
                                     <div>
