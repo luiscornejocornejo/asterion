717667a24f794c2c57d655a52c7678c8431a9ac3
luis cornejo <kayser1712@gmail.com>
2025-01-18 22:44:06 -0300

    rrr

--- resources/views/sienna/tu/bot/botpres.blade.php
+++ resources/views/sienna/tu/bot/botpres.blade.php
@@ -65,17 +65,7 @@ $tipobot=$val->tipo;
     });
   </script>
    
-<div class="<?php echo $vero; ?> d-flex justify-content-between mt-2 mb-2">
-    <div></div>
 
-    <div class="me-2">
-        <button class="btn btn-success " data-bs-toggle="modal" data-bs-target="#open-conversation">
-            <i class="mdi mdi-whatsapp me-1"></i>Reabrir conversación
-        </button>
-    </div>
-
-
-</div>
 <div class="me-2">
 <!-- CSS de Select2 -->
 <!-- CSS de Select2 -->
@@ -88,36 +78,9 @@ $tipobot=$val->tipo;
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js" defer></script>
 
 <!-- HTML del Select -->
-<select onChange="mos(this.options[this.selectedIndex].value)" class="form-select js-example-basic-single" aria-label="Default select example">
-    <option>Seleccionar</option>
-    <?php foreach($datosatajos as $val) { ?>
-        <option value="<?php echo htmlspecialchars($val->contenido); ?>"><?php echo $val->nombre; ?></option>
-    <?php } ?>
-</select>
-
-
-<!-- JavaScript para inicialización -->
-<script>
-    function mos(dd) {
-        document.getElementById("cop").innerHTML = null;
-        document.getElementById("cop").innerHTML = dd;
-    }
-
-    // Espera a que todo el DOM esté listo y las librerías estén cargadas
-    document.addEventListener("DOMContentLoaded", function() {
-        if ($.fn.select2) { // Verifica si select2 está disponible
-            $('.js-example-basic-single').select2();
-        } else {
-            console.error("Select2 no se cargó correctamente");
-        }
-    });
-</script>
-
 
 
 
 
 </div>
-<div class="me-2">
-<textarea class="form-control" id="cop"></textarea>
-</div>
+
