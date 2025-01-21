42c64786e2625da0be8cebb8feac97e40c76fe1f
luis cornejo <kayser1712@gmail.com>
2025-01-21 01:08:17 -0300

    rrr

--- resources/views/sienna/ticketsmodals/conversacion.blade.php
+++ resources/views/sienna/ticketsmodals/conversacion.blade.php
@@ -6,6 +6,31 @@
                     <div class="modal-body" id="vista">
                         
                     </div>
+                    <div class="card-body" style="margin:20px;" id="registros">
+                            <div class="d-flex justify-content-between">
+                            <div>
+                                <h4 class="fw-normal text-dark" title="Number of Customers">
+                                Información de usuario2
+                                </h4>
+                            </div>
+                            <div>
+                                <i
+                                class="mdi mdi-card-account-details widget-icon bg-secondary-lighten text-secondary"
+                                ></i>
+                            </div>
+                            </div>
+                            <hr style="margin-top: 10px" />
+                            <div class="d-flex mt-2">
+                            <i class="mdi mdi-card-account-details"></i>&nbsp;Numero cliente:&nbsp;
+                            <span class="badge badge-secondary-lighten line-h"> <span id="clientec"></span> </span>
+                            </div>
+
+                           
+                            
+                            
+                           
+                           
+                    </div>
                    
                    
                 </div><!-- /.modal-content -->
