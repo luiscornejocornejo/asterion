c41fa36b00d77a530d4e66101c3bccd0959a959b
luis cornejo <kayser1712@gmail.com>
2025-02-03 22:42:53 -0300

    eee

--- resources/views/sienna/flotantes/salientes.blade.php
+++ resources/views/sienna/flotantes/salientes.blade.php
@@ -13,103 +13,6 @@ foreach($datosservicios2 as $valservicios2){
 
 
 ?>
-<div id="warning-alert-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-sm">
-        <div class="modal-content">
-            <div class="modal-header " style="background-color:#3c4655">
-                <h5 class="modal-title text-white" id="exampleModalLabel"><i class="mdi mdi-whatsapp text-success"></i> Enviar plantilla de Whatsapp</h5>
-
-            </div>
-
-            <?php if($botpresservicio){?>
-                <form id="frmAgregarBienCapitalizable" action="/" method="post">
-                @csrf
-
-                <div class="modal-body p-2">
-                    <div id="resul"></div>
-
-                    <div class="mb-3 mx-2">
-                        <label for="telefono" class="form-label">Whatsapp2</label>
-                        <input name="telefono" type="cel" id="telefono" placeholder="+5491133258450" class="form-control" required>
-                    </div>
-
-                    <div class="mb-3 mx-2">
-                        <label for="template" class="form-label">Plantilla</label>
-                        <select class="form-select" id="template">
-                        <?php
-
-                        $query22 = "SELECT * FROM sienna_bp_templates";
-
-                        $resultados22 = DB::select($query22);
-                        foreach ($resultados22 as $val22) {
-                            $id = $val22->id;
-                            $nombre = $val22->nombre;
-                            $tipo_bp_salientes = $val22->tipo_bp_salientes;
-                            echo "<option value='" . $id . "'>" . $nombre . "</option>";
-                        }
-                        ?>
-                    </select>
-                    </div>
-                    <div class="alert alert-warning mx-2" role="alert">
-                        <i class="ri-alert-line me-1 align-middle font-16"></i> Atención - Este proceso puede demorar unos minutos y el usuario debe responder el mensaje enviado.
-                    </div>
-
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary mb-0" data-bs-dismiss="modal">Cancelar</button>
-                    <button type="button" style="background-color: #ffc95c;" class="btn  mb-0 " onclick="enviarmensaje()" class="  "><span style="color: #495057;">Enviar</span></button>
-                </div>
-            </form>
-            <?php
-
-            }else{?>
-            <form id="frmAgregarBienCapitalizable" action="/" method="post">
-                @csrf
-
-                <div class="modal-body p-2">
-                    <div id="resul"></div>
-
-                    <div class="mb-3 mx-2">
-                        <label for="telefono" class="form-label">Whatsapp1</label>
-                        <input name="telefono" type="cel" id="telefono" placeholder="+5491133258450" class="form-control" required>
-                    </div>
-
-                    <div class="mb-3 mx-2">
-                        <label for="template" class="form-label">Plantilla</label>
-                        <select class="form-select" id="template">
-                        <?php
-
-                        $query22 = "SELECT id, nombre, url, descripcion FROM template";
-
-                        $resultados22 = DB::select($query22);
-                        foreach ($resultados22 as $val22) {
-                            $url = $val22->url;
-                            $nombre = $val22->nombre;
-                            $descripcion = $val22->descripcion;
-                            echo "<option value='" . $url . "'>" . $nombre . "</option>";
-                        }
-                        ?>
-                    </select>
-                    </div>
-                    <div class="alert alert-warning mx-2" role="alert">
-                        <i class="ri-alert-line me-1 align-middle font-16"></i> Atención - Este proceso puede demorar unos minutos y el usuario debe responder el mensaje enviado.
-                    </div>
-
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary mb-0" data-bs-dismiss="modal">Cancelar</button>
-                    <button type="button" style="background-color: #ffc95c;" class="btn  mb-0 " onclick="mensaje('<?php echo $saliente = session('saliente'); ?>')" class="  "><span style="color: #495057;">Enviar</span></button>
-                </div>
-            </form>
-
-            <?php }?>
-
-
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
-
 <script>
     function mensaje(saliente) {
 
@@ -243,3 +146,100 @@ foreach($datosservicios2 as $valservicios2){
         }
     }
 </script>
+<div id="warning-alert-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header " style="background-color:#3c4655">
+                <h5 class="modal-title text-white" id="exampleModalLabel"><i class="mdi mdi-whatsapp text-success"></i> Enviar plantilla de Whatsapp</h5>
+
+            </div>
+
+            <?php if($botpresservicio){?>
+                <form id="frmAgregarBienCapitalizable" action="/" method="post">
+                @csrf
+
+                <div class="modal-body p-2">
+                    <div id="resul"></div>
+
+                    <div class="mb-3 mx-2">
+                        <label for="telefono" class="form-label">Whatsapp2</label>
+                        <input name="telefono" type="cel" id="telefono" placeholder="+5491133258450" class="form-control" required>
+                    </div>
+
+                    <div class="mb-3 mx-2">
+                        <label for="template" class="form-label">Plantilla</label>
+                        <select class="form-select" id="template">
+                        <?php
+
+                        $query22 = "SELECT * FROM sienna_bp_templates";
+
+                        $resultados22 = DB::select($query22);
+                        foreach ($resultados22 as $val22) {
+                            $id = $val22->id;
+                            $nombre = $val22->nombre;
+                            $tipo_bp_salientes = $val22->tipo_bp_salientes;
+                            echo "<option value='" . $id . "'>" . $nombre . "</option>";
+                        }
+                        ?>
+                    </select>
+                    </div>
+                    <div class="alert alert-warning mx-2" role="alert">
+                        <i class="ri-alert-line me-1 align-middle font-16"></i> Atención - Este proceso puede demorar unos minutos y el usuario debe responder el mensaje enviado.
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary mb-0" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="button" style="background-color: #ffc95c;" class="btn  mb-0 " onclick="enviarmensaje()" class="  "><span style="color: #495057;">Enviar</span></button>
+                </div>
+            </form>
+            <?php
+
+            }else{?>
+            <form id="frmAgregarBienCapitalizable" action="/" method="post">
+                @csrf
+
+                <div class="modal-body p-2">
+                    <div id="resul"></div>
+
+                    <div class="mb-3 mx-2">
+                        <label for="telefono" class="form-label">Whatsapp1</label>
+                        <input name="telefono" type="cel" id="telefono" placeholder="+5491133258450" class="form-control" required>
+                    </div>
+
+                    <div class="mb-3 mx-2">
+                        <label for="template" class="form-label">Plantilla</label>
+                        <select class="form-select" id="template">
+                        <?php
+
+                        $query22 = "SELECT id, nombre, url, descripcion FROM template";
+
+                        $resultados22 = DB::select($query22);
+                        foreach ($resultados22 as $val22) {
+                            $url = $val22->url;
+                            $nombre = $val22->nombre;
+                            $descripcion = $val22->descripcion;
+                            echo "<option value='" . $url . "'>" . $nombre . "</option>";
+                        }
+                        ?>
+                    </select>
+                    </div>
+                    <div class="alert alert-warning mx-2" role="alert">
+                        <i class="ri-alert-line me-1 align-middle font-16"></i> Atención - Este proceso puede demorar unos minutos y el usuario debe responder el mensaje enviado.
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary mb-0" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="button" style="background-color: #ffc95c;" class="btn  mb-0 " onclick="mensaje('<?php echo $saliente = session('saliente'); ?>')" class="  "><span style="color: #495057;">Enviar</span></button>
+                </div>
+            </form>
+
+            <?php }?>
+
+
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
