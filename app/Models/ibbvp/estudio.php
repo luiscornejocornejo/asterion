4b183762d635b993765bd71350cb64e90513cacc
luiscornejocornejo <kayser19992005@gmail.com>
2025-03-04 20:49:32 -0300

    ddd

---
+++ app/Models/ibbvp/estudio.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\ibbvp;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class estudio extends Model
+{
+    protected $table = 'estudio';
+}
