03f5c1e84bab7621a022967cbba71d2016d623b4
luiscornejocornejo <kayser19992005@gmail.com>
2025-03-04 00:56:59 -0300

    eee

--- resources/views/sienna/inicio.blade.php
+++ resources/views/sienna/inicio.blade.php
@@ -1,14 +1,17 @@
 @include('facu.header')
+<div class="wrapper menuitem-active">
+    @include('facu.menu')
 <div class="container-fluid">
     <div class="row">
         <div class="col-sm-12">
             <div class="text-center">
-                <img src="img/SuricataSuspendido.png" alt="Imágen cliente suspendido" style="max-width: 200px;">
                 <h3 class="">Bienvenido</h3>
 
             </div>
         </div><!-- end col -->
     </div>
 </div>
+</div>
+
  
 @include('facu.footer')
