15c1c872903321d3983e76093aac6fe731d00b3b
luiscornejocornejo <kayser19992005@gmail.com>
2025-03-04 21:12:58 -0300

    eee

--- resources/views/sienna/estudio.blade.php
+++ resources/views/sienna/estudio.blade.php
@@ -182,16 +182,18 @@ function estado($intedb,$ba){
 
                 </div>
                 <?php $canti++;}?>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </a>
-        </div>
+                </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </a>
+                </div>
+
+         
           
 </div>
 
