c5aa6bdb07f001b922f63773ac899328fb1ea520
luiscornejocornejo <kayser19992005@gmail.com>
2025-03-09 20:14:32 -0300

    eee

--- resources/views/facu/footer.blade.php
+++ resources/views/facu/footer.blade.php
@@ -130,6 +130,14 @@ $('input[name="daterange"]').daterangepicker({
 <svg id="SvgjsSvg1001" width="2" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev" style="overflow: hidden; top: -100%; left: -100%; position: absolute; opacity: 0;"><defs id="SvgjsDefs1002"></defs><polyline id="SvgjsPolyline1003" points="0,0"></polyline><path id="SvgjsPath1004" d="M0 0 "></path></svg><div class="daterangepicker ltr single opensright"><div class="ranges"></div><div class="drp-calendar left single" style="display: block;"><div class="calendar-table"></div><div class="calendar-time" style="display: none;"></div></div><div class="drp-calendar right" style="display: none;"><div class="calendar-table"></div><div class="calendar-time" style="display: none;"></div></div><div class="drp-buttons"><span class="drp-selected"></span><button class="cancelBtn btn btn-sm btn-default" type="button">Cancel</button><button class="applyBtn btn btn-sm btn-primary" disabled="disabled" type="button">Apply</button> </div></div><div class="jvectormap-label"></div>
 
 
+<style>
 
+html[data-sidenav-size=full]:not([data-layout=topnav]) .leftside-menu {
+    margin-left: 0 !important;
+    border-color: inherit !important;
+    opacity: 1 !important;
+}
+
+</style>
 </body>
 </html>
