6ba900db4eab4124510f9a2582c122b67de4114c
luiscornejocornejo <kayser19992005@gmail.com>
2025-03-09 09:49:17 -0300

    ggg

--- resources/views/sienna/dash.blade.php
+++ resources/views/sienna/dash.blade.php
@@ -7,55 +7,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.0/axios.min.js"></script>
 
 
-<script>
-    let navegador = navigator.userAgent;
-    if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(
-            /iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator
-        .userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
-        tipo = <?php echo $tipodemenu = session('tipodemenu'); ?>;
-        console.log("Estás usando un dispositivo móvil!!" + tipo);
 
-
-
-    } else {
-        console.log("No estás usando un móvil");
-    }
-</script>
-<?php
-$tokeninterno = session('tokeninterno');
-$miip = request()->ip();
-?>
-<script type="application/javascript">
-    necesito="<?php echo $miip;?>";
-    console.log("My public IP address is: ", necesito," mi subdominio es :  datos:","<?php echo $tokeninterno;?>");
-    let hay=<?php echo $tokeninterno;?>;
-    if(hay==0) {
-        console.log("no logear");
-    }
-    else{
-        console.log("logear");
-        var URLactual = window.location.href;
-        var porciones = URLactual.split('.');
-        let result = porciones[0].replace("https://", "");
-        url2 = "https://"+result+".suricata.cloud/api/telefonia?ip=" + necesito ;
-        console.log(url2);
-
-        axios.get(url2)
-        .then(function (response) {
-            console.log("data:");
-            console.log(response.data);
-        })
-        .catch(function (error) {
-             // función para capturar el error
-            console.log("error:");
-            console.log(error);
-        })
-        .then(function () {
-             // función que siempre se ejecuta
-        });
-
-    }
-</script>
 
 <div class="wrapper menuitem-active">
     @include('facu.menu')
