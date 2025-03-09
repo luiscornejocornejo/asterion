09ec6896e406414ae6db76fd0ed9cf0f70cc8f87
luiscornejocornejo <kayser19992005@gmail.com>
2025-03-09 20:04:34 -0300

    hhh

--- resources/views/facu/menu.blade.php
+++ resources/views/facu/menu.blade.php
@@ -1,4 +1,22 @@
+ <!-- preloader css -->
+ <link rel="stylesheet" href="assetsfacu/css/preloader.min.css"
+            type="text/css" />
 
+        <!-- Bootstrap Css -->
+        <link href="assetsfacu/css/bootstrap.min.css" id="bootstrap-style"
+            rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="assetsfacu/css/icons.min.css" rel="stylesheet" type="text/css"
+            />
+        <!-- App Css-->
+        <link href="assetsfacu/css/app.min.css" id="app-style" rel="stylesheet"
+            type="text/css" />
+
+        <!-- Custom Css -->
+        <link href="assetsfacu/css/custom.css" rel="stylesheet" type="text/css" />
+
+        <link href="assetsfacu/css/main.css" rel="stylesheet"
+    type="text/css" />
             <!-- ========== Left Sidebar Start ========== -->
             <div class="vertical-menu">
 
