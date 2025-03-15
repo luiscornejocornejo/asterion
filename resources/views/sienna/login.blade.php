66646875926e1cf6790d97aaddbf05447865261e
luiscornejocornejo <kayser19992005@gmail.com>
2025-03-15 17:42:18 -0300

    eee

--- resources/views/sienna/login.blade.php
+++ resources/views/sienna/login.blade.php
@@ -104,7 +104,7 @@ $clientIp = $_SERVER['SERVER_ADDR'] ?? 'No disponible';
 
                                         </div>
                                         <div class="mb-3">
-                                            <button style="background-color: #ffc95c;" class="btn w-100 waves-effect waves-light" type="submit">Log In</button>
+                                            <button style="background-color:rgb(106, 13, 102);" class="btn w-100 waves-effect waves-light" type="submit">Log In</button>
                                         </div>
                                     </form>
 
